7a721debe9847ee86dfc0426fa362a733d9d7eb5
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-07-30 11:00:17 -0400

    Changed RunTime to RunTimeInSeconds

--- MediaBrowser.TV/Metadata/SeriesXmlParser.cs
+++ MediaBrowser.TV/Metadata/SeriesXmlParser.cs
@@ -66,7 +66,7 @@ namespace MediaBrowser.TV.Metadata
                             int runtime;
                             if (int.TryParse(text.Split(' ')[0], out runtime))
                             {
-                                item.RunTime = TimeSpan.FromMinutes(runtime);
+                                item.RunTimeInSeconds = runtime * 60;
                             }
                         }
                         break;
