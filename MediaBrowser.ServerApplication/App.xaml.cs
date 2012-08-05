67f01df101344f4e56385d50eb347355d14219e9
RedShirtMB Mark Linton redshirt linton <RedShirtMB Mark Linton redshirt.linton@gmail.com>
2012-08-04 19:44:08 -0700

    Made ServerApplication a Single Instance app.

--- MediaBrowser.ServerApplication/App.xaml.cs
+++ MediaBrowser.ServerApplication/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Shell;
 using MediaBrowser.Controller;
 
 namespace MediaBrowser.ServerApplication
@@ -12,8 +13,32 @@ namespace MediaBrowser.ServerApplication
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        private const string Unique = "MediaBrowser3";
+        
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+                application.InitializeComponent();
+                application.Run();
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        #region ISingleInstanceApp Members
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // handle command line arguments of second instance
+            // ...
+            return true;
+        }
+        #endregion
+
         protected override void OnExit(ExitEventArgs e)
         {
             base.OnExit(e);
