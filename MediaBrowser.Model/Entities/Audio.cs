7303c6be3213891330da1dc44e87d44206dd653d
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-08-10 10:58:53 -0400

    Added additional audio properties

--- MediaBrowser.Model/Entities/Audio.cs
+++ MediaBrowser.Model/Entities/Audio.cs
@@ -3,5 +3,8 @@ namespace MediaBrowser.Model.Entities
 {
     public class Audio : BaseItem
     {
+        public int BitRate { get; set; }
+        public int Channels { get; set; }
+        public int SampleRate { get; set; }
     }
 }
