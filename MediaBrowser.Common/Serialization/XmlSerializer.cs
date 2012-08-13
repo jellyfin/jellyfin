099ac83cc32033fceca9371d2969bab16f28b6ca
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-08-13 10:51:42 -0400

    Added an XmlSerializer to common

---
+++ MediaBrowser.Common/Serialization/XmlSerializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace MediaBrowser.Common.Serialization
+{
+    /// <summary>
+    /// Provides a wrapper around third party xml serialization.
+    /// </summary>
+    public class XmlSerializer
+    {
+        public static void SerializeToStream<T>(T obj, Stream stream)
+        {
+            ServiceStack.Text.XmlSerializer.SerializeToStream(obj, stream);
+        }
+
+        public static void SerializeToFile<T>(T obj, string file)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(file))
+            {
+                ServiceStack.Text.XmlSerializer.SerializeToWriter<T>(obj, streamWriter);
+            }
+        }
+
+        public static object DeserializeFromFile(Type type, string file)
+        {
+            using (Stream stream = File.OpenRead(file))
+            {
+                return ServiceStack.Text.XmlSerializer.DeserializeFromStream(type, stream);
+            }
+        }
+
+        public static T DeserializeFromFile<T>(string file)
+        {
+            using (Stream stream = File.OpenRead(file))
+            {
+                return ServiceStack.Text.XmlSerializer.DeserializeFromStream<T>(stream);
+            }
+        }
+
+        public static T DeserializeFromStream<T>(Stream stream)
+        {
+            return ServiceStack.Text.XmlSerializer.DeserializeFromStream<T>(stream);
+        }
+
+        public static T DeserializeFromString<T>(string data)
+        {
+            return ServiceStack.Text.XmlSerializer.DeserializeFromString<T>(data);
+        }
+    }
+}
