dc47be2760e4a28277e357f8a694c39a3cd99975
RedShirtMB Mark Linton redshirt linton <RedShirtMB Mark Linton redshirt.linton@gmail.com>
2012-08-19 11:45:08 -0700

    Adjust splashscreen layout and hide close button.

--- MediaBrowser.Common/UI/Splash.xaml.cs
+++ MediaBrowser.Common/UI/Splash.xaml.cs
@@ -8,8 +8,10 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Runtime.InteropServices;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Interop;
 using System.Windows.Shapes;
 
 using MediaBrowser.Model.Progress;
@@ -21,6 +23,13 @@ namespace MediaBrowser.Common.UI
     /// </summary>
     public partial class Splash : Window
     {
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
 
         public Splash(Progress<TaskProgress> progress)
         {
@@ -33,6 +42,12 @@ namespace MediaBrowser.Common.UI
         {
             this.lblProgress.Content = e.Description;
             this.pbProgress.Value = (double)e.PercentComplete;
+        }
+
+        private void Splash_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
         }
     }
 }
