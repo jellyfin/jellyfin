0a16aeb4483ca8241c781eb40a07942b46bc171c
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-08-24 10:09:28 -0400

    Override FindItemById in Movie to search special features

--- MediaBrowser.Movies/Entities/Movie.cs
+++ MediaBrowser.Movies/Entities/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MediaBrowser.Model.Entities;
 
 namespace MediaBrowser.Movies.Entities
@@ -6,5 +8,25 @@ namespace MediaBrowser.Movies.Entities
     public class Movie : Video
     {
         public IEnumerable<Video> SpecialFeatures { get; set; }
+
+        /// <summary>
+        /// Finds an item by ID, recursively
+        /// </summary>
+        public override BaseItem FindItemById(Guid id)
+        {
+            var item = base.FindItemById(id);
+
+            if (item != null)
+            {
+                return item;
+            }
+
+            if (SpecialFeatures != null)
+            {
+                return SpecialFeatures.FirstOrDefault(i => i.Id == id);
+            }
+
+            return null;
+        }
     }
 }
