6b87713580ea33ffd687694993e1a13b2ad492d7
RedShirtMB Mark Linton redshirt linton <RedShirtMB Mark Linton redshirt.linton@gmail.com>
2012-08-25 11:22:09 -0700

    Removed PInvoke code that hid the close button.

--- MediaBrowser.Common/UI/Splash.xaml.cs
+++ MediaBrowser.Common/UI/Splash.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Interop;
 using MahApps.Metro.Controls;
 using MediaBrowser.Common.Logging;
 using MediaBrowser.Model.Progress;
@@ -13,13 +11,6 @@ namespace MediaBrowser.Common.UI
     /// </summary>
     public partial class Splash : MetroWindow
     {
-        private const int GWL_STYLE = -16;
-        private const int WS_SYSMENU = 0x80000;
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
         public Splash(Progress<TaskProgress> progress)
         {
             InitializeComponent();
@@ -42,9 +33,6 @@ namespace MediaBrowser.Common.UI
 
         private void Splash_Loaded(object sender, RoutedEventArgs e)
         {
-            var hwnd = new WindowInteropHelper(this).Handle;
-            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
-
             // Setting this in markup throws an exception at runtime
             ShowTitleBar = false;
         }
