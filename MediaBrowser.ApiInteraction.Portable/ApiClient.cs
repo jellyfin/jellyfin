a48dd192aef449a316344326344a16878e5b3894
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-09-06 21:25:09 -0400

    Added a fully portable ApiClient

---
+++ MediaBrowser.ApiInteraction.Portable/ApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace MediaBrowser.ApiInteraction.Portable
+{
+    public class ApiClient : BaseApiClient
+    {
+        private HttpWebRequest GetNewRequest(string url)
+        {
+            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+        }
+    }
+}
