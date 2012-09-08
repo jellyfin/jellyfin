44ee6265225c9b89f3b6c86890c11b509afe485f
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-09-08 10:51:40 -0400

    Added a SeriesInfo DTO

---
+++ MediaBrowser.Model/DTO/SeriesInfo.cs
@@ -0,0 +1,18 @@
+﻿using ProtoBuf;
+using System;
+
+namespace MediaBrowser.Model.DTO
+{
+    [ProtoContract]
+    public class SeriesInfo
+    {
+        [ProtoMember(1)]
+        public string Status { get; set; }
+
+        [ProtoMember(2)]
+        public string AirTime { get; set; }
+
+        [ProtoMember(3)]
+        public DayOfWeek[] AirDays { get; set; }
+    }
+}
