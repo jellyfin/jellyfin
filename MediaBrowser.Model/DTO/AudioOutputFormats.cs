5ff0717775d813f234824bb4becb80b2e4ee019e
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-09-09 10:03:49 -0400

    Added aac to the AudioResolver

--- MediaBrowser.Model/DTO/AudioOutputFormats.cs
+++ MediaBrowser.Model/DTO/AudioOutputFormats.cs
@@ -3,6 +3,7 @@ namespace MediaBrowser.Model.DTO
 {
     /// <summary>
     /// These are the audio output formats that the api is cabaple of streaming
+    /// This does not limit the inputs, only the outputs.
     /// </summary>
     public enum AudioOutputFormats
     {
