6c9ecb6d2e7bdc61d608521d238b3a94c14b279a
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-09-10 21:37:40 -0400

    Moved a few classes into their own files

---
+++ MediaBrowser.Model/Entities/AudioStream.cs
@@ -0,0 +1,26 @@
+﻿using ProtoBuf;
+
+namespace MediaBrowser.Model.Entities
+{
+    [ProtoContract]
+    public class AudioStream
+    {
+        [ProtoMember(1)]
+        public string Codec { get; set; }
+
+        [ProtoMember(2)]
+        public string Language { get; set; }
+
+        [ProtoMember(3)]
+        public int BitRate { get; set; }
+
+        [ProtoMember(4)]
+        public int Channels { get; set; }
+
+        [ProtoMember(5)]
+        public int SampleRate { get; set; }
+
+        [ProtoMember(6)]
+        public bool IsDefault { get; set; }
+    }
+}
