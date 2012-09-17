fb88e4d5fcd02ada0999a09b68a49617f4a35309
LukePulverenti Luke Pulverenti luke pulverenti <LukePulverenti Luke Pulverenti luke.pulverenti@gmail.com>
2012-09-17 14:09:35 -0400

    Added the ability to reload the server and created a Plugins solution

--- MediaBrowser.Api/Plugin.cs
+++ MediaBrowser.Api/Plugin.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.Composition;
 namespace MediaBrowser.Api
 {
     [Export(typeof(BasePlugin))]
-    public class Plugin : BaseGenericPlugin<BasePluginConfiguration>
+    public class Plugin : BasePlugin
     {
         public override string Name
         {
---
+++ MediaBrowser.Plugins.DefaultTheme/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using MediaBrowser.Common.Plugins;
+using System.ComponentModel.Composition;
+
+namespace MediaBrowser.Plugins.DefaultTheme
+{
+    [Export(typeof(BasePlugin))]
+    public class Plugin : BaseTheme
+    {
+        public override string Name
+        {
+            get { return "Default Theme"; }
+        }
+
+        protected override void InitializeInUi()
+        {
+            base.InitializeInUi();
+        }
+    }
+}
