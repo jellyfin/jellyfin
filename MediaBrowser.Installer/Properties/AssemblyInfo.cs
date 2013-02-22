fab627c845d980e46f1385f460c0c4d19fcabc86
LukePulverenti <luke.pulverenti@gmail.com>
2013-02-22 02:17:50 -0500

    added a master version file

--- MediaBrowser.Installer/Properties/AssemblyInfo.cs
+++ MediaBrowser.Installer/Properties/AssemblyInfo.cs
@@ -48,8 +48,3 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SharedVersion.cs
@@ -0,0 +1,3 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("3.0.*")]
