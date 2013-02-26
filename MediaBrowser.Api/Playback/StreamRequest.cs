9d68a7f1a9c10307a96ad72d5c9d8e157dfb02ba
LukePulverenti <luke.pulverenti@gmail.com>
2013-02-26 16:53:51 -0500

    added a StreamRequest object

---
+++ MediaBrowser.Api/Playback/StreamRequest.cs
@@ -0,0 +1,39 @@
+﻿using MediaBrowser.Model.Dto;
+
+namespace MediaBrowser.Api.Playback
+{
+    public class StreamRequest
+    {
+        public string Id { get; set; }
+
+        public AudioCodecs? AudioCodec { get; set; }
+
+        public long? StartTimeTicks { get; set; }
+
+        public int? AudioBitRate { get; set; }
+
+        public VideoCodecs? VideoCodec { get; set; }
+
+        public int? VideoBitRate { get; set; }
+
+        public int? AudioStreamIndex { get; set; }
+
+        public int? VideoStreamIndex { get; set; }
+
+        public int? SubtitleStreamIndex { get; set; }
+
+        public int? AudioChannels { get; set; }
+
+        public int? AudioSampleRate { get; set; }
+
+        public int? Width { get; set; }
+
+        public int? Height { get; set; }
+
+        public int? MaxWidth { get; set; }
+
+        public int? MaxHeight { get; set; }
+
+        public double? Framerate { get; set; }
+    }
+}
