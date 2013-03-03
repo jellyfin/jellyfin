2937583ee20c303540f194bd661f1a3fa973f30a
Eric Reed <ebr@mediabrowser3.com>
2013-03-03 15:55:30 -0500

    Incremental LastfmArtistProvider

---
+++ MediaBrowser.Controller/Providers/Music/LastfmArtistProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Serialization;
+
+namespace MediaBrowser.Controller.Providers.Music
+{
+    public class LastfmArtistProvider : LastfmBaseArtistProvider
+    {
+        public LastfmArtistProvider(IJsonSerializer jsonSerializer, IHttpClient httpClient, ILogManager logManager) 
+            : base(jsonSerializer, httpClient, logManager)
+        {
+        }
+
+        protected override Task FetchLastfmData(BaseItem item, string id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
