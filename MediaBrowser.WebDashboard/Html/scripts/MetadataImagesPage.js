62448129a66f50b6ac2874c6da514399f974340e
Eric Reed <ebr@mediabrowser3.com>
2013-03-04 11:59:35 -0500

    Config page changes for image options

--- MediaBrowser.WebDashboard/Html/scripts/MetadataImagesPage.js
+++ MediaBrowser.WebDashboard/Html/scripts/MetadataImagesPage.js
@@ -20,18 +20,24 @@
         $('#chkRefreshItemImages', page).checked(config.RefreshItemImages).checkboxradio("refresh");
         $('#txtNumbackdrops', page).val(config.MaxBackdrops);
 
-        $('#chkDownloadMovieArt', page).checked(config.DownloadMovieArt).checkboxradio("refresh");
-        $('#chkDownloadMovieBanner', page).checked(config.DownloadMovieBanner).checkboxradio("refresh");
-        $('#chkDownloadMovieDisc', page).checked(config.DownloadMovieDisc).checkboxradio("refresh");
-        $('#chkDownloadMovieLogo', page).checked(config.DownloadMovieLogo).checkboxradio("refresh");
-        $('#chkDownloadMovieThumb', page).checked(config.DownloadMovieThumb).checkboxradio("refresh");
-        $('#chKDownloadTVArt', page).checked(config.DownloadTVArt).checkboxradio("refresh");
-        $('#chkDownloadTVBanner', page).checked(config.DownloadTVBanner).checkboxradio("refresh");
-        $('#chkDownloadTVLogo', page).checked(config.DownloadTVLogo).checkboxradio("refresh");
-        $('#chkDownloadTVThumb', page).checked(config.DownloadTVThumb).checkboxradio("refresh");
-        $('#chkDownloadSeasonBanner', page).checked(config.DownloadTVSeasonBanner).checkboxradio("refresh");
-        $('#chkDownloadSeasonThumb', page).checked(config.DownloadTVSeasonThumb).checkboxradio("refresh");
-        $('#chkDownloadSeasonBackdrops', page).checked(config.DownloadTVSeasonBackdrops).checkboxradio("refresh");
+        $('#chkDownloadMovieArt', page).checked(config.DownloadMovieImages.Art).checkboxradio("refresh");
+        $('#chkDownloadMovieBanner', page).checked(config.DownloadMovieImages.Banner).checkboxradio("refresh");
+        $('#chkDownloadMovieDisc', page).checked(config.DownloadMovieImages.Disc).checkboxradio("refresh");
+        $('#chkDownloadMovieLogo', page).checked(config.DownloadMovieImages.Logo).checkboxradio("refresh");
+        $('#chkDownloadMovieThumb', page).checked(config.DownloadMovieImages.Thumb).checkboxradio("refresh");
+        $('#chKDownloadTVArt', page).checked(config.DownloadSeriesImages.Art).checkboxradio("refresh");
+        $('#chkDownloadTVBanner', page).checked(config.DownloadSeriesImages.Banner).checkboxradio("refresh");
+        $('#chkDownloadTVLogo', page).checked(config.DownloadSeriesImages.Logo).checkboxradio("refresh");
+        $('#chkDownloadTVThumb', page).checked(config.DownloadSeriesImages.Thumb).checkboxradio("refresh");
+        $('#chkDownloadSeasonBanner', page).checked(config.DownloadSeasonImages.Banner).checkboxradio("refresh");
+        $('#chkDownloadSeasonThumb', page).checked(config.DownloadSeasonImages.Thumb).checkboxradio("refresh");
+        $('#chkDownloadSeasonBackdrops', page).checked(config.DownloadSeasonImages.Backdrops).checkboxradio("refresh");
+        $('#chkDownloadArtistThumb', page).checked(config.DownloadMusicArtistImages.Thumb).checkboxradio("refresh");
+        $('#chkDownloadArtistBackdrops', page).checked(config.DownloadMusicArtistImages.Backdrops).checkboxradio("refresh");
+        $('#chkDownloadArtistLogo', page).checked(config.DownloadMusicArtistImages.Logo).checkboxradio("refresh");
+        $('#chkDownloadArtistBanner', page).checked(config.DownloadMusicArtistImages.Banner).checkboxradio("refresh");
+        $('#chkDownloadAlbumPrimary', page).checked(config.DownloadMusicAlbumImages.Primary).checkboxradio("refresh");
+        $('#chkDownloadAlbumBackdrops', page).checked(config.DownloadMusicAlbumImages.Backdrops).checkboxradio("refresh");
 
         Dashboard.hideLoadingMsg();
     },
@@ -50,18 +56,24 @@
             config.RefreshItemImages = $('#chkRefreshItemImages', form).checked();
             config.MaxBackdrops = $('#txtNumbackdrops', form).val();
 
-            config.DownloadMovieArt = $('#chkDownloadMovieArt', form).checked();
-            config.DownloadMovieBanner = $('#chkDownloadMovieBanner', form).checked();
-            config.DownloadMovieDisc = $('#chkDownloadMovieDisc', form).checked();
-            config.DownloadMovieLogo = $('#chkDownloadMovieLogo', form).checked();
-            config.DownloadMovieThumb = $('#chkDownloadMovieThumb', form).checked();
-            config.DownloadTVArt = $('#chKDownloadTVArt', form).checked();
-            config.DownloadTVBanner = $('#chkDownloadTVBanner', form).checked();
-            config.DownloadTVLogo = $('#chkDownloadTVLogo', form).checked();
-            config.DownloadTVThumb = $('#chkDownloadTVThumb', form).checked();
-            config.DownloadTVSeasonBanner = $('#chkDownloadSeasonBanner', form).checked();
-            config.DownloadTVSeasonThumb = $('#chkDownloadSeasonThumb', form).checked();
-            config.DownloadTVSeasonBackdrops = $('#chkDownloadSeasonBackdrops', form).checked();
+            config.DownloadMovieImages.Art = $('#chkDownloadMovieArt', form).checked();
+            config.DownloadMovieImages.Banner = $('#chkDownloadMovieBanner', form).checked();
+            config.DownloadMovieImages.Disc = $('#chkDownloadMovieDisc', form).checked();
+            config.DownloadMovieImages.Logo = $('#chkDownloadMovieLogo', form).checked();
+            config.DownloadMovieImages.Thumb = $('#chkDownloadMovieThumb', form).checked();
+            config.DownloadSeriesImages.Art = $('#chKDownloadTVArt', form).checked();
+            config.DownloadSeriesImages.Banner = $('#chkDownloadTVBanner', form).checked();
+            config.DownloadSeriesImages.Logo = $('#chkDownloadTVLogo', form).checked();
+            config.DownloadSeriesImages.Thumb = $('#chkDownloadTVThumb', form).checked();
+            config.DownloadSeasonImages.Banner = $('#chkDownloadSeasonBanner', form).checked();
+            config.DownloadSeasonImages.Thumb = $('#chkDownloadSeasonThumb', form).checked();
+            config.DownloadSeasonImages.Backdrops = $('#chkDownloadSeasonBackdrops', form).checked();
+            config.DownloadMusicArtistImages.Backdrops = $('#chkDownloadArtistBackdrops', form).checked();
+            config.DownloadMusicArtistImages.Logo = $('#chkDownloadArtistLogo', form).checked();
+            config.DownloadMusicArtistImages.Thumb = $('#chkDownloadArtistThumb', form).checked();
+            config.DownloadMusicArtistImages.Banner = $('#chkDownloadArtistBanner', form).checked();
+            config.DownloadMusicAlbumImages.Primary = $('#chkDownloadAlbumPrimary', form).checked();
+            config.DownloadMusicAlbumImages.Backdrops = $('#chkDownloadAlbumBackdrops', form).checked();
 
             ApiClient.updateServerConfiguration(config).done(Dashboard.processServerConfigurationUpdateResult);
         });
