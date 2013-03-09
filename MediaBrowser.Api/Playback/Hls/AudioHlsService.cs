9b3a97edfc1d37de37986b4bdac3263c9367bf8f
LukePulverenti <luke.pulverenti@gmail.com>
2013-03-09 01:05:52 -0500

    some progress towards brining back hls, but not finished yet

--- MediaBrowser.Api/Playback/Hls/AudioHlsService.cs
+++ MediaBrowser.Api/Playback/Hls/AudioHlsService.cs
@@ -2,10 +2,32 @@
 using MediaBrowser.Controller;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Model.Dto;
+using ServiceStack.ServiceHost;
 using System;
+using System.IO;
 
 namespace MediaBrowser.Api.Playback.Hls
 {
+    /// <summary>
+    /// Class GetHlsAudioStream
+    /// </summary>
+    [Route("/Audio/{Id}/stream.m3u8", "GET")]
+    [ServiceStack.ServiceHost.Api(Description = "Gets an audio stream using HTTP live streaming.")]
+    public class GetHlsAudioStream : StreamRequest
+    {
+
+    }
+
+    [Route("/Audio/{Id}/segments/{SegmentId}.mp3", "GET")]
+    [Route("/Audio/{Id}/segments/{SegmentId}.aac", "GET")]
+    [ServiceStack.ServiceHost.Api(Description = "Gets an Http live streaming segment file. Internal use only.")]
+    public class GetHlsAudioSegment
+    {
+        public string Id { get; set; }
+
+        public string SegmentId { get; set; }
+    }
+
     /// <summary>
     /// Class AudioHlsService
     /// </summary>
@@ -14,6 +36,25 @@ namespace MediaBrowser.Api.Playback.Hls
         public AudioHlsService(IServerApplicationPaths appPaths, IUserManager userManager, ILibraryManager libraryManager, IIsoManager isoManager)
             : base(appPaths, userManager, libraryManager, isoManager)
         {
+        }
+
+        public object Get(GetHlsAudioSegment request)
+        {
+            var file = SegmentFilePrefix + request.SegmentId + Path.GetExtension(Request.PathInfo);
+
+            file = Path.Combine(ApplicationPaths.EncodedMediaCachePath, file);
+
+            return ToStaticFileResult(file);
+        }
+
+        /// <summary>
+        /// Gets the specified request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>System.Object.</returns>
+        public object Get(GetHlsAudioStream request)
+        {
+            return ProcessRequest(request);
         }
 
         /// <summary>
@@ -75,7 +116,7 @@ namespace MediaBrowser.Api.Playback.Hls
                 return ".mp3";
             }
 
-            throw new InvalidOperationException("Only aac and mp3 audio codecs are supported.");
+            throw new ArgumentException("Must specify either aac or mp3 audio codec.");
         }
 
         /// <summary>
