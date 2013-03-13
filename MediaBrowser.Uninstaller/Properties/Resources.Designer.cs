84eb69ce8754a933ff5d6db9c7c3cbe78952de19
Eric Reed <ebr@mediabrowser3.com>
2013-03-13 10:00:49 -0400

    Consolidate uninstaller into one assembly and don't allow run directly

--- MediaBrowser.Uninstaller/Properties/Resources.Designer.cs
+++ MediaBrowser.Uninstaller/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MediaBrowser.Uninstaller.Execute.Properties {
+namespace MediaBrowser.Uninstaller.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MediaBrowser.Uninstaller.Execute.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MediaBrowser.Uninstaller.Execute.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MediaBrowser.Uninstaller.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
