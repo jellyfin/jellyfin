2f64303dcdc3c0a550c66c16584d330bf8c3c174
Eric Reed <ebr@mediabrowser3.com>
2013-03-13 17:52:56 -0400

    Attempt to fix in-place updates #52

--- MediaBrowser.Common.Implementations/Updates/ApplicationUpdater.cs
+++ MediaBrowser.Common.Implementations/Updates/ApplicationUpdater.cs
@@ -21,10 +21,15 @@ namespace MediaBrowser.Common.Implementations.Updates
             // Use our installer passing it the specific archive
             // We need to copy to a temp directory and execute it there
             var source = Path.Combine(appPaths.ProgramSystemPath, UpdaterExe);
-            var target = Path.Combine(Path.GetTempPath(), UpdaterExe);
+            var tempUpdater = Path.Combine(Path.GetTempPath(), UpdaterExe);
             var product = app == MBApplication.MBTheater ? "mbt" : "server";
-            File.Copy(source, target, true);
-            Process.Start(target, string.Format("product={0} archive=\"{1}\" caller={2} pismo=false", product, archive, Process.GetCurrentProcess().Id));
+            File.Copy(source, tempUpdater, true);
+            // Our updater needs SS and ionic
+            source = Path.Combine(appPaths.ProgramSystemPath, "ServiceStack.Text.dll");
+            File.Copy(source, Path.Combine(Path.GetTempPath(), "ServiceStack.Text.dll"), true);
+            source = Path.Combine(appPaths.ProgramSystemPath, "Ionic.Zip.dll");
+            File.Copy(source, Path.Combine(Path.GetTempPath(), "Ionic.Zip.dll"), true);
+            Process.Start(tempUpdater, string.Format("product={0} archive=\"{1}\" caller={2} pismo=false", product, archive, Process.GetCurrentProcess().Id));
 
             // That's it.  The installer will do the work once we exit
         }
