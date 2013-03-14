a7f06c84b14ea79ce5af8e928597f6fd119088a3
Eric Reed <ebr@mediabrowser3.com>
2013-03-14 12:40:36 -0400

    Hook new registration load to changing supporter keys

--- MediaBrowser.Common/Security/IRequiresRegistration.cs
+++ MediaBrowser.Common/Security/IRequiresRegistration.cs
@@ -4,6 +4,12 @@ namespace MediaBrowser.Common.Security
 {
     public interface IRequiresRegistration
     {
+        /// <summary>
+        /// Load all registration information required for this entity.
+        /// Your class should re-load all MBRegistrationRecords when this is called even if they were
+        /// previously loaded.
+        /// </summary>
+        /// <returns></returns>
         Task LoadRegistrationInfoAsync();
     }
 }
