e25258e1c062865ad50cbffb9e31284068c36811
LukePulverenti <luke.pulverenti@gmail.com>
2013-03-20 12:54:32 -0400

    moved playback event args object

--- MediaBrowser.Controller/Library/PlaybackProgressEventArgs.cs
+++ MediaBrowser.Controller/Library/PlaybackProgressEventArgs.cs
@@ -1,13 +1,15 @@
-﻿using MediaBrowser.Common.Events;
+﻿using System;
+using MediaBrowser.Controller.Entities;
 
-namespace MediaBrowser.Controller.Entities
+namespace MediaBrowser.Controller.Library
 {
     /// <summary>
     /// Holds information about a playback progress event
     /// </summary>
-    public class PlaybackProgressEventArgs : GenericEventArgs<BaseItem>
+    public class PlaybackProgressEventArgs : EventArgs
     {
         public User User { get; set; }
         public long? PlaybackPositionTicks { get; set; }
+        public BaseItem Item { get; set; }
     }
 }
