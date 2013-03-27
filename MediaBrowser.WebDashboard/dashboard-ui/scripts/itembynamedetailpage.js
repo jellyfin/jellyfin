d1e129804f0c0b1ce9831aaa39806dc490405dc2
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-03-27 18:29:37 -0400

    add stub ibn page

---
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/itembynamedetailpage.js
@@ -0,0 +1,39 @@
+﻿var ItemByNameDetailPage = {
+
+    onPageShow: function () {
+        ItemByNameDetailPage.reload();
+    },
+
+    reload: function () {
+
+        Dashboard.showLoadingMsg();
+
+        var person = getParameterByName('person');
+
+        if (person) {
+            ApiClient.getPerson(person).done(ItemByNameDetailPage.renderItem);
+            return;
+        }
+
+        var studio = getParameterByName('studio');
+
+        if (studio) {
+            ApiClient.getStudio(person).done(ItemByNameDetailPage.renderItem);
+            return;
+        }
+
+        var genre = getParameterByName('genre');
+
+        if (genre) {
+            ApiClient.getGenre(genre).done(ItemByNameDetailPage.renderItem);
+            return;
+        }
+    },
+
+    renderItem: function (item) {
+
+        Dashboard.hideLoadingMsg();
+    }
+};
+
+$(document).on('pageshow', "#itemByNameDetailPage", ItemByNameDetailPage.onPageShow);
