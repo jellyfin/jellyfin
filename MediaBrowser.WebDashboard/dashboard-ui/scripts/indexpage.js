68d902a69d5a44c2d4d64a77274ec98494a3f697
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-01 14:55:56 -0400

    better dashboard background and text adjustments

--- MediaBrowser.WebDashboard/dashboard-ui/scripts/indexpage.js
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/indexpage.js
@@ -5,7 +5,7 @@
         var page = this;
 
         var userId = Dashboard.getCurrentUserId();
-        
+
         if (!userId) {
             return;
         }
@@ -25,6 +25,27 @@
             }));
 
         });
+
+        var views = [
+            { name: "Movies", url: "moviesrecommended.html", img: "css/images/items/list/chapter.png", background: "#E12026" },
+            { name: "TV Shows", url: "tvrecommended.html", img: "css/images/items/list/collection.png", background: "#FF870F" },
+            { name: "Music", url: "musicrecommended.html", img: "css/images/items/list/audiowide.png", background: "#4BB3DD" }
+        ];
+
+        var html = '';
+
+        for (var i = 0, length = views.length; i < length; i++) {
+
+            var view = views[i];
+
+            html += '<div class="posterViewItem">';
+            html += '<a href="' + view.url + '">';
+            html += '<img style="background: ' + view.background + ';" src="' + view.img + '"><div class="posterViewItemText">' + view.name + '</div>';
+            html += '</a>';
+            html += '</div>';
+        }
+
+        $('#views', page).html(html);
     });
 
 })(jQuery, document);
