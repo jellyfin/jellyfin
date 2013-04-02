a10b025e8f912f3e5af31f622a15eb0d2620fd42
Techywarrior <techywarrior@gmail.com>
2013-04-01 18:28:01 -0700

    TV recommended page

--- MediaBrowser.WebDashboard/dashboard-ui/scripts/tvrecommended.js
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/tvrecommended.js
@@ -1,1 +1,56 @@
-﻿
+﻿(function ($, document) {
+
+	$(document).on('pageshow', "#tvRecommendedPage", function () {
+
+		var page = this;
+
+		var options = {
+
+			SortBy: "DateCreated",
+			SortOrder: "Descending",
+			IncludeItemTypes: "Episode",
+			Limit: 5,
+			Recursive: true,
+			Fields: "PrimaryImageAspectRatio"
+		};
+
+		ApiClient.getItems(Dashboard.getCurrentUserId(), options).done(function (result) {
+
+			$('#recentlyAddedItems', page).html(Dashboard.getPosterViewHtml({
+				items: result.Items,
+				useAverageAspectRatio: true
+			}));
+
+		});
+
+
+		options = {
+
+			SortBy: "DatePlayed",
+			SortOrder: "Descending",
+			IncludeItemTypes: "Episode",
+			Filters: "IsResumable",
+			Limit: 5,
+			Recursive: true,
+			Fields: "PrimaryImageAspectRatio"
+		};
+
+		ApiClient.getItems(Dashboard.getCurrentUserId(), options).done(function (result) {
+
+			if (result.Items.length) {
+				$('#resumableSection', page).show();
+			} else {
+				$('#resumableSection', page).hide();
+			}
+
+			$('#resumableItems', page).html(Dashboard.getPosterViewHtml({
+				items: result.Items,
+				useAverageAspectRatio: true
+			}));
+
+		});
+
+	});
+
+
+})(jQuery, document);
