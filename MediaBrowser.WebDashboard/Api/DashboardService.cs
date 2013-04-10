ed97646c05b7e405544c9018a5d14fb0c6056445
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-10 15:33:19 -0400

    use new script format

--- MediaBrowser.WebDashboard/Api/DashboardService.cs
+++ MediaBrowser.WebDashboard/Api/DashboardService.cs
@@ -17,7 +17,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MediaBrowser.WebDashboard.Api
