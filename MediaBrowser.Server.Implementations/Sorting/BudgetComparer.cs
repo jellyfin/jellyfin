9f3c54d0c0a65e64ec82812df7382388973a6bd2
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-12 10:32:04 -0400

    Added budget as a sort order

---
+++ MediaBrowser.Server.Implementations/Sorting/BudgetComparer.cs
@@ -0,0 +1,29 @@
+﻿using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Sorting;
+using MediaBrowser.Model.Querying;
+
+namespace MediaBrowser.Server.Implementations.Sorting
+{
+    public class BudgetDateComparer : IBaseItemComparer
+    {
+        /// <summary>
+        /// Compares the specified x.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>System.Int32.</returns>
+        public int Compare(BaseItem x, BaseItem y)
+        {
+            return (x.Budget ?? 0).CompareTo(y.Budget ?? 0);
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name
+        {
+            get { return ItemSortBy.Budget; }
+        }
+    }
+}
