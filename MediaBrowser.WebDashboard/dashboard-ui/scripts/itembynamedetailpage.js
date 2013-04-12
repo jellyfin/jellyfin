a9d744cdab671f5f5bbd19081b781ab4c73c68f1
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-12 14:22:40 -0400

    display new fields on person page

--- MediaBrowser.WebDashboard/dashboard-ui/scripts/itembynamedetailpage.js
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/itembynamedetailpage.js
@@ -52,6 +52,33 @@
 
         renderUserDataIcons(page, item);
         renderLinks(page, item);
+
+        if (item.Type == "Person" && item.PremiereDate) {
+
+            var birthday = parseISO8601Date(item.PremiereDate, { toLocal: true }).toDateString();
+
+            $('#itemBirthday', page).show().html("Birthday:&nbsp;&nbsp;" + birthday);
+        } else {
+            $('#itemBirthday', page).hide();
+        }
+
+        if (item.Type == "Person" && item.EndDate) {
+
+            var deathday = parseISO8601Date(item.EndDate, { toLocal: true }).toDateString();
+
+            $('#itemDeathDate', page).show().html("Death day:&nbsp;&nbsp;" + deathday);
+        } else {
+            $('#itemDeathDate', page).hide();
+        }
+
+        if (item.Type == "Person" && item.ProductionLocations && item.ProductionLocations.length) {
+
+            var gmap = '<a target="_blank" href="https://maps.google.com/maps?q=' + item.ProductionLocations[0] + '">' + item.ProductionLocations[0] + '</a>';
+
+            $('#itemBirthLocation', page).show().html("Birth place:&nbsp;&nbsp;" + gmap).trigger('create');
+        } else {
+            $('#itemBirthLocation', page).hide();
+        }
     }
 
     function renderLinks(page, item) {
