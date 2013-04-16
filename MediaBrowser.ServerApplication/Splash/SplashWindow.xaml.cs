14dd0426f57b529094c7d0da86bc611ce1506098
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-16 12:45:28 -0400

    added splash page

---
+++ MediaBrowser.ServerApplication/Splash/SplashWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MediaBrowser.ServerApplication.Splash
+{
+    /// <summary>
+    /// Interaction logic for SplashWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
