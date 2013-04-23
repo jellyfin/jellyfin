6d5d722bc6c259fbdf6cb9b6df839e5dea2177d0
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-23 17:05:21 -0400

    updated video js

--- MediaBrowser.WebDashboard/Api/DashboardService.cs
+++ MediaBrowser.WebDashboard/Api/DashboardService.cs
@@ -398,7 +398,7 @@ namespace MediaBrowser.WebDashboard.Api
             var files = new[]
                             {
                                 "http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css",
-                                "thirdparty/video-js/video-js.min.css",
+                                "thirdparty/video-js-3.2.0/video-js.min.css",
                                 "thirdparty/jqm-icon-pack-3.0/font-awesome/jqm-icon-pack-3.0.0-fa.css",
                                 "css/all.css" + versionString
                             };
@@ -421,7 +421,7 @@ namespace MediaBrowser.WebDashboard.Api
                             {
                                 "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js", 
                                 "http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js",
-                                "thirdparty/video-js/video.min.js",
+                                "thirdparty/video-js-3.2.0/video.min.js",
                                 "scripts/all.js" + versionString
             };
 
