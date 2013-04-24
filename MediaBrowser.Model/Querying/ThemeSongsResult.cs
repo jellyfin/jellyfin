191befa7888bae6c4f4265200aa2bc0e1e96fbf1
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-24 12:37:12 -0400

    improve theme song methods

---
+++ MediaBrowser.Model/Querying/ThemeSongsResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MediaBrowser.Model.Querying
+{
+    /// <summary>
+    /// Class ThemeSongsResult
+    /// </summary>
+    public class ThemeSongsResult : ItemsResult
+    {
+        /// <summary>
+        /// Gets or sets the owner id.
+        /// </summary>
+        /// <value>The owner id.</value>
+        public string OwnerId { get; set; }
+    }
+}
