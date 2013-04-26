f9b5eb5b8aa53d986da712fb71815051540a95a8
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-26 15:20:53 -0400

    Added search hint service

--- MediaBrowser.Controller/Library/ILibrarySearchEngine.cs
+++ MediaBrowser.Controller/Library/ILibrarySearchEngine.cs
@@ -1,5 +1,6 @@
 ﻿using MediaBrowser.Controller.Entities;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MediaBrowser.Controller.Library
 {
@@ -16,5 +17,13 @@ namespace MediaBrowser.Controller.Library
         /// <returns>IEnumerable{BaseItem}.</returns>
         /// <exception cref="System.ArgumentNullException">searchTerm</exception>
         IEnumerable<BaseItem> Search(IEnumerable<BaseItem> items, string searchTerm);
+
+        /// <summary>
+        /// Gets the search hints.
+        /// </summary>
+        /// <param name="inputItems">The input items.</param>
+        /// <param name="searchTerm">The search term.</param>
+        /// <returns>Task{IEnumerable{BaseItem}}.</returns>
+        Task<IEnumerable<BaseItem>> GetSearchHints(IEnumerable<BaseItem> inputItems, string searchTerm);
     }
 }
