acc50cc36c38a3bb78cdad49935c5d8b3a6ff74c
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-04-29 22:00:42 -0400

    new boxset filters

--- MediaBrowser.WebDashboard/dashboard-ui/scripts/boxsets.js
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/boxsets.js
@@ -1,120 +1,148 @@
 ﻿(function ($, document) {
 
-	// The base query options
-	var query = {
+    // The base query options
+    var query = {
 
-		SortBy: "SortName",
-		SortOrder: "Ascending",
-		IncludeItemTypes: "BoxSet",
-		Recursive: true,
-		Fields: "DisplayMediaType,ItemCounts,DateCreated,UserData",
-		Limit: LibraryBrowser.getDetaultPageSize(),
-		StartIndex: 0
-	};
+        SortBy: "SortName",
+        SortOrder: "Ascending",
+        IncludeItemTypes: "BoxSet",
+        Recursive: true,
+        Fields: "DisplayMediaType,ItemCounts,DateCreated,UserData",
+        Limit: LibraryBrowser.getDetaultPageSize(),
+        StartIndex: 0
+    };
 
 
-	function reloadItems(page) {
+    function reloadItems(page) {
 
-		Dashboard.showLoadingMsg();
+        Dashboard.showLoadingMsg();
 
-		ApiClient.getItems(Dashboard.getCurrentUserId(), query).done(function (result) {
+        ApiClient.getItems(Dashboard.getCurrentUserId(), query).done(function (result) {
 
-		    var html = '';
+            var html = '';
 
-		    $('.listTopPaging', page).html(LibraryBrowser.getPagingHtml(query, result.TotalRecordCount, true)).trigger('create');
+            $('.listTopPaging', page).html(LibraryBrowser.getPagingHtml(query, result.TotalRecordCount, true)).trigger('create');
 
-		    html += LibraryBrowser.getPosterDetailViewHtml({
-		        items: result.Items,
-		        context: "movies"
-		    });
+            html += LibraryBrowser.getPosterDetailViewHtml({
+                items: result.Items,
+                context: "movies"
+            });
 
-		    html += LibraryBrowser.getPagingHtml(query, result.TotalRecordCount);
+            html += LibraryBrowser.getPagingHtml(query, result.TotalRecordCount);
 
-		    $('#items', page).html(html).trigger('create');
+            $('#items', page).html(html).trigger('create');
 
-		    $('.selectPage', page).on('change', function () {
-		        query.StartIndex = (parseInt(this.value) - 1) * query.Limit;
-		        reloadItems(page);
-		    });
+            $('.selectPage', page).on('change', function () {
+                query.StartIndex = (parseInt(this.value) - 1) * query.Limit;
+                reloadItems(page);
+            });
 
-		    $('.btnNextPage', page).on('click', function () {
-		        query.StartIndex += query.Limit;
-		        reloadItems(page);
-		    });
+            $('.btnNextPage', page).on('click', function () {
+                query.StartIndex += query.Limit;
+                reloadItems(page);
+            });
 
-		    $('.btnPreviousPage', page).on('click', function () {
-		        query.StartIndex -= query.Limit;
-		        reloadItems(page);
-		    });
+            $('.btnPreviousPage', page).on('click', function () {
+                query.StartIndex -= query.Limit;
+                reloadItems(page);
+            });
 
-		    $('.selectPageSize', page).on('change', function () {
-		        query.Limit = parseInt(this.value);
-		        query.StartIndex = 0;
-		        reloadItems(page);
-		    });
+            $('.selectPageSize', page).on('change', function () {
+                query.Limit = parseInt(this.value);
+                query.StartIndex = 0;
+                reloadItems(page);
+            });
 
-		    Dashboard.hideLoadingMsg();
-		});
-	}
+            Dashboard.hideLoadingMsg();
+        });
+    }
 
-	$(document).on('pageinit', "#boxsetsPage", function () {
+    $(document).on('pageinit', "#boxsetsPage", function () {
 
-		var page = this;
+        var page = this;
 
-		$('.radioSortBy', this).on('click', function () {
-			query.SortBy = this.getAttribute('data-sortby');
-			reloadItems(page);
-		});
+        $('.radioSortBy', this).on('click', function () {
+            query.SortBy = this.getAttribute('data-sortby');
+            reloadItems(page);
+        });
 
-		$('.radioSortOrder', this).on('click', function () {
-			query.SortOrder = this.getAttribute('data-sortorder');
-			reloadItems(page);
-		});
+        $('.radioSortOrder', this).on('click', function () {
+            query.SortOrder = this.getAttribute('data-sortorder');
+            reloadItems(page);
+        });
 
-		$('.chkStandardFilter', this).on('change', function () {
+        $('.chkStandardFilter', this).on('change', function () {
 
-			var filterName = this.getAttribute('data-filter');
-			var filters = query.Filters || "";
+            var filterName = this.getAttribute('data-filter');
+            var filters = query.Filters || "";
 
-			filters = (',' + filters).replace(',' + filterName, '').substring(1);
+            filters = (',' + filters).replace(',' + filterName, '').substring(1);
 
-			if (this.checked) {
-				filters = filters ? (filters + ',' + filterName) : filterName;
-			}
+            if (this.checked) {
+                filters = filters ? (filters + ',' + filterName) : filterName;
+            }
 
-			query.StartIndex = 0;
-			query.Filters = filters;
+            query.StartIndex = 0;
+            query.Filters = filters;
 
-			reloadItems(page);
-		});
+            reloadItems(page);
+        });
 
-	}).on('pagebeforeshow', "#boxsetsPage", function () {
+        $('#chkTrailer', this).on('change', function () {
 
-	    reloadItems(this);
+            query.StartIndex = 0;
+            query.HasTrailer = this.checked ? true : null;
 
-	}).on('pageshow', "#boxsetsPage", function () {
+            reloadItems(page);
+        });
 
-			// Reset form values using the last used query
-			$('.radioSortBy', this).each(function () {
+        $('#chkThemeSong', this).on('change', function () {
 
-				this.checked = query.SortBy == this.getAttribute('data-sortby');
+            query.StartIndex = 0;
+            query.HasThemeSong = this.checked ? true : null;
 
-			}).checkboxradio('refresh');
+            reloadItems(page);
+        });
 
-			$('.radioSortOrder', this).each(function () {
+        $('#chkThemeVideo', this).on('change', function () {
 
-				this.checked = query.SortOrder == this.getAttribute('data-sortorder');
+            query.StartIndex = 0;
+            query.HasThemeVideo = this.checked ? true : null;
 
-			}).checkboxradio('refresh');
+            reloadItems(page);
+        });
 
-			$('.chkStandardFilter', this).each(function () {
+    }).on('pagebeforeshow', "#boxsetsPage", function () {
 
-				var filters = "," + (query.Filters || "");
-				var filterName = this.getAttribute('data-filter');
+        reloadItems(this);
 
-				this.checked = filters.indexOf(',' + filterName) != -1;
+    }).on('pageshow', "#boxsetsPage", function () {
 
-			}).checkboxradio('refresh');
-		});
+        // Reset form values using the last used query
+        $('.radioSortBy', this).each(function () {
+
+            this.checked = query.SortBy == this.getAttribute('data-sortby');
+
+        }).checkboxradio('refresh');
+
+        $('.radioSortOrder', this).each(function () {
+
+            this.checked = query.SortOrder == this.getAttribute('data-sortorder');
+
+        }).checkboxradio('refresh');
+
+        $('.chkStandardFilter', this).each(function () {
+
+            var filters = "," + (query.Filters || "");
+            var filterName = this.getAttribute('data-filter');
+
+            this.checked = filters.indexOf(',' + filterName) != -1;
+
+        }).checkboxradio('refresh');
+
+        $('#chkTrailer', this).checked(query.HasTrailer == true).checkboxradio('refresh');
+        $('#chkThemeSong', this).checked(query.HasThemeSong == true).checkboxradio('refresh');
+        $('#chkThemeVideo', this).checked(query.HasThemeVideo == true).checkboxradio('refresh');
+    });
 
 })(jQuery, document);
