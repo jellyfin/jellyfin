4c7d0ee808146d4daa70ea85e1e44989be6db52d
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-05-11 00:10:58 -0400

    added CriticReviews api endpoint

---
+++ MediaBrowser.Model/Querying/ItemReviewsResult.cs
@@ -0,0 +1,30 @@
+﻿using MediaBrowser.Model.Entities;
+
+namespace MediaBrowser.Model.Querying
+{
+    /// <summary>
+    /// Class ItemReviewsResult
+    /// </summary>
+    public class ItemReviewsResult
+    {
+        /// <summary>
+        /// Gets or sets the item reviews.
+        /// </summary>
+        /// <value>The item reviews.</value>
+        public ItemReview[] ItemReviews { get; set; }
+
+        /// <summary>
+        /// The total number of records available
+        /// </summary>
+        /// <value>The total record count.</value>
+        public int TotalRecordCount { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ItemsResult" /> class.
+        /// </summary>
+        public ItemReviewsResult()
+        {
+            ItemReviews = new ItemReview[] { };
+        }
+   }
+}
