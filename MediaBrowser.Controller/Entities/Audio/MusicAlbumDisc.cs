57d7e9fccc1434d51876ab9baf23c20c80140706
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-05-12 10:06:08 -0400

    fixes #268 - (Multicd) Albums with discnumbers in subfolders on filesystem = Multiple album objects

---
+++ MediaBrowser.Controller/Entities/Audio/MusicAlbumDisc.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MediaBrowser.Controller.Entities.Audio
+{
+    /// <summary>
+    /// Class MusicAlbumDisc
+    /// </summary>
+    public class MusicAlbumDisc : Folder
+    {
+        /// <summary>
+        /// Gets or sets the display type of the media.
+        /// </summary>
+        /// <value>The display type of the media.</value>
+        public override string DisplayMediaType
+        {
+            get
+            {
+                return "Disc";
+            }
+            set
+            {
+                base.DisplayMediaType = value;
+            }
+        }
+    }
+}
