7a0aebf7f861319f168b52693638ddfaa17d43c6
Luis <ruisu15@gmail.com>
2013-05-20 01:45:54 -0400

    Fix move condition

--- MediaBrowser.Api/Library/LibraryHelpers.cs
+++ MediaBrowser.Api/Library/LibraryHelpers.cs
@@ -77,12 +77,18 @@ namespace MediaBrowser.Api.Library
 
             if (!string.Equals(currentPath, newPath, StringComparison.OrdinalIgnoreCase) && Directory.Exists(newPath))
             {
+                throw new ArgumentException("There is already a media collection with the name " + newPath + ".");
+            }
+            //Only make a two-phase move when changing capitalization
+            if (string.Equals(currentPath, newPath, StringComparison.OrdinalIgnoreCase))
+            {
                 //Create an unique name
                 var temporaryName = Guid.NewGuid().ToString();
                 var temporaryPath = Path.Combine(rootFolderPath, temporaryName);
                 Directory.Move(currentPath,temporaryPath);
                 currentPath = temporaryPath;
             }
+
             Directory.Move(currentPath, newPath);
         }
 
