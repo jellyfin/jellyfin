dd61a2cdf73fcccf1c8507e9288726791d16aaef
Luis Almanzar <ruisu15@gmail.com>
2013-06-09 16:51:57 -0300

    Don't save date if is the default min value
    Avoid empty dates to be saved

--- MediaBrowser.Api/LibraryService.cs
+++ MediaBrowser.Api/LibraryService.cs
@@ -258,8 +258,8 @@ namespace MediaBrowser.Api
             item.Studios = request.Studios.Select(x=>x.Name).ToList();
             item.People = request.People.Select(x=> new PersonInfo{Name = x.Name,Role = x.Role,Type = x.Type}).ToList();
 
-            item.EndDate = request.EndDate;
-            item.PremiereDate = request.PremiereDate;
+            item.EndDate = request.EndDate != default(DateTime) ? request.EndDate : null;
+            item.PremiereDate = request.PremiereDate != default(DateTime)? request.PremiereDate : null;
             item.ProductionYear = request.ProductionYear;
             item.AspectRatio = request.AspectRatio;
             item.Language = request.Language;
