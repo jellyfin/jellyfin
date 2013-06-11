150c74ece1b4d91c079502081da4aee67e82b7d5
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-06-11 10:06:08 -0400

    fixes #335 - Tasks Stalled

--- MediaBrowser.Providers/Movies/FanArtMovieUpdatesPrescanTask.cs
+++ MediaBrowser.Providers/Movies/FanArtMovieUpdatesPrescanTask.cs
@@ -130,18 +130,7 @@ namespace MediaBrowser.Providers.Movies
 
             foreach (var id in list)
             {
-                try
-                {
-                    await UpdateMovie(id, moviesDataPath, cancellationToken).ConfigureAwait(false);
-                }
-                catch (HttpException ex)
-                {
-                    // Already logged at lower levels, but don't fail the whole operation, unless something other than a timeout
-                    if (!ex.IsTimedOut)
-                    {
-                        throw;
-                    }
-                }
+                await UpdateMovie(id, moviesDataPath, cancellationToken).ConfigureAwait(false);
 
                 numComplete++;
                 double percent = numComplete;
