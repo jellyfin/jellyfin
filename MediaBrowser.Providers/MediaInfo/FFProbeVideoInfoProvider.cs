6df1ef5c6b0ef5b6411efb629540863da9c8b442
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-06-14 08:19:22 -0400

    Allow two character language code for subtitles

--- MediaBrowser.Providers/MediaInfo/FFProbeVideoInfoProvider.cs
+++ MediaBrowser.Providers/MediaInfo/FFProbeVideoInfoProvider.cs
@@ -345,7 +345,7 @@ namespace MediaBrowser.Providers.MediaInfo
                     // Try to translate to three character code
                     // Be flexible and check against both the full and three character versions
                     var culture = _localization.GetCultures()
-                        .FirstOrDefault(i => string.Equals(i.DisplayName, language, StringComparison.OrdinalIgnoreCase) || string.Equals(i.Name, language, StringComparison.OrdinalIgnoreCase) || string.Equals(i.ThreeLetterISOLanguageName, language, StringComparison.OrdinalIgnoreCase));
+                        .FirstOrDefault(i => string.Equals(i.DisplayName, language, StringComparison.OrdinalIgnoreCase) || string.Equals(i.Name, language, StringComparison.OrdinalIgnoreCase) || string.Equals(i.ThreeLetterISOLanguageName, language, StringComparison.OrdinalIgnoreCase) || string.Equals(i.TwoLetterISOLanguageName, language, StringComparison.OrdinalIgnoreCase));
 
                     if (culture != null)
                     {
