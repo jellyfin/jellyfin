3a0b23f527d3dce370588a979c4d3553f5f76a3a
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-06-16 21:48:55 -0400

    added next up query

---
+++ MediaBrowser.Model/Querying/NextUpQuery.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MediaBrowser.Model.Querying
+{
+    public class NextUpQuery
+    {
+        /// <summary>
+        /// Gets or sets the user id.
+        /// </summary>
+        /// <value>The user id.</value>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Skips over a given number of items within the results. Use for paging.
+        /// </summary>
+        /// <value>The start index.</value>
+        public int? StartIndex { get; set; }
+
+        /// <summary>
+        /// The maximum number of items to return
+        /// </summary>
+        /// <value>The limit.</value>
+        public int? Limit { get; set; }
+
+        /// <summary>
+        /// Fields to return within the items, in addition to basic information
+        /// </summary>
+        /// <value>The fields.</value>
+        public ItemFields[] Fields { get; set; }
+    }
+}
