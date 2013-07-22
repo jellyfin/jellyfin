cf953adcaca45b575bc6009275d3f71c65bb1468
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-07-22 13:07:39 -0400

    Added IsInMixedFolder for game

--- MediaBrowser.Controller/Entities/Game.cs
+++ MediaBrowser.Controller/Entities/Game.cs
@@ -23,5 +23,43 @@ namespace MediaBrowser.Controller.Entities
         /// </summary>
         /// <value>The game system.</value>
         public string GameSystem { get; set; }
+
+        /// <summary>
+        /// Returns true if the game is combined with other games in the same folder
+        /// </summary>
+        public bool IsInMixedFolder { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override string MetaLocation
+        {
+            get
+            {
+                var directoryName = System.IO.Path.GetDirectoryName(Path);
+
+                if (IsInMixedFolder)
+                {
+                    // It's a file
+                    var baseMetaPath = System.IO.Path.Combine(directoryName, "metadata");
+                    var fileName = System.IO.Path.GetFileNameWithoutExtension(Path);
+
+                    return fileName != null ? System.IO.Path.Combine(baseMetaPath, fileName) : null;
+                }
+
+                return directoryName;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override bool UseParentPathToCreateResolveArgs
+        {
+            get
+            {
+                return !IsInMixedFolder;
+            }
+        }
     }
 }
