582edde742d8c694a90fc6bd93a603245ae1358e
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-08-12 15:13:36 -0400

    added cancellation token to get image stream

---
+++ MediaBrowser.Model/ApiClient/ApiClientExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediaBrowser.Model.ApiClient
+{
+    /// <summary>
+    /// Class ApiClientExtensions
+    /// </summary>
+    public static class ApiClientExtensions
+    {
+        /// <summary>
+        /// Gets the image stream async.
+        /// </summary>
+        /// <param name="apiClient">The API client.</param>
+        /// <param name="url">The URL.</param>
+        /// <returns>Task{Stream}.</returns>
+        public static Task<Stream> GetImageStreamAsync(this IApiClient apiClient, string url)
+        {
+            return apiClient.GetImageStreamAsync(url, CancellationToken.None);
+        }
+    }
+}
