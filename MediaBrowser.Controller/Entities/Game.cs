45f2861d0263d7d48becdd8062b79a3b8033833f
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-08-15 15:09:52 -0400

    completed multiple movie in folder support

--- MediaBrowser.Controller/Entities/Game.cs
+++ MediaBrowser.Controller/Entities/Game.cs
@@ -25,29 +25,13 @@ namespace MediaBrowser.Controller.Entities
         public string GameSystem { get; set; }
 
         /// <summary>
-        /// Returns true if the game is combined with other games in the same folder
-        /// </summary>
-        public bool IsInMixedFolder { get; set; }
-
-        /// <summary>
         /// 
         /// </summary>
         public override string MetaLocation
         {
             get
             {
-                var directoryName = System.IO.Path.GetDirectoryName(Path);
-
-                if (IsInMixedFolder)
-                {
-                    // It's a file
-                    var baseMetaPath = System.IO.Path.Combine(directoryName, "metadata");
-                    var fileName = System.IO.Path.GetFileNameWithoutExtension(Path);
-
-                    return fileName != null ? System.IO.Path.Combine(baseMetaPath, fileName) : null;
-                }
-
-                return directoryName;
+                return System.IO.Path.GetDirectoryName(Path);
             }
         }
 
