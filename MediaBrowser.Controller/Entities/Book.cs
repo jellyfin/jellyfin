d0f3262ba3afbe33aaf97e6744764485a9ef44f6
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-08-30 19:54:49 -0400

    Added Book

---
+++ MediaBrowser.Controller/Entities/Book.cs
@@ -0,0 +1,35 @@
+﻿
+namespace MediaBrowser.Controller.Entities
+{
+    public class Book : BaseItem
+    {
+        public override string MediaType
+        {
+            get
+            {
+                return Model.Entities.MediaType.Book;
+            }
+        }
+
+        public string SeriesName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override string MetaLocation
+        {
+            get
+            {
+                return System.IO.Path.GetDirectoryName(Path);
+            }
+        }
+
+        protected override bool UseParentPathToCreateResolveArgs
+        {
+            get
+            {
+                return !IsInMixedFolder;
+            }
+        }
+    }
+}
