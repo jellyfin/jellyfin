685502b2e9fe30dea721162b8a34c3dcc8099a1a
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-09-09 23:13:12 -0400

    created custom endpoints for the mbt default theme

---
+++ MediaBrowser.Api/DefaultTheme/MoviesView.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MediaBrowser.Api.DefaultTheme
+{
+    public class MoviesView
+    {
+        public ItemStub[] SpotlightItems { get; set; }
+        public ItemStub[] BackdropItems { get; set; }
+        public ItemStub[] MovieItems { get; set; }
+        public ItemStub[] PeopleItems { get; set; }
+
+        public ItemStub[] BoxSetItems { get; set; }
+        public ItemStub[] TrailerItems { get; set; }
+        public ItemStub[] HDItems { get; set; }
+        public ItemStub[] ThreeDItems { get; set; }
+    }
+}
---
+++ MediaBrowser.Api/DefaultTheme/TvView.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MediaBrowser.Api.DefaultTheme
+{
+    public class TvView
+    {
+        public ItemStub[] SpotlightItems { get; set; }
+        public ItemStub[] BackdropItems { get; set; }
+        public ItemStub[] ShowsItems { get; set; }
+        public ItemStub[] ActorItems { get; set; }
+    }
+}
