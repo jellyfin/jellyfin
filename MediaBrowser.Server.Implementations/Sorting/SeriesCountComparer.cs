928e16a0145e2c854ee9e546aaa3a312a63e74ea
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-09-13 16:53:46 -0400

    Added video bitrate sort order

--- MediaBrowser.Server.Implementations/Sorting/SeriesCountComparer.cs
+++ MediaBrowser.Server.Implementations/Sorting/SeriesCountComparer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MediaBrowser.Controller.Entities;
+﻿using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Persistence;
 using MediaBrowser.Controller.Sorting;
