e3aa6cbd7d17679c7963a5d71baae54bc6c3e5ac
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-09-20 11:42:27 -0400

    exposed GetEnhancedImage on ImageProcessor

--- MediaBrowser.Controller/Drawing/IImageProcessor.cs
+++ MediaBrowser.Controller/Drawing/IImageProcessor.cs
@@ -77,5 +77,14 @@ namespace MediaBrowser.Controller.Drawing
         /// <param name="toStream">To stream.</param>
         /// <returns>Task.</returns>
         Task ProcessImage(ImageProcessingOptions options, Stream toStream);
+
+        /// <summary>
+        /// Gets the enhanced image.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="imageType">Type of the image.</param>
+        /// <param name="imageIndex">Index of the image.</param>
+        /// <returns>Task{System.String}.</returns>
+        Task<string> GetEnhancedImage(BaseItem item, ImageType imageType, int imageIndex);
     }
 }
