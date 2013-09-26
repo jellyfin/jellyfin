98442402a5f8f8f37de5dcac07a1d08666623d74
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-09-26 11:48:14 -0400

    fixes #550 - Add internal interfaces for live tv

---
+++ MediaBrowser.Controller/LiveTv/ChannelInfo.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MediaBrowser.Controller.LiveTv
+{
+    /// <summary>
+    /// Class ChannelInfo
+    /// </summary>
+    public class ChannelInfo
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the service.
+        /// </summary>
+        /// <value>The name of the service.</value>
+        public string ServiceName { get; set; }
+    }
+}
---
+++ MediaBrowser.Controller/LiveTv/ILiveTvManager.cs
@@ -0,0 +1,30 @@
+﻿using MediaBrowser.Model.LiveTv;
+using System.Collections.Generic;
+
+namespace MediaBrowser.Controller.LiveTv
+{
+    /// <summary>
+    /// Manages all live tv services installed on the server
+    /// </summary>
+    public interface ILiveTvManager
+    {
+        /// <summary>
+        /// Gets the services.
+        /// </summary>
+        /// <value>The services.</value>
+        IReadOnlyList<ILiveTvService> Services { get; }
+
+        /// <summary>
+        /// Adds the parts.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        void AddParts(IEnumerable<ILiveTvService> services);
+
+        /// <summary>
+        /// Gets the channel info dto.
+        /// </summary>
+        /// <param name="info">The info.</param>
+        /// <returns>ChannelInfoDto.</returns>
+        ChannelInfoDto GetChannelInfoDto(ChannelInfo info);
+    }
+}
