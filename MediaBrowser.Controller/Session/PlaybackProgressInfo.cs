e6aca53e83906a3d39d0d9d39ea25ee0ca6c5345
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-09-30 11:05:07 -0400

    made session a little more flexible for playing non-library items

---
+++ MediaBrowser.Controller/Session/PlaybackProgressInfo.cs
@@ -0,0 +1,38 @@
+﻿using MediaBrowser.Controller.Entities;
+using System;
+
+namespace MediaBrowser.Controller.Session
+{
+    public class PlaybackProgressInfo
+    {
+        /// <summary>
+        /// Gets or sets the item.
+        /// </summary>
+        /// <value>The item.</value>
+        public BaseItem Item { get; set; }
+
+        /// <summary>
+        /// Gets or sets the session id.
+        /// </summary>
+        /// <value>The session id.</value>
+        public Guid SessionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is paused.
+        /// </summary>
+        /// <value><c>true</c> if this instance is paused; otherwise, <c>false</c>.</value>
+        public bool IsPaused { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is muted.
+        /// </summary>
+        /// <value><c>true</c> if this instance is muted; otherwise, <c>false</c>.</value>
+        public bool IsMuted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position ticks.
+        /// </summary>
+        /// <value>The position ticks.</value>
+        public long? PositionTicks { get; set; }
+    }
+}
