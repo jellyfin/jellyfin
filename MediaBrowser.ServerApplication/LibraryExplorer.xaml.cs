a7c8750b52d213ced122bd54c69936fb2379068c
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-09-30 14:59:50 -0400

    removed mahapps

--- MediaBrowser.ServerApplication/LibraryExplorer.xaml.cs
+++ MediaBrowser.ServerApplication/LibraryExplorer.xaml.cs
@@ -1,12 +1,10 @@
-﻿using MahApps.Metro.Controls;
-using MediaBrowser.Common;
+﻿using MediaBrowser.Common;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Localization;
 using MediaBrowser.Controller.Persistence;
-using MediaBrowser.Model.Dto;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Logging;
 using MediaBrowser.Model.Querying;
@@ -29,7 +27,7 @@ namespace MediaBrowser.ServerApplication
     /// <summary>
     /// Interaction logic for LibraryExplorer.xaml
     /// </summary>
-    public partial class LibraryExplorer : MetroWindow
+    public partial class LibraryExplorer : Window
     {
         private readonly ILogger _logger;
 
