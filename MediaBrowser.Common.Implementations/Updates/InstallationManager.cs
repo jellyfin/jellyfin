5c8d0eb7c74e82f5bd4ee5d54f20d2206739b280
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-10-07 14:22:31 -0400

    update service text in wizard

--- MediaBrowser.Common.Implementations/Updates/InstallationManager.cs
+++ MediaBrowser.Common.Implementations/Updates/InstallationManager.cs
@@ -168,7 +168,7 @@ namespace MediaBrowser.Common.Implementations.Updates
             {
                 // Let dev users get results more often for testing purposes
                 var cacheLength = _config.CommonConfiguration.SystemUpdateLevel == PackageVersionClass.Dev
-                                      ? TimeSpan.FromHours(1)
+                                      ? TimeSpan.FromMinutes(15)
                                       : TimeSpan.FromHours(12);
 
                 if ((DateTime.UtcNow - _lastPackageListResult.Item2) < cacheLength)
