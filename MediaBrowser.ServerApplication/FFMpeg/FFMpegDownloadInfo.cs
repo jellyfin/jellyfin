acdc9f87f07e9fa3dcc2c12794cea618070a51af
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-10-13 11:16:12 -0400

    updated ffmpeg

--- MediaBrowser.ServerApplication/FFMpeg/FFMpegDownloadInfo.cs
+++ MediaBrowser.ServerApplication/FFMpeg/FFMpegDownloadInfo.cs
@@ -3,14 +3,14 @@ namespace MediaBrowser.ServerApplication.FFMpeg
 {
     public static class FFMpegDownloadInfo
     {
-        public static string Version = "ffmpeg20130904.1";
+        public static string Version = "ffmpeg20131011";
 
         public static string[] FfMpegUrls = new[]
                 {
-                    "https://github.com/MediaBrowser/MediaBrowser.Resources/raw/master/ffmpeg/windows/ffmpeg-20130904-git-f974289-win32-static.7z",
+                    "https://github.com/MediaBrowser/MediaBrowser.Resources/raw/master/ffmpeg/windows/ffmpeg-20131011-git-024bf3a-win32-static.7z",
 
-                    "http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20130904-git-f974289-win32-static.7z",
-                    "https://www.dropbox.com/s/a81cb2ob23fwcfs/ffmpeg-20130904-git-f974289-win32-static.7z?dl=1"
+                    "http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20131011-git-024bf3a-win32-static.7z",
+                    "https://www.dropbox.com/s/u4q74t1wyn9wlqc/ffmpeg-20131011-git-024bf3a-win32-static.7z?dl=1"
                 };
 
         public static string FFMpegFilename = "ffmpeg.exe";
