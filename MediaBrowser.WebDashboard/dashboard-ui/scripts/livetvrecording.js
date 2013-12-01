857c4d90292d3aa6269175f5637cf187c6099adf
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-12-01 11:09:05 -0500

    add missing files

---
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/livetvrecording.js
@@ -0,0 +1,19 @@
+﻿function deleteRecording(page, id) {
+
+    Dashboard.confirm("Are you sure you wish to delete this recording?", "Confirm Recording Deletion", function (result) {
+
+        if (result) {
+
+            Dashboard.showLoadingMsg();
+
+            ApiClient.deleteLiveTvRecording(id).done(function () {
+
+                Dashboard.alert('Recording deleted');
+
+                reload(page);
+            });
+        }
+
+    });
+}
+
