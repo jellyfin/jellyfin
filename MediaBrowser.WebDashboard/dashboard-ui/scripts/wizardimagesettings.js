a95a5c57de41dad0ca56c0f456efa29359b9dfe6
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-12-22 22:45:51 -0500

    added new wizard page with more chapter image control

---
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/wizardimagesettings.js
@@ -0,0 +1,51 @@
+﻿(function ($, document) {
+
+    function save(page) {
+
+        Dashboard.showLoadingMsg();
+
+        // After saving chapter task, now save server config
+        ApiClient.getServerConfiguration().done(function (config) {
+
+            config.EnableVideoImageExtraction = $('#chkVIdeoImages', page).checked();
+            config.ImageSavingConvention = $('#selectImageSavingConvention', page).val();
+
+            config.EnableMovieChapterImageExtraction = $('#chkMovies', page).checked();
+            config.EnableEpisodeChapterImageExtraction = $('#chkEpisodes', page).checked();
+            config.EnableOtherVideoChapterImageExtraction = $('#chkOtherVideos', page).checked();
+
+            ApiClient.updateServerConfiguration(config).done(function (result) {
+
+                navigateToNextPage();
+
+            });
+        });
+
+    }
+
+    function navigateToNextPage() {
+
+        ApiClient.getSystemInfo().done(function (systemInfo) {
+
+            var os = systemInfo.OperatingSystem.toLowerCase();
+
+            if (os.indexOf('windows') != -1) {
+                Dashboard.navigate('wizardservice.html');
+            } else {
+                Dashboard.navigate('wizardfinish.html');
+            }
+
+        });
+    }
+
+    $(document).on('pageinit', "#wizardImageSettingsPage", function () {
+
+        var page = this;
+
+        $('#btnNextPage', page).on('click', function () {
+
+            save(page);
+        });
+    });
+
+})(jQuery, document, window);
