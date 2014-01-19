2c1426542a741e40fe86bc58b8474c7dcdf6ded2
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-01-19 13:08:17 -0500

    #680 - Support new episode file sorting added dummy results repository

---
+++ MediaBrowser.Controller/Persistence/IFileSortingRepository.cs
@@ -0,0 +1,25 @@
+﻿using MediaBrowser.Model.FileSorting;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediaBrowser.Controller.Persistence
+{
+    public interface IFileSortingRepository
+    {
+        /// <summary>
+        /// Saves the result.
+        /// </summary>
+        /// <param name="result">The result.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Task.</returns>
+        Task SaveResult(FileSortingResult result, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Gets the results.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <returns>IEnumerable{FileSortingResult}.</returns>
+        IEnumerable<FileSortingResult> GetResults(FileSortingResultQuery query);
+    }
+}
---
+++ MediaBrowser.Model/FileSorting/FileSortingResultQuery.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MediaBrowser.Model.FileSorting
+{
+    public class FileSortingResultQuery
+    {
+        /// <summary>
+        /// Skips over a given number of items within the results. Use for paging.
+        /// </summary>
+        /// <value>The start index.</value>
+        public int? StartIndex { get; set; }
+
+        /// <summary>
+        /// The maximum number of items to return
+        /// </summary>
+        /// <value>The limit.</value>
+        public int? Limit { get; set; }
+    }
+}
