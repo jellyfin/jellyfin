5f245301ede15bc9700fff696cebf9cf33f17762
abeloin <alexandre.beloin@gmail.com>
2014-01-22 13:43:15 -0500

    Added comment for: Replaced GetInvalidFileNameChars for Mono

--- MediaBrowser.Common.Implementations/IO/CommonFileSystem.cs
+++ MediaBrowser.Common.Implementations/IO/CommonFileSystem.cs
@@ -130,6 +130,7 @@ namespace MediaBrowser.Common.Implementations.IO
         /// The invalid file name chars
         /// </summary>
         #if __MonoCS__
+        //GetInvalidFileNameChars is less restrictive in Linux/Mac than Windows, this mimic Windows behavior for mono under Linux/Mac.
         private static readonly char[] InvalidFileNameChars = new char [41] { '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07',
             '\x08', '\x09', '\x0A', '\x0B', '\x0C', '\x0D', '\x0E', '\x0F', '\x10', '\x11', '\x12',
             '\x13', '\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1A', '\x1B', '\x1C', '\x1D',
