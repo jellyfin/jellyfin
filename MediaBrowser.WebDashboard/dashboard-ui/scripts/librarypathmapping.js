ee6180591eacd45975b8b6969d308f6e71607986
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-01-29 15:57:17 -0500

    added path mapping page stub

---
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/librarypathmapping.js
@@ -0,0 +1,44 @@
+﻿(function ($, document, window) {
+
+    function loadPage(page, config) {
+
+
+        Dashboard.hideLoadingMsg();
+    }
+
+    $(document).on('pageshow', ".libraryPathMappingForm", function () {
+
+        Dashboard.showLoadingMsg();
+
+        var page = this;
+
+        ApiClient.getServerConfiguration().done(function (config) {
+
+            loadPage(page, config);
+
+        });
+
+    });
+
+    window.LibraryPathMappingPage = {
+
+        onSubmit: function () {
+
+            Dashboard.showLoadingMsg();
+
+            var form = this;
+
+            ApiClient.getServerConfiguration().done(function (config) {
+
+
+                ApiClient.updateServerConfiguration(config).done(Dashboard.processServerConfigurationUpdateResult);
+            });
+
+            // Disable default form submission
+            return false;
+
+        }
+
+    };
+
+})(jQuery, document, window);
