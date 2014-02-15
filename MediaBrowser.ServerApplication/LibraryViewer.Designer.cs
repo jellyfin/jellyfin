60213b2d3c2b03f088bb4668960b1c3a0a5ab8f6
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-02-15 12:17:09 -0500

    convert library explorer to win forms

---
+++ MediaBrowser.ServerApplication/LibraryViewer.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace MediaBrowser.ServerApplication
+{
+    partial class LibraryViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LibraryViewer));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.txtJson = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.selectUser = new System.Windows.Forms.ComboBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(260, 517);
+            this.treeView1.TabIndex = 0;
+            // 
+            // txtJson
+            // 
+            this.txtJson.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtJson.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJson.Location = new System.Drawing.Point(260, 0);
+            this.txtJson.Multiline = true;
+            this.txtJson.Name = "txtJson";
+            this.txtJson.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtJson.Size = new System.Drawing.Size(524, 517);
+            this.txtJson.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.selectUser);
+            this.panel1.Controls.Add(this.lblType);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 44);
+            this.panel1.TabIndex = 2;
+            // 
+            // selectUser
+            // 
+            this.selectUser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.selectUser.FormattingEnabled = true;
+            this.selectUser.Location = new System.Drawing.Point(12, 12);
+            this.selectUser.Name = "selectUser";
+            this.selectUser.Size = new System.Drawing.Size(121, 21);
+            this.selectUser.TabIndex = 2;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(260, 15);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(0, 13);
+            this.lblType.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtJson);
+            this.panel2.Controls.Add(this.treeView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 44);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(784, 517);
+            this.panel2.TabIndex = 3;
+            // 
+            // LibraryViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LibraryViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Library Explorer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtJson;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox selectUser;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ MediaBrowser.ServerApplication/LibraryViewer.cs
@@ -0,0 +1,243 @@
+﻿using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Persistence;
+using MediaBrowser.Model.Querying;
+using MediaBrowser.Model.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SortOrder = MediaBrowser.Model.Entities.SortOrder;
+
+namespace MediaBrowser.ServerApplication
+{
+    public partial class LibraryViewer : Form
+    {
+        private readonly IJsonSerializer _jsonSerializer;
+        private readonly ILibraryManager _libraryManager;
+        private readonly IDisplayPreferencesRepository _displayPreferencesManager;
+        private readonly IItemRepository _itemRepository;
+
+        private User _currentUser;
+
+        public LibraryViewer(IJsonSerializer jsonSerializer, IUserManager userManager, ILibraryManager libraryManager, IDisplayPreferencesRepository displayPreferencesManager, IItemRepository itemRepo)
+        {
+            InitializeComponent();
+
+            _jsonSerializer = jsonSerializer;
+            _libraryManager = libraryManager;
+            _displayPreferencesManager = displayPreferencesManager;
+            _itemRepository = itemRepo;
+
+            foreach (var user in userManager.Users)
+                selectUser.Items.Add(user);
+            selectUser.Items.Insert(0, new User { Name = "Physical" });
+            selectUser.SelectedIndex = 0;
+
+            selectUser.SelectedIndexChanged += selectUser_SelectedIndexChanged;
+            treeView1.AfterSelect += treeView1_AfterSelect;
+        }
+
+        void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node != null)
+            {
+                var item = (BaseItem)e.Node.Tag;
+                lblType.Text = "Type: " + item.GetType().Name;
+
+                var json = FormatJson(_jsonSerializer.SerializeToString(item));
+
+                if (item is IHasMediaStreams)
+                {
+                    var mediaStreams = _itemRepository.GetMediaStreams(new MediaStreamQuery
+                    {
+                        ItemId = item.Id
+
+                    }).ToList();
+
+                    if (mediaStreams.Count > 0)
+                    {
+                        json += "\n\nMedia Streams:\n\n" + FormatJson(_jsonSerializer.SerializeToString(mediaStreams));
+                    }
+                }
+
+                txtJson.Text = json;
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            selectUser_SelectedIndexChanged(null, null);
+        }
+
+        void selectUser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _currentUser = selectUser.SelectedItem as User;
+            if (_currentUser != null)
+                LoadTree();
+        }
+
+        private void LoadTree()
+        {
+            treeView1.Nodes.Clear();
+
+            IEnumerable<BaseItem> children = _currentUser.Name == "Physical" ? new[] { _libraryManager.RootFolder } : _libraryManager.RootFolder.GetChildren(_currentUser, true);
+            children = OrderByName(children, _currentUser);
+
+            foreach (Folder folder in children)
+            {
+
+                var currentFolder = folder;
+
+                var node = new TreeNode { Tag = currentFolder };
+
+                var subChildren = currentFolder.GetChildren(_currentUser, true);
+                subChildren = OrderByName(subChildren, _currentUser);
+                AddChildren(node, subChildren, _currentUser);
+                node.Text = currentFolder.Name + " (" +
+                            node.Nodes.Count + ")";
+
+                treeView1.Nodes.Add(node);
+            }
+        }
+
+        /// <summary>
+        /// Adds the children.
+        /// </summary>
+        /// <param name="parent">The parent.</param>
+        /// <param name="children">The children.</param>
+        /// <param name="user">The user.</param>
+        private void AddChildren(TreeNode parent, IEnumerable<BaseItem> children, User user)
+        {
+            foreach (var item in children)
+            {
+                var node = new TreeNode { Tag = item };
+                var subFolder = item as Folder;
+                if (subFolder != null)
+                {
+                    var prefs = _displayPreferencesManager.GetDisplayPreferences(subFolder.DisplayPreferencesId, user.Id, "LibraryExplorer");
+
+                    AddChildren(node, OrderBy(subFolder.GetChildren(user, true), user, prefs.SortBy), user);
+                    node.Text = item.Name + " (" + node.Nodes.Count + ")";
+                }
+                else
+                {
+                    node.Text = item.Name;
+                }
+                parent.Nodes.Add(node);
+            }
+        }
+
+        /// <summary>
+        /// Orders the name of the by.
+        /// </summary>
+        /// <param name="items">The items.</param>
+        /// <param name="user">The user.</param>
+        /// <returns>IEnumerable{BaseItem}.</returns>
+        private IEnumerable<BaseItem> OrderByName(IEnumerable<BaseItem> items, User user)
+        {
+            return OrderBy(items, user, ItemSortBy.SortName);
+        }
+
+        /// <summary>
+        /// Orders the name of the by.
+        /// </summary>
+        /// <param name="items">The items.</param>
+        /// <param name="user">The user.</param>
+        /// <returns>IEnumerable{BaseItem}.</returns>
+        private IEnumerable<BaseItem> OrderBy(IEnumerable<BaseItem> items, User user, string order)
+        {
+            return _libraryManager.Sort(items, user, new[] { order }, SortOrder.Ascending);
+        }
+
+        /// <summary>
+        /// The INDEN t_ STRING
+        /// </summary>
+        private const string INDENT_STRING = "    ";
+        /// <summary>
+        /// Formats the json.
+        /// </summary>
+        /// <param name="str">The STR.</param>
+        /// <returns>System.String.</returns>
+        private static string FormatJson(string str)
+        {
+            var indent = 0;
+            var quoted = false;
+            var sb = new StringBuilder();
+            for (var i = 0; i < str.Length; i++)
+            {
+                var ch = str[i];
+                switch (ch)
+                {
+                    case '{':
+                    case '[':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, ++indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case '}':
+                    case ']':
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, --indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        sb.Append(ch);
+                        break;
+                    case '"':
+                        sb.Append(ch);
+                        bool escaped = false;
+                        var index = i;
+                        while (index > 0 && str[--index] == '\\')
+                            escaped = !escaped;
+                        if (!escaped)
+                            quoted = !quoted;
+                        break;
+                    case ',':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case ':':
+                        sb.Append(ch);
+                        if (!quoted)
+                            sb.Append(" ");
+                        break;
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Class Extensions
+    /// </summary>
+    static class Extensions
+    {
+        /// <summary>
+        /// Fors the each.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ie">The ie.</param>
+        /// <param name="action">The action.</param>
+        public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+        {
+            foreach (var i in ie)
+            {
+                action(i);
+            }
+        }
+    }
+}
