c6bd890cb2976c4cbf5c278a5987320dcb631652
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-03-07 23:20:31 -0500

    refined collection editing

--- MediaBrowser.Controller/Collections/CollectionCreationOptions.cs
+++ MediaBrowser.Controller/Collections/CollectionCreationOptions.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using MediaBrowser.Model.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace MediaBrowser.Controller.Collections
 {
-    public class CollectionCreationOptions
+    public class CollectionCreationOptions : IHasProviderIds
     {
         public string Name { get; set; }
 
         public Guid? ParentId { get; set; }
 
         public bool IsLocked { get; set; }
+
+        public Dictionary<string, string> ProviderIds { get; set; }
+
+        public CollectionCreationOptions()
+        {
+            ProviderIds = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        }
     }
 }
