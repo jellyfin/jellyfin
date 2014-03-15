bf30936550a0b9be69e646a1b27988914ce9ec4a
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-03-15 18:52:43 -0400

    #712 - Support grouping multiple versions of a movie

--- MediaBrowser.Api/VideosService.cs
+++ MediaBrowser.Api/VideosService.cs
@@ -22,6 +22,21 @@ namespace MediaBrowser.Api
         [ApiMember(Name = "Id", Description = "Item Id", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "GET")]
         public string Id { get; set; }
     }
+
+    [Route("/Videos/{Id}/AlternateVersions", "GET")]
+    [Api(Description = "Gets alternate versions of a video.")]
+    public class GetAlternateVersions : IReturn<ItemsResult>
+    {
+        [ApiMember(Name = "UserId", Description = "Optional. Filter by user id, and attach user data", IsRequired = false, DataType = "string", ParameterType = "query", Verb = "GET")]
+        public Guid? UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>The id.</value>
+        [ApiMember(Name = "Id", Description = "Item Id", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "GET")]
+        public string Id { get; set; }
+    }
     
     public class VideosService : BaseApiService
     {
@@ -48,7 +63,7 @@ namespace MediaBrowser.Api
             var item = string.IsNullOrEmpty(request.Id)
                            ? (request.UserId.HasValue
                                   ? user.RootFolder
-                                  : (Folder)_libraryManager.RootFolder)
+                                  : _libraryManager.RootFolder)
                            : _dtoService.GetItemByDtoId(request.Id, request.UserId);
 
             // Get everything
@@ -58,8 +73,37 @@ namespace MediaBrowser.Api
 
             var video = (Video)item;
 
-            var items = video.AdditionalPartIds.Select(_libraryManager.GetItemById)
-                         .OrderBy(i => i.SortName)
+            var items = video.GetAdditionalParts()
+                         .Select(i => _dtoService.GetBaseItemDto(i, fields, user, video))
+                         .ToArray();
+
+            var result = new ItemsResult
+            {
+                Items = items,
+                TotalRecordCount = items.Length
+            };
+
+            return ToOptimizedSerializedResultUsingCache(result);
+        }
+
+        public object Get(GetAlternateVersions request)
+        {
+            var user = request.UserId.HasValue ? _userManager.GetUserById(request.UserId.Value) : null;
+
+            var item = string.IsNullOrEmpty(request.Id)
+                           ? (request.UserId.HasValue
+                                  ? user.RootFolder
+                                  : _libraryManager.RootFolder)
+                           : _dtoService.GetItemByDtoId(request.Id, request.UserId);
+
+            // Get everything
+            var fields = Enum.GetNames(typeof(ItemFields))
+                    .Select(i => (ItemFields)Enum.Parse(typeof(ItemFields), i, true))
+                    .ToList();
+
+            var video = (Video)item;
+
+            var items = video.GetAlternateVersions()
                          .Select(i => _dtoService.GetBaseItemDto(i, fields, user, video))
                          .ToArray();
 
