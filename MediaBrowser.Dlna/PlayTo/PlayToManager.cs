c9ca62329e0e2d28dfc28f568b09a202f5b6e0ff
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-03-23 17:46:13 -0400

    added an lg profile

--- MediaBrowser.Dlna/PlayTo/PlayToManager.cs
+++ MediaBrowser.Dlna/PlayTo/PlayToManager.cs
@@ -176,7 +176,7 @@ namespace MediaBrowser.Dlna.PlayTo
                     {
                         socket.SendTo(request, new IPEndPoint(IPAddress.Parse("239.255.255.250"), 1900));
 
-                        var delay = _config.Configuration.DlnaOptions.ClientDiscoveryIntervalSeconds*1000;
+                        var delay = _config.Configuration.DlnaOptions.ClientDiscoveryIntervalSeconds * 1000;
 
                         await Task.Delay(delay).ConfigureAwait(false);
                     }
