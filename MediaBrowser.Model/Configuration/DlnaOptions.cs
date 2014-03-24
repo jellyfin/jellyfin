5c22638d401644d3f72175bd8a9078cced8b042b
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-03-24 08:47:39 -0400

    stub out dlna server

--- MediaBrowser.Model/Configuration/DlnaOptions.cs
+++ MediaBrowser.Model/Configuration/DlnaOptions.cs
@@ -4,12 +4,14 @@ namespace MediaBrowser.Model.Configuration
     public class DlnaOptions
     {
         public bool EnablePlayTo { get; set; }
+        public bool EnableServer { get; set; }
         public bool EnableDebugLogging { get; set; }
         public int ClientDiscoveryIntervalSeconds { get; set; }
 
         public DlnaOptions()
         {
             EnablePlayTo = true;
+            EnableServer = true;
             ClientDiscoveryIntervalSeconds = 60;
         }
     }
