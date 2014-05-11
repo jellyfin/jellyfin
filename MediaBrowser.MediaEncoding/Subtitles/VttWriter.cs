8f56b74d63eb624cf26d3b1392c45beab3fb42b3
Luis Miguel Almánzar <ruisu15@gmail.com>
2014-05-11 02:29:44 -0400

    add VTT writer implementation

--- MediaBrowser.MediaEncoding/Subtitles/VttWriter.cs
+++ MediaBrowser.MediaEncoding/Subtitles/VttWriter.cs
@@ -9,8 +9,18 @@ namespace MediaBrowser.MediaEncoding.Subtitles
 {
     public class VttWriter : ISubtitleWriter
     {
-        public void Write(SubtitleTrackInfo info, Stream stream)
-        {
+        public void Write(SubtitleTrackInfo info, Stream stream) {
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.WriteLine("WEBVTT");
+                writer.WriteLine(string.Empty);
+                foreach (var trackEvent in info.TrackEvents)
+                {
+                    writer.WriteLine(@"{0:hh\:mm\:ss\.fff} --> {1:hh\:mm\:ss\.fff}", TimeSpan.FromTicks(trackEvent.StartPositionTicks), TimeSpan.FromTicks(trackEvent.EndPositionTicks));
+                    writer.WriteLine(trackEvent.Text);
+                    writer.WriteLine(string.Empty);
+                }
+            }
         }
     }
 }
