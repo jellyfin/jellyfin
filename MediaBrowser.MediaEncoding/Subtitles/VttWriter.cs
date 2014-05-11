4b418b043dfe2a04c422b01e23674a000b0ab0c5
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-05-11 00:47:48 -0400

    stub out subtitle writer

---
+++ MediaBrowser.MediaEncoding/Subtitles/VttWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBrowser.MediaEncoding.Subtitles
+{
+    public class VttWriter : ISubtitleWriter
+    {
+        public void Write(SubtitleTrackInfo info, Stream stream)
+        {
+        }
+    }
+}
