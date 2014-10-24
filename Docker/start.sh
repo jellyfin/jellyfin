148b257226802b35d84da60ff475c00135dff64e
HurricaneHernandez <carlos@techbyte.ca>
2014-10-24 16:30:13 -0600

    Removed dllmap adjustments from startup, mediabrowser needs to use libwebp compiled by Luke.

--- Docker/start.sh
+++ Docker/start.sh
@@ -2,8 +2,5 @@
 
 # Start MediaBrowser Server
 cd /opt/mediabrowser
-sed -i -e 's%<dllmap dll="libwebp" target="libwebp.so" os="linux" cpu="!x86,!x86_64"/>%%' /opt/mediabrowser/Imazen.WebP.dll.config
-sed -i -e 's%<dllmap dll="libwebp" target="./libwebp/linux/lib64/libwebp.so" os="linux" wordsize="64"/>%%' /opt/mediabrowser/Imazen.WebP.dll.config
-sed -i -e 's%<dllmap dll="libwebp" target="./libwebp/linux/lib/libwebp.so" os="linux" wordsize="32"/>%<dllmap dll="libwebp" target="libwebp.so.5" os="linux"/>%' /opt/mediabrowser/Imazen.WebP.dll.config
 
 mono MediaBrowser.Server.Mono.exe -programdata /config
