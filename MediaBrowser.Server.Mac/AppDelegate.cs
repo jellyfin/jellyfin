6a9a5fa1f78f6482ed25ca47ab1d44ac0ef62165
Luke <luke.pulverenti@gmail.com>
2014-11-21 12:08:50 -0500

    add server.mac project

---
+++ MediaBrowser.Server.Mac/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace MediaBrowser.Server.Mac
+{
+	public partial class AppDelegate : NSApplicationDelegate
+	{
+		MainWindowController mainWindowController;
+
+		public AppDelegate ()
+		{
+		}
+
+		public override void FinishedLaunching (NSObject notification)
+		{
+			mainWindowController = new MainWindowController ();
+			mainWindowController.Window.MakeKeyAndOrderFront (this);
+		}
+	}
+}
+
---
+++ MediaBrowser.Server.Mac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MediaBrowser.Server.Mac
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register ("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
