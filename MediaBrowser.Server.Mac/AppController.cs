e29238130ca2532fd1b371bca0e049f258a9af4b
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-11-21 12:13:21 -0500

    add mac files

---
+++ MediaBrowser.Server.Mac/AppController.cs
@@ -0,0 +1,29 @@
+using System;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+
+namespace StatusMenu
+{
+	[Register("AppController")]
+	public partial class AppController : NSObject
+	{
+		public AppController()
+		{
+
+		}
+
+		public override void AwakeFromNib()
+		{
+			var statusItem = NSStatusBar.SystemStatusBar.CreateStatusItem(30);
+			statusItem.Menu = statusMenu;
+			statusItem.Image = NSImage.ImageNamed("f3bfd_Untitled-thumb");
+			statusItem.HighlightMode = true;
+		}
+
+		partial void HelloWorld(NSObject sender)
+		{
+			Console.WriteLine("hello world");
+		}
+	}
+}
+
