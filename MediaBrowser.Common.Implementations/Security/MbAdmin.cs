3763f7d912cd38740d053d677d064a480fba6d23
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-12-17 00:52:34 -0500

    resolve update check errors

--- MediaBrowser.Common.Implementations/Security/MbAdmin.cs
+++ MediaBrowser.Common.Implementations/Security/MbAdmin.cs
@@ -3,7 +3,7 @@ namespace MediaBrowser.Common.Implementations.Security
 {
     public class MbAdmin
     {
-        public const string HttpUrl = "https://www.mb3admin.com/admin/";
+        public const string HttpUrl = "http://www.mb3admin.com/admin/";
         
         /// <summary>
         /// Leaving as http for now until we get it squared away
