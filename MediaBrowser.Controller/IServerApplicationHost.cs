MediaBrowser/Emby

Ssl in mediabrowser against new listener.

MediaBrowser.Controller/IServerApplicationHost.cs
@@ -37,6 +37,28 @@ namespace MediaBrowser.Controller
         int HttpServerPort { get; }
 
         /// <summary>
+        /// Gets the HTTPS server port.
+        /// </summary>
+        /// <value>The HTTPS server port.</value>
+        int HttpsServerPort { get; }
+
+        /// <summary>
+        /// Gets the value indiciating if an https port should be hosted.
+        /// </summary>
+        /// <value>
+        /// The value indiciating if an https port should be hosted.
+        /// </value>
+        bool UseHttps { get; }
+
+        /// <summary>
+        /// Gets the value pointing to the file system where the ssl certiifcate is located.
+        /// </summary>
+        /// <value>
+        /// The value pointing to the file system where the ssl certiifcate is located.
+        /// </value>
+        string CertificateLocation { get; }
+
+        /// <summary>
         /// Gets a value indicating whether this instance has update available.
         /// </summary>
         /// <value><c>true</c> if this instance has update available; otherwise, <c>false</c>.</value>

MediaBrowser.Controller/Net/IServerManager.cs
@@ -15,7 +15,9 @@ namespace MediaBrowser.Controller.Net
         /// Starts this instance.
         /// </summary>
         /// <param name="urlPrefixes">The URL prefixes.</param>
-        void Start(IEnumerable<string> urlPrefixes);
+        /// <param name="certificateLocation">If an https prefix is specified, 
+        /// the ssl certificate localtion on the file system.</param>
+        void Start(IEnumerable<string> urlPrefixes, string certificateLocation);
 
         /// <summary>
         /// Sends a message to all clients currently connected via a web socket

MediaBrowser.Model/System/SystemInfo.cs
@@ -51,12 +51,6 @@ namespace MediaBrowser.Model.System
         public List<InstallationInfo> InProgressInstallations { get; set; }
 
         /// <summary>
-        /// Gets or sets the web socket port number.
-        /// </summary>
-        /// <value>The web socket port number.</value>
-        public int WebSocketPortNumber { get; set; }
-
-        /// <summary>
         /// Gets or sets the completed installations.
         /// </summary>
         /// <value>The completed installations.</value>
@@ -127,6 +121,25 @@ namespace MediaBrowser.Model.System
         /// </summary>
         /// <value>The HTTP server port number.</value>
         public int HttpServerPortNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value pointing to the file system where the ssl certiifcate is located.
+        /// </summary>
+        /// <value>The value pointing to the file system where the ssl certiifcate is located.</value>
+        public bool UseHttps { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value pointing to the file system where the ssl certiifcate is located..
+        /// </summary>
+        /// <value>The value pointing to the file system where the ssl certiifcate is located..</value>
+        public string CertificateLocation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the HTTPS server port number.
+        /// </summary>
+        /// <value>The HTTPS server port number.</value>
+        public int HttpsServerPortNumber { get; set; }
+
 
         /// <summary>
         /// Gets or sets a value indicating whether this instance has update available.

MediaBrowser.Server.Implementations/HttpServer/SocketSharp/WebSocketSharpListener.cs
@@ -17,11 +17,14 @@ namespace MediaBrowser.Server.Implementations.HttpServer.SocketSharp
 
         private readonly ILogger _logger;
         private readonly Action<string> _endpointListener;
+        private readonly string  _certificateLocation ;
 
-        public WebSocketSharpListener(ILogger logger, Action<string> endpointListener)
+        public WebSocketSharpListener(ILogger logger, Action<string> endpointListener, 
+            string certificateLocation)
         {
             _logger = logger;
             _endpointListener = endpointListener;
+            _certificateLocation = certificateLocation;
         }
 
         public Action<Exception, IRequest> ErrorHandler { get; set; }
@@ -33,7 +36,7 @@ namespace MediaBrowser.Server.Implementations.HttpServer.SocketSharp
         public void Start(IEnumerable<string> urlPrefixes)
         {
             if (_listener == null)
-                _listener = new HttpListener(new SocketSharpLogger(_logger));
+                _listener = new HttpListener(new SocketSharpLogger(_logger), _certificateLocation);
 
             foreach (var prefix in urlPrefixes)
             {

MediaBrowser.Server.Implementations/ServerManager/ServerManager.cs
@@ -99,22 +99,22 @@ namespace MediaBrowser.Server.Implementations.ServerManager
         /// <summary>
         /// Starts this instance.
         /// </summary>
-        public void Start(IEnumerable<string> urlPrefixes)
+        public void Start(IEnumerable<string> urlPrefixes, string certificateLocation)
         {
-            ReloadHttpServer(urlPrefixes);
+            ReloadHttpServer(urlPrefixes, certificateLocation);
         }
 
         /// <summary>
         /// Restarts the Http Server, or starts it if not currently running
         /// </summary>
-        private void ReloadHttpServer(IEnumerable<string> urlPrefixes)
+        private void ReloadHttpServer(IEnumerable<string> urlPrefixes, string certificateLocation)
         {
             _logger.Info("Loading Http Server");
 
             try
             {
                 HttpServer = _applicationHost.Resolve<IHttpServer>();
-                HttpServer.StartServer(urlPrefixes);
+                HttpServer.StartServer(urlPrefixes, certificateLocation);
             }
             catch (SocketException ex)
             {

jellyfin/jellyfin

MediaBrowser.Controller/IServerApplicationHost.cs
@@ -37,6 +37,28 @@ namespace MediaBrowser.Controller
         int HttpServerPort { get; }
 
         /// <summary>
+        /// Gets the HTTPS server port.
+        /// </summary>
+        /// <value>The HTTPS server port.</value>
+        int HttpsServerPort { get; }
+
+        /// <summary>
+        /// Gets the value indiciating if an https port should be hosted.
+        /// </summary>
+        /// <value>
+        /// The value indiciating if an https port should be hosted.
+        /// </value>
+        bool UseHttps { get; }
+
+        /// <summary>
+        /// Gets the value pointing to the file system where the ssl certiifcate is located.
+        /// </summary>
+        /// <value>
+        /// The value pointing to the file system where the ssl certiifcate is located.
+        /// </value>
+        string CertificatePath { get; }
+
+        /// <summary>
         /// Gets a value indicating whether this instance has update available.
         /// </summary>
         /// <value><c>true</c> if this instance has update available; otherwise, <c>false</c>.</value>

MediaBrowser.Controller/Net/IServerManager.cs
@@ -15,7 +15,9 @@ namespace MediaBrowser.Controller.Net
         /// Starts this instance.
         /// </summary>
         /// <param name="urlPrefixes">The URL prefixes.</param>
-        void Start(IEnumerable<string> urlPrefixes);
+        /// <param name="certificatePath">If an https prefix is specified, 
+        /// the ssl certificate localtion on the file system.</param>
+        void Start(IEnumerable<string> urlPrefixes, string certificatePath);
 
         /// <summary>
         /// Sends a message to all clients currently connected via a web socket

MediaBrowser.Model/System/SystemInfo.cs
@@ -123,6 +123,24 @@ namespace MediaBrowser.Model.System
         public int HttpServerPortNumber { get; set; }
 
         /// <summary>
+        /// Gets or sets the value pointing to the file system where the ssl certiifcate is located.
+        /// </summary>
+        /// <value>The value pointing to the file system where the ssl certiifcate is located.</value>
+        public bool UseHttps { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value pointing to the file system where the ssl certiifcate is located..
+        /// </summary>
+        /// <value>The value pointing to the file system where the ssl certiifcate is located..</value>
+        public string CertificatePath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the HTTPS server port number.
+        /// </summary>
+        /// <value>The HTTPS server port number.</value>
+        public int HttpsPortNumber { get; set; }
+
+        /// <summary>
         /// Gets or sets a value indicating whether this instance has update available.
         /// </summary>
         /// <value><c>true</c> if this instance has update available; otherwise, <c>false</c>.</value>

MediaBrowser.Server.Implementations/HttpServer/SocketSharp/WebSocketSharpListener.cs
@@ -18,11 +18,14 @@ namespace MediaBrowser.Server.Implementations.HttpServer.SocketSharp
 
         private readonly ILogger _logger;
         private readonly Action<string> _endpointListener;
+        private readonly string  _certificatePath ;
 
-        public WebSocketSharpListener(ILogger logger, Action<string> endpointListener)
+        public WebSocketSharpListener(ILogger logger, Action<string> endpointListener, 
+            string certificatePath)
         {
             _logger = logger;
             _endpointListener = endpointListener;
+            _certificatePath = certificatePath;
         }
 
         public Action<Exception, IRequest> ErrorHandler { get; set; }
@@ -34,7 +37,7 @@ namespace MediaBrowser.Server.Implementations.HttpServer.SocketSharp
         public void Start(IEnumerable<string> urlPrefixes)
         {
             if (_listener == null)
-                _listener = new HttpListener(new PatternsLogger(_logger), null);
+                _listener = new HttpListener(new PatternsLogger(_logger), _certificatePath);
 
             foreach (var prefix in urlPrefixes)
             {

MediaBrowser.Server.Implementations/ServerManager/ServerManager.cs
@@ -99,22 +99,22 @@ namespace MediaBrowser.Server.Implementations.ServerManager
         /// <summary>
         /// Starts this instance.
         /// </summary>
-        public void Start(IEnumerable<string> urlPrefixes)
+        public void Start(IEnumerable<string> urlPrefixes, string certificatePath)
         {
-            ReloadHttpServer(urlPrefixes);
+            ReloadHttpServer(urlPrefixes, certificatePath);
         }
 
         /// <summary>
         /// Restarts the Http Server, or starts it if not currently running
         /// </summary>
-        private void ReloadHttpServer(IEnumerable<string> urlPrefixes)
+        private void ReloadHttpServer(IEnumerable<string> urlPrefixes, string certificatePath)
         {
             _logger.Info("Loading Http Server");
 
             try
             {
                 HttpServer = _applicationHost.Resolve<IHttpServer>();
-                HttpServer.StartServer(urlPrefixes);
+                HttpServer.StartServer(urlPrefixes, certificatePath);
             }
             catch (SocketException ex)
             {