23531054fd4219ed50424c29fb9d15415248a16a
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-02-22 14:05:38 -0500

    cloud sync progress

--- MediaBrowser.Controller/Sync/ICloudSyncProvider.cs
+++ MediaBrowser.Controller/Sync/ICloudSyncProvider.cs
@@ -1,5 +1,7 @@
 ﻿using MediaBrowser.Model.Sync;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MediaBrowser.Controller.Sync
 {
@@ -17,5 +19,17 @@ namespace MediaBrowser.Controller.Sync
         /// <param name="userId">The user identifier.</param>
         /// <returns>IEnumerable&lt;SyncTarget&gt;.</returns>
         IEnumerable<SyncTarget> GetSyncTargets(string userId);
+
+        /// <summary>
+        /// Transfers the item file.
+        /// </summary>
+        /// <param name="serverId">The server identifier.</param>
+        /// <param name="itemId">The item identifier.</param>
+        /// <param name="inputFile">The input file.</param>
+        /// <param name="pathParts">The path parts.</param>
+        /// <param name="target">The target.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Task.</returns>
+        Task TransferItemFile(string serverId, string itemId, string inputFile, string[] pathParts, SyncTarget target, CancellationToken cancellationToken);
     }
 }
