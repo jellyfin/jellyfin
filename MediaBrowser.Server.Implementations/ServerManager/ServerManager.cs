55c47e50fc261849c277cb65654d91a8c5a8e308
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-03-08 15:06:15 -0400

    exclude httplistener from project

--- MediaBrowser.Server.Implementations/ServerManager/ServerManager.cs
+++ MediaBrowser.Server.Implementations/ServerManager/ServerManager.cs
@@ -1,5 +1,4 @@
-﻿using MediaBrowser.Common.Net;
-using MediaBrowser.Controller;
+﻿using MediaBrowser.Controller;
 using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Net;
 using MediaBrowser.Model.Logging;
