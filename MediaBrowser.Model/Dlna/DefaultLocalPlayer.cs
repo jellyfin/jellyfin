ceb88b1359092dfc9551b15539a3e5182c91424f
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-03-12 02:06:57 -0400

    support file playback with StreamBuilder

---
+++ MediaBrowser.Model/Dlna/DefaultLocalPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace MediaBrowser.Model.Dlna
+{
+    public class DefaultLocalPlayer : ILocalPlayer
+    {
+        public bool CanAccessFile(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public bool CanAccessDirectory(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public virtual bool CanAccessUrl(string url, bool requiresCustomRequestHeaders)
+        {
+            return false;
+        }
+    }
+}
