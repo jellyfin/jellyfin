42e236915041a63d0b9944b8400cc121a4f3cba4
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-04-01 17:55:50 -0400

    add missing PlaySessionId

--- MediaBrowser.Model/Dlna/StreamInfo.cs
+++ MediaBrowser.Model/Dlna/StreamInfo.cs
@@ -69,7 +69,7 @@ namespace MediaBrowser.Model.Dlna
         public SubtitleDeliveryMethod SubtitleDeliveryMethod { get; set; }
         public string SubtitleFormat { get; set; }
 
-        public PlaybackInfoResponse PlaybackInfo { get; set; }
+        public string PlaySessionId { get; set; }
 
         public string MediaSourceId
         {
@@ -207,14 +207,18 @@ namespace MediaBrowser.Model.Dlna
             list.Add(new NameValuePair("Profile", item.VideoProfile ?? string.Empty));
             list.Add(new NameValuePair("Cabac", item.Cabac.HasValue ? item.Cabac.Value.ToString() : string.Empty));
 
-            string playSessionId = item.PlaybackInfo == null ? null : item.PlaybackInfo.PlaySessionId;
-            list.Add(new NameValuePair("PlaySessionId", playSessionId ?? string.Empty));
+            list.Add(new NameValuePair("PlaySessionId", item.PlaySessionId ?? string.Empty));
             list.Add(new NameValuePair("api_key", accessToken ?? string.Empty));
 
             string liveStreamId = item.MediaSource == null ? null : item.MediaSource.LiveStreamId;
             list.Add(new NameValuePair("LiveStreamId", liveStreamId ?? string.Empty));
 
             return list;
+        }
+
+        public List<SubtitleStreamInfo> GetExternalSubtitles(bool includeSelectedTrackOnly, string baseUrl, string accessToken)
+        {
+            return GetExternalSubtitles(includeSelectedTrackOnly, false, baseUrl, accessToken);
         }
 
         public List<SubtitleStreamInfo> GetExternalSubtitles(bool includeSelectedTrackOnly, bool enableAllProfiles, string baseUrl, string accessToken)
