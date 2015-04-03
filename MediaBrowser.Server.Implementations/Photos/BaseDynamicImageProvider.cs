ef505c8e9e2b8f348aeaa89be6bc446014b72996
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-04-03 12:58:12 -0400

    update images

--- MediaBrowser.Server.Implementations/Photos/BaseDynamicImageProvider.cs
+++ MediaBrowser.Server.Implementations/Photos/BaseDynamicImageProvider.cs
@@ -126,7 +126,7 @@ namespace MediaBrowser.Server.Implementations.Photos
 
         protected abstract Task<List<BaseItem>> GetItemsWithImages(IHasImages item);
 
-        private const string Version = "17";
+        private const string Version = "18";
         protected string GetConfigurationCacheKey(List<BaseItem> items, string itemName)
         {
             var parts = Version + "_" + (itemName ?? string.Empty) + "_" +
