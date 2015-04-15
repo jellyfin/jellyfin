c9b11c309d08aa27113e085ce323077fc38b98a0
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-04-15 00:05:47 -0400

    3.0.5582.1

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("3.0.*")]
-//[assembly: AssemblyVersion("3.0.5582.0")]
+//[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyVersion("3.0.5582.1")]
