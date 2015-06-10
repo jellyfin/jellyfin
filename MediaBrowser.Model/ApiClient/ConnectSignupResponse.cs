89fd0da1bac5cf70a4e3122c2eeb1a46aa2a6e28
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-06-10 09:37:07 -0400

    updated nuget

---
+++ MediaBrowser.Model/ApiClient/ConnectSignupResponse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MediaBrowser.Model.ApiClient
+{
+    public class ConnectSignupResponse
+    {
+        public bool IsSuccessful { get; set; }
+        public bool IsEmailInUse { get; set; }
+        public bool IsUsernameInUse { get; set; }
+    }
+}
