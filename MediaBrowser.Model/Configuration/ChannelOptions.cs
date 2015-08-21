2dfb9f3c942fbe0e15b46a9b2498b26cc34f682e
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-08-21 12:54:11 -0400

    update channels

--- MediaBrowser.Model/Configuration/ChannelOptions.cs
+++ MediaBrowser.Model/Configuration/ChannelOptions.cs
@@ -3,19 +3,6 @@
     public class ChannelOptions
     {
         public int? PreferredStreamingWidth { get; set; }
-
         public string DownloadPath { get; set; }
-        public int? MaxDownloadAge { get; set; }
-
-        public string[] DownloadingChannels { get; set; }
-
-        public double? DownloadSizeLimit { get; set; }
-
-        public ChannelOptions()
-        {
-            DownloadingChannels = new string[] { };
-            DownloadSizeLimit = .5;
-            MaxDownloadAge = 30;
-        }
     }
 }
