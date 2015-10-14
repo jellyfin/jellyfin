840a4b76422b90feaf4ccbe074ae1d8eb8ba47a7
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-10-14 01:46:11 -0400

    collection fixes

--- MediaBrowser.WebDashboard/Api/PackageCreator.cs
+++ MediaBrowser.WebDashboard/Api/PackageCreator.cs
@@ -609,7 +609,6 @@ namespace MediaBrowser.WebDashboard.Api
                                 "mediaplayer.js",
                                 "mediaplayer-video.js",
                                 "alphapicker.js",
-                                "collectioneditor.js",
                                 "notifications.js",
                                 "remotecontrol.js",
                                 "search.js",
