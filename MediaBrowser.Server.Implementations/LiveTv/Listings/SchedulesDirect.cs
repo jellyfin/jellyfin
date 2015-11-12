040c0ea21aa02402429f81e063994b4a503fa271
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-11-11 21:25:12 -0500

    fix schedules direct request dates

--- MediaBrowser.Server.Implementations/LiveTv/Listings/SchedulesDirect.cs
+++ MediaBrowser.Server.Implementations/LiveTv/Listings/SchedulesDirect.cs
@@ -48,10 +48,10 @@ namespace MediaBrowser.Server.Implementations.LiveTv.Listings
         {
             List<string> dates = new List<string>();
 
-            var start = new List<DateTime> { startDateUtc, startDateUtc.ToLocalTime() }.Min();
-            var end = new List<DateTime> { endDateUtc, endDateUtc.ToLocalTime() }.Max();
+            var start = new List<DateTime> { startDateUtc, startDateUtc.ToLocalTime() }.Min().Date;
+            var end = new List<DateTime> { endDateUtc, endDateUtc.ToLocalTime() }.Max().Date;
 
-            while (start.DayOfYear <= end.Day)
+            while (start <= end)
             {
                 dates.Add(start.ToString("yyyy-MM-dd"));
                 start = start.AddDays(1);
