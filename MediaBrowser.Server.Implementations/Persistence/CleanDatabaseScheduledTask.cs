4c5ea9070f68c15447d3fac961f9c3ef9b5a0265
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-11-13 15:54:04 -0500

    add master changes

--- MediaBrowser.Server.Implementations/Persistence/CleanDatabaseScheduledTask.cs
+++ MediaBrowser.Server.Implementations/Persistence/CleanDatabaseScheduledTask.cs
@@ -149,7 +149,7 @@ namespace MediaBrowser.Server.Implementations.Persistence
 
                 if (item != null)
                 {
-                    _logger.Debug("Cleaning item {0} type: {1} path: {2}", item.Name, item.GetType().Name, item.Path ?? string.Empty);
+                    _logger.Info("Cleaning item {0} type: {1} path: {2}", item.Name, item.GetType().Name, item.Path ?? string.Empty);
 
                     await _libraryManager.DeleteItem(item, new DeleteOptions
                     {
@@ -170,7 +170,6 @@ namespace MediaBrowser.Server.Implementations.Persistence
         {
             var result = _itemRepo.GetItemIdsWithPath(new InternalItemsQuery
             {
-                IsOffline = false,
                 LocationType = LocationType.FileSystem,
                 //Limit = limit,
 
@@ -213,6 +212,8 @@ namespace MediaBrowser.Server.Implementations.Persistence
                         await libraryItem.UpdateToRepository(ItemUpdateType.None, cancellationToken).ConfigureAwait(false);
                         continue;
                     }
+
+                    _logger.Info("Deleting item from database {0} because path no longer exists. type: {1} path: {2}", libraryItem.Name, libraryItem.GetType().Name, libraryItem.Path ?? string.Empty);
 
                     await _libraryManager.DeleteItem(libraryItem, new DeleteOptions
                     {
