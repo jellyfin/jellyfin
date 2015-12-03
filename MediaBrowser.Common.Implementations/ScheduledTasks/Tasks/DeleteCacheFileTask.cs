4deb3017c3a881b60f3b26704bfbef683bdec29e
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-12-02 23:31:58 -0500

    add cache file deletion error handling

--- MediaBrowser.Common.Implementations/ScheduledTasks/Tasks/DeleteCacheFileTask.cs
+++ MediaBrowser.Common.Implementations/ScheduledTasks/Tasks/DeleteCacheFileTask.cs
@@ -128,7 +128,18 @@ namespace MediaBrowser.Common.Implementations.ScheduledTasks.Tasks
                 DeleteEmptyFolders(directory);
                 if (!_fileSystem.GetFileSystemEntryPaths(directory).Any())
                 {
-					_fileSystem.DeleteDirectory(directory, false);
+                    try
+                    {
+                        _fileSystem.DeleteDirectory(directory, false);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        _logger.ErrorException("Error deleting directory {0}", ex, directory);
+                    }
+                    catch (IOException ex)
+                    {
+                        _logger.ErrorException("Error deleting directory {0}", ex, directory);
+                    }
                 }
             }
         }
@@ -138,6 +149,10 @@ namespace MediaBrowser.Common.Implementations.ScheduledTasks.Tasks
             try
             {
                 _fileSystem.DeleteFile(path);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                _logger.ErrorException("Error deleting file {0}", ex, path);
             }
             catch (IOException ex)
             {
