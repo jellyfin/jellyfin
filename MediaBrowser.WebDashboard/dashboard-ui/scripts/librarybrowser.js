c9e07c9b828bd8202e290bbede4cb94cd893b799
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-12-14 15:34:55 -0500

    update tabs

--- MediaBrowser.WebDashboard/dashboard-ui/scripts/librarybrowser.js
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/librarybrowser.js
@@ -155,7 +155,7 @@
         enableFullPaperTabs: function () {
 
             if (browserInfo.animate && !browserInfo.mobile) {
-                return true;
+                //return true;
             }
 
             return AppInfo.isNativeApp;
