5c03b0d769b230883218e1526441bd7fd0cf7cbc
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-12-14 16:55:21 -0500

    fix merge conflict

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("3.0.*")]
-//[assembly: AssemblyVersion("3.0.5781.5")]
+//[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyVersion("3.0.5781.6")]
