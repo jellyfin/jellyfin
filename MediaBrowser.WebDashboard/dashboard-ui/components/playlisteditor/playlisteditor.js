4f7614b9e6dca414b5a74fbb055996c192a0d470
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-01-01 14:38:15 -0500

    progress on popup metadata editor

--- MediaBrowser.WebDashboard/dashboard-ui/components/playlisteditor/playlisteditor.js
+++ MediaBrowser.WebDashboard/dashboard-ui/components/playlisteditor/playlisteditor.js
@@ -1,4 +1,4 @@
-﻿define(['components/paperdialoghelper', 'paper-dialog', 'paper-input', 'paper-fab'], function (paperDialogHelper) {
+﻿define(['components/paperdialoghelper', 'paper-dialog', 'paper-input'], function (paperDialogHelper) {
 
     var lastPlaylistId = '';
 
