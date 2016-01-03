5acc026c834e71a1f5015e51decc7264b3410c7c
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-01-03 14:42:14 -0500

    update github updater

--- MediaBrowser.Server.Startup.Common/ApplicationHost.cs
+++ MediaBrowser.Server.Startup.Common/ApplicationHost.cs
@@ -1327,7 +1327,6 @@ namespace MediaBrowser.Server.Startup.Common
             }
             else if (ConfigurationManager.CommonConfiguration.SystemUpdateLevel == PackageVersionClass.Dev)
             {
-                excludeSuffixes.Add("-dev");
                 cacheLength = TimeSpan.FromMinutes(5);
                 includePreRelease = true;
             }
