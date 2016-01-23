6ae17fd42a548adce04626b1600882b3aa287360
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-01-22 20:35:43 -0500

    add dvbsub as not text

--- MediaBrowser.Model/Entities/MediaStream.cs
+++ MediaBrowser.Model/Entities/MediaStream.cs
@@ -195,6 +195,7 @@ namespace MediaBrowser.Model.Entities
 
             return StringHelper.IndexOfIgnoreCase(codec, "pgs") == -1 &&
                    StringHelper.IndexOfIgnoreCase(codec, "dvd") == -1 &&
+                   StringHelper.IndexOfIgnoreCase(codec, "dvbsub") == -1 &&
                    !StringHelper.EqualsIgnoreCase(codec, "sub");
         }
 
