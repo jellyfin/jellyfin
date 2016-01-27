c48a4cfb992d486cf55f8d687b6ff0559ae38783
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-01-27 01:06:02 -0500

    remove xml savers

--- MediaBrowser.Api/StartupWizardService.cs
+++ MediaBrowser.Api/StartupWizardService.cs
@@ -70,7 +70,6 @@ namespace MediaBrowser.Api
             _config.Configuration.EnableStandaloneMetadata = true;
             _config.Configuration.EnableLibraryMetadataSubFolder = true;
             _config.Configuration.EnableCustomPathSubFolders = true;
-            _config.Configuration.DisableXmlSavers = true;
             _config.Configuration.DisableStartupScan = true;
             _config.Configuration.EnableUserViews = true;
             _config.Configuration.EnableDateLastRefresh = true;
