30857c15700c98b912a050c9113e6e85649475d2
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-01-30 16:15:04 -0500

    update components

--- MediaBrowser.WebDashboard/dashboard-ui/bower_components/emby-webcomponents/paperdialoghelper/paperdialoghelper.js
+++ MediaBrowser.WebDashboard/dashboard-ui/bower_components/emby-webcomponents/paperdialoghelper/paperdialoghelper.js
@@ -109,12 +109,15 @@
         dlg.entryAnimation = options.entryAnimation || defaultEntryAnimation;
         dlg.exitAnimation = 'fade-out-animation';
 
+        // If it's not fullscreen then lower the default animation speed to make it open really fast
+        var entryAnimationDuration = options.entryAnimationDuration || (options.size ? 240 : 300);
+
         dlg.animationConfig = {
             // scale up
             'entry': {
                 name: options.entryAnimation || 'scale-up-animation',
                 node: dlg,
-                timing: { duration: options.entryAnimationDuration || 300, easing: 'ease-out' }
+                timing: { duration: entryAnimationDuration, easing: 'ease-out' }
             },
             // fade out
             'exit': {
