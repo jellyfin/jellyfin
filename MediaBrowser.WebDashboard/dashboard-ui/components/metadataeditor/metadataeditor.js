969105b07226492c4720f701a2d4396574a352d7
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-02-15 08:32:34 -0500

    fix date fields

--- MediaBrowser.WebDashboard/dashboard-ui/components/metadataeditor/metadataeditor.js
+++ MediaBrowser.WebDashboard/dashboard-ui/components/metadataeditor/metadataeditor.js
@@ -585,13 +585,13 @@
 
         if (item.Type == "Person") {
             context.querySelector('#txtProductionYear').label = Globalize.translate('LabelBirthYear');
-            context.querySelector("#txtPremiereDate").innerHTML = Globalize.translate('LabelBirthDate');
-            context.querySelector("#txtEndDate").innerHTML = Globalize.translate('LabelDeathDate');
+            context.querySelector("#txtPremiereDate").label = Globalize.translate('LabelBirthDate');
+            context.querySelector("#txtEndDate").label = Globalize.translate('LabelDeathDate');
             $('#fldPlaceOfBirth', context).show();
         } else {
             context.querySelector('#txtProductionYear').label = Globalize.translate('LabelYear');
-            context.querySelector("#txtPremiereDate").innerHTML = Globalize.translate('LabelReleaseDate');
-            context.querySelector("#txtEndDate").innerHTML = Globalize.translate('LabelEndDate');
+            context.querySelector("#txtPremiereDate").label = Globalize.translate('LabelReleaseDate');
+            context.querySelector("#txtEndDate").label = Globalize.translate('LabelEndDate');
             $('#fldPlaceOfBirth', context).hide();
         }
 
