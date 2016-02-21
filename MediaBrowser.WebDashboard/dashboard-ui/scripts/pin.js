a8170573926fa71da6cf39ef7663218d06f22bdc
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-02-21 01:25:25 -0500

    update local pin feature

---
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/pin.js
@@ -0,0 +1,60 @@
+﻿define([], function () {
+
+    function onSubmit(e) {
+
+        var form = e.target;
+
+        Dashboard.showLoadingMsg();
+
+        ApiClient.ajax({
+
+            type: "POST",
+            url: ApiClient.getUrl('Auth/Pin/Validate'),
+            data: JSON.stringify({
+                Pin: form.querySelector('#txtPin').value
+            }),
+            contentType: "application/json",
+            dataType: 'json'
+
+        }).then(function (result) {
+
+            Dashboard.hideLoadingMsg();
+            Dashboard.alert({
+                message: Globalize.translate('PinCodeConfirmedMessage', result.AppName),
+                title: Globalize.translate('HeaderThankYou'),
+                callback: function () {
+                    Dashboard.navigate('index.html');
+                }
+            });
+
+        }, function () {
+
+            Dashboard.hideLoadingMsg();
+            Dashboard.alert({
+                message: Globalize.translate('PinCodeInvalidMessage'),
+                title: Globalize.translate('PinCodeInvalid')
+            });
+        });
+
+        // Disable default form submission
+        e.preventDefault();
+        return false;
+    }
+
+    pageIdOn('pageinit', 'pinEntryPage', function () {
+
+        var page = this;
+
+        page.querySelector('form').addEventListener('submit', onSubmit);
+
+    });
+
+    pageIdOn('pageshow', 'pinEntryPage', function () {
+
+        var page = this;
+
+        var txtPin = page.querySelector('#txtPin');
+        txtPin.focus();
+        txtPin.value = '';
+    });
+});
