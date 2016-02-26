887bc7e54d264ae872999547c7282c692dbe6110
softworkz <softworkz@hotmail.com>
2016-02-27 00:52:00 +0100

    Create new series backend implementation

--- MediaBrowser.Model/FileOrganization/EpisodeFileOrganizationRequest.cs
+++ MediaBrowser.Model/FileOrganization/EpisodeFileOrganizationRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace MediaBrowser.Model.FileOrganization
+﻿using System.Collections.Generic;
+
+namespace MediaBrowser.Model.FileOrganization
 {
     public class EpisodeFileOrganizationRequest
     {
@@ -13,5 +15,38 @@
         public int? EndingEpisodeNumber { get; set; }
 
         public bool RememberCorrection { get; set; }
+        public string NewSeriesName { get; set; }
+
+        public string NewSeriesYear { get; set; }
+
+        public string NewSeriesProviderIds { get; set; }
+
+        public string TargetFolder { get; set; }
+
+        public Dictionary<string, string> NewSeriesProviderIdsDictionary
+        {
+            get
+            {
+                var dic = new Dictionary<string, string>();
+
+                if (!string.IsNullOrEmpty(NewSeriesProviderIds))
+                {
+                    var str = NewSeriesProviderIds.Replace("{", "").Replace("}", "").Replace("\"", "");
+
+                    foreach (var item in str.Split(','))
+                    {
+                        var itemArr = item.Split(':');
+                        if (itemArr.Length > 1)
+                        {
+                            var key = itemArr[0].Trim();
+                            var val = itemArr[1].Trim();
+                            dic.Add(key, val);
+                        }
+                    }
+                }
+
+                return dic;
+            }
+        }
     }
 }
