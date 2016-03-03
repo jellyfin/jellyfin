d433ede13bb794e3de103ded39dab14df4eae0de
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-03-03 01:03:23 -0500

    disable sat/ip host for now

--- MediaBrowser.Server.Implementations/LiveTv/TunerHosts/SatIp/SatIpHost.cs
+++ MediaBrowser.Server.Implementations/LiveTv/TunerHosts/SatIp/SatIpHost.cs
@@ -19,153 +19,153 @@ using MediaBrowser.Model.Serialization;
 
 namespace MediaBrowser.Server.Implementations.LiveTv.TunerHosts.SatIp
 {
-    public class SatIpHost : BaseTunerHost, ITunerHost
-    {
-        private readonly IFileSystem _fileSystem;
-        private readonly IHttpClient _httpClient;
+    //public class SatIpHost : BaseTunerHost, ITunerHost
+    //{
+    //    private readonly IFileSystem _fileSystem;
+    //    private readonly IHttpClient _httpClient;
         
-        public SatIpHost(IConfigurationManager config, ILogger logger, IJsonSerializer jsonSerializer, IMediaEncoder mediaEncoder, IFileSystem fileSystem, IHttpClient httpClient)
-            : base(config, logger, jsonSerializer, mediaEncoder)
-        {
-            _fileSystem = fileSystem;
-            _httpClient = httpClient;
-        }
+    //    public SatIpHost(IConfigurationManager config, ILogger logger, IJsonSerializer jsonSerializer, IMediaEncoder mediaEncoder, IFileSystem fileSystem, IHttpClient httpClient)
+    //        : base(config, logger, jsonSerializer, mediaEncoder)
+    //    {
+    //        _fileSystem = fileSystem;
+    //        _httpClient = httpClient;
+    //    }
 
-        private const string ChannelIdPrefix = "sat_";
+    //    private const string ChannelIdPrefix = "sat_";
         
-        protected override async Task<IEnumerable<ChannelInfo>> GetChannelsInternal(TunerHostInfo tuner, CancellationToken cancellationToken)
-        {
-            var satInfo = (SatIpTunerHostInfo) tuner;
+    //    protected override async Task<IEnumerable<ChannelInfo>> GetChannelsInternal(TunerHostInfo tuner, CancellationToken cancellationToken)
+    //    {
+    //        var satInfo = (SatIpTunerHostInfo) tuner;
 
-            return await new M3uParser(Logger, _fileSystem, _httpClient).Parse(satInfo.M3UUrl, ChannelIdPrefix, tuner.Id, cancellationToken).ConfigureAwait(false);
-        }
+    //        return await new M3uParser(Logger, _fileSystem, _httpClient).Parse(satInfo.M3UUrl, ChannelIdPrefix, tuner.Id, cancellationToken).ConfigureAwait(false);
+    //    }
 
-        public static string DeviceType
-        {
-            get { return "satip"; }
-        }
+    //    public static string DeviceType
+    //    {
+    //        get { return "satip"; }
+    //    }
 
-        public override string Type
-        {
-            get { return DeviceType; }
-        }
+    //    public override string Type
+    //    {
+    //        get { return DeviceType; }
+    //    }
 
-        protected override async Task<List<MediaSourceInfo>> GetChannelStreamMediaSources(TunerHostInfo tuner, string channelId, CancellationToken cancellationToken)
-        {
-            var urlHash = tuner.Url.GetMD5().ToString("N");
-            var prefix = ChannelIdPrefix + urlHash;
-            if (!channelId.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
-            {
-                return null;
-            }
+    //    protected override async Task<List<MediaSourceInfo>> GetChannelStreamMediaSources(TunerHostInfo tuner, string channelId, CancellationToken cancellationToken)
+    //    {
+    //        var urlHash = tuner.Url.GetMD5().ToString("N");
+    //        var prefix = ChannelIdPrefix + urlHash;
+    //        if (!channelId.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+    //        {
+    //            return null;
+    //        }
 
-            var channels = await GetChannels(tuner, true, cancellationToken).ConfigureAwait(false);
-            var m3uchannels = channels.Cast<M3UChannel>();
-            var channel = m3uchannels.FirstOrDefault(c => string.Equals(c.Id, channelId, StringComparison.OrdinalIgnoreCase));
-            if (channel != null)
-            {
-                var path = channel.Path;
-                MediaProtocol protocol = MediaProtocol.File;
-                if (path.StartsWith("http", StringComparison.OrdinalIgnoreCase))
-                {
-                    protocol = MediaProtocol.Http;
-                }
-                else if (path.StartsWith("rtmp", StringComparison.OrdinalIgnoreCase))
-                {
-                    protocol = MediaProtocol.Rtmp;
-                }
-                else if (path.StartsWith("rtsp", StringComparison.OrdinalIgnoreCase))
-                {
-                    protocol = MediaProtocol.Rtsp;
-                }
+    //        var channels = await GetChannels(tuner, true, cancellationToken).ConfigureAwait(false);
+    //        var m3uchannels = channels.Cast<M3UChannel>();
+    //        var channel = m3uchannels.FirstOrDefault(c => string.Equals(c.Id, channelId, StringComparison.OrdinalIgnoreCase));
+    //        if (channel != null)
+    //        {
+    //            var path = channel.Path;
+    //            MediaProtocol protocol = MediaProtocol.File;
+    //            if (path.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+    //            {
+    //                protocol = MediaProtocol.Http;
+    //            }
+    //            else if (path.StartsWith("rtmp", StringComparison.OrdinalIgnoreCase))
+    //            {
+    //                protocol = MediaProtocol.Rtmp;
+    //            }
+    //            else if (path.StartsWith("rtsp", StringComparison.OrdinalIgnoreCase))
+    //            {
+    //                protocol = MediaProtocol.Rtsp;
+    //            }
 
-                var mediaSource = new MediaSourceInfo
-                {
-                    Path = channel.Path,
-                    Protocol = protocol,
-                    MediaStreams = new List<MediaStream>
-                    {
-                        new MediaStream
-                        {
-                            Type = MediaStreamType.Video,
-                            // Set the index to -1 because we don't know the exact index of the video stream within the container
-                            Index = -1,
-                            IsInterlaced = true
-                        },
-                        new MediaStream
-                        {
-                            Type = MediaStreamType.Audio,
-                            // Set the index to -1 because we don't know the exact index of the audio stream within the container
-                            Index = -1
+    //            var mediaSource = new MediaSourceInfo
+    //            {
+    //                Path = channel.Path,
+    //                Protocol = protocol,
+    //                MediaStreams = new List<MediaStream>
+    //                {
+    //                    new MediaStream
+    //                    {
+    //                        Type = MediaStreamType.Video,
+    //                        // Set the index to -1 because we don't know the exact index of the video stream within the container
+    //                        Index = -1,
+    //                        IsInterlaced = true
+    //                    },
+    //                    new MediaStream
+    //                    {
+    //                        Type = MediaStreamType.Audio,
+    //                        // Set the index to -1 because we don't know the exact index of the audio stream within the container
+    //                        Index = -1
 
-                        }
-                    },
-                    RequiresOpening = false,
-                    RequiresClosing = false
-                };
+    //                    }
+    //                },
+    //                RequiresOpening = false,
+    //                RequiresClosing = false
+    //            };
 
-                return new List<MediaSourceInfo> { mediaSource };
-            }
-            return new List<MediaSourceInfo> { };
-        }
+    //            return new List<MediaSourceInfo> { mediaSource };
+    //        }
+    //        return new List<MediaSourceInfo> { };
+    //    }
 
-        protected override async Task<MediaSourceInfo> GetChannelStream(TunerHostInfo tuner, string channelId, string streamId, CancellationToken cancellationToken)
-        {
-            var sources = await GetChannelStreamMediaSources(tuner, channelId, cancellationToken).ConfigureAwait(false);
+    //    protected override async Task<MediaSourceInfo> GetChannelStream(TunerHostInfo tuner, string channelId, string streamId, CancellationToken cancellationToken)
+    //    {
+    //        var sources = await GetChannelStreamMediaSources(tuner, channelId, cancellationToken).ConfigureAwait(false);
 
-            return sources.First();
-        }
+    //        return sources.First();
+    //    }
 
-        protected override async Task<bool> IsAvailableInternal(TunerHostInfo tuner, string channelId, CancellationToken cancellationToken)
-        {
-            var updatedInfo = await SatIpDiscovery.Current.GetInfo(tuner.Url, cancellationToken).ConfigureAwait(false);
+    //    protected override async Task<bool> IsAvailableInternal(TunerHostInfo tuner, string channelId, CancellationToken cancellationToken)
+    //    {
+    //        var updatedInfo = await SatIpDiscovery.Current.GetInfo(tuner.Url, cancellationToken).ConfigureAwait(false);
 
-            return updatedInfo.TunersAvailable > 0;
-        }
+    //        return updatedInfo.TunersAvailable > 0;
+    //    }
 
-        protected override bool IsValidChannelId(string channelId)
-        {
-            return channelId.StartsWith(ChannelIdPrefix, StringComparison.OrdinalIgnoreCase);
-        }
+    //    protected override bool IsValidChannelId(string channelId)
+    //    {
+    //        return channelId.StartsWith(ChannelIdPrefix, StringComparison.OrdinalIgnoreCase);
+    //    }
 
-        protected override List<TunerHostInfo> GetTunerHosts()
-        {
-            return SatIpDiscovery.Current.DiscoveredHosts;
-        }
+    //    protected override List<TunerHostInfo> GetTunerHosts()
+    //    {
+    //        return SatIpDiscovery.Current.DiscoveredHosts;
+    //    }
 
-        public string Name
-        {
-            get { return "Sat IP"; }
-        }
+    //    public string Name
+    //    {
+    //        get { return "Sat IP"; }
+    //    }
 
-        public Task<List<LiveTvTunerInfo>> GetTunerInfos(CancellationToken cancellationToken)
-        {
-            var list = GetTunerHosts()
-            .SelectMany(i => GetTunerInfos(i, cancellationToken))
-            .ToList();
+    //    public Task<List<LiveTvTunerInfo>> GetTunerInfos(CancellationToken cancellationToken)
+    //    {
+    //        var list = GetTunerHosts()
+    //        .SelectMany(i => GetTunerInfos(i, cancellationToken))
+    //        .ToList();
 
-            return Task.FromResult(list);
-        }
+    //        return Task.FromResult(list);
+    //    }
 
-        public List<LiveTvTunerInfo> GetTunerInfos(TunerHostInfo info, CancellationToken cancellationToken)
-        {
-            var satInfo = (SatIpTunerHostInfo) info;
+    //    public List<LiveTvTunerInfo> GetTunerInfos(TunerHostInfo info, CancellationToken cancellationToken)
+    //    {
+    //        var satInfo = (SatIpTunerHostInfo) info;
 
-            var list = new List<LiveTvTunerInfo>();
+    //        var list = new List<LiveTvTunerInfo>();
 
-            for (var i = 0; i < satInfo.Tuners; i++)
-            {
-                list.Add(new LiveTvTunerInfo
-                {
-                    Name = satInfo.FriendlyName ?? Name,
-                    SourceType = Type,
-                    Status = LiveTvTunerStatus.Available,
-                    Id = info.Url.GetMD5().ToString("N") + i.ToString(CultureInfo.InvariantCulture),
-                    Url = info.Url
-                });
-            }
+    //        for (var i = 0; i < satInfo.Tuners; i++)
+    //        {
+    //            list.Add(new LiveTvTunerInfo
+    //            {
+    //                Name = satInfo.FriendlyName ?? Name,
+    //                SourceType = Type,
+    //                Status = LiveTvTunerStatus.Available,
+    //                Id = info.Url.GetMD5().ToString("N") + i.ToString(CultureInfo.InvariantCulture),
+    //                Url = info.Url
+    //            });
+    //        }
 
-            return list;
-        }
-    }
+    //        return list;
+    //    }
+    //}
 }
