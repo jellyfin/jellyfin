7187a470b2cb48eb771b15a63df64886535079df
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-03-04 22:39:49 -0500

    update components

--- MediaBrowser.WebDashboard/dashboard-ui/bower_components/emby-webcomponents/images/basicimagefetcher.js
+++ MediaBrowser.WebDashboard/dashboard-ui/bower_components/emby-webcomponents/images/basicimagefetcher.js
@@ -4,18 +4,21 @@ define([], function () {
 
         if (elem.tagName !== "IMG") {
 
-            var tmp = new Image();
+            return new Promise(function (resolve, reject) {
 
-            tmp.onload = function () {
-                elem.style.backgroundImage = "url('" + url + "')";
-            };
-            tmp.src = url;
+                var tmp = new Image();
+
+                tmp.onload = function () {
+                    elem.style.backgroundImage = "url('" + url + "')";
+                    resolve(elem);
+                };
+                tmp.src = url;
+            });
 
         } else {
             elem.setAttribute("src", url);
+            return Promise.resolve(elem);
         }
-
-        return Promise.resolve(elem);
     }
 
     return {
