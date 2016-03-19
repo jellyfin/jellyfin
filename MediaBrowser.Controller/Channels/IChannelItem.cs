bcda2a8f0eaa304a946da29485cbc695de4b1519
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-03-19 18:45:11 -0400

    update channels

---
+++ MediaBrowser.Controller/Channels/IChannelItem.cs
@@ -0,0 +1,11 @@
+﻿using MediaBrowser.Controller.Entities;
+
+namespace MediaBrowser.Controller.Channels
+{
+    public interface IChannelItem : IHasImages, IHasTags
+    {
+        string ChannelId { get; set; }
+
+        string ExternalId { get; set; }
+    }
+}
