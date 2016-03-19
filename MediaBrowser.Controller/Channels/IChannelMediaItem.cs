a91602227198fed268f0bb69fa8acc717a6d4e96
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-03-19 18:45:11 -0400

    update channels

---
+++ MediaBrowser.Controller/Channels/IChannelMediaItem.cs
@@ -0,0 +1,18 @@
+﻿using MediaBrowser.Model.Channels;
+using MediaBrowser.Model.Entities;
+using System.Collections.Generic;
+
+namespace MediaBrowser.Controller.Channels
+{
+    public interface IChannelMediaItem : IChannelItem
+    {
+        long? RunTimeTicks { get; set; }
+        string MediaType { get; }
+
+        ChannelMediaContentType ContentType { get; set; }
+
+        ExtraType? ExtraType { get; set; }
+
+        List<ChannelMediaInfo> ChannelMediaSources { get; set; }
+    }
+}
