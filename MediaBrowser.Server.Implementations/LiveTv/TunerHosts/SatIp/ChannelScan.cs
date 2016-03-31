54de1b744b996683fe9209c8ebbd2316290e9eb0
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-03-31 15:32:26 -0400

    stub out sat channel scan

---
+++ MediaBrowser.Server.Implementations/LiveTv/TunerHosts/SatIp/ChannelScan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Model.LiveTv;
+
+namespace MediaBrowser.Server.Implementations.LiveTv.TunerHosts.SatIp
+{
+    public class ChannelScan
+    {
+        public async Task<List<SatChannel>> Scan(TunerHostInfo info, CancellationToken cancellationToken)
+        {
+            return new List<SatChannel>();
+        }
+    }
+
+    public class SatChannel
+    {
+        // TODO: Add properties
+    }
+}
