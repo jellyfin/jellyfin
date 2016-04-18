37e2f0d25dd72f063e135f1c23ef8072629dc7ac
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-04-17 23:50:44 -0400

    record nal length size

--- MediaBrowser.Server.Implementations/Persistence/MediaStreamColumns.cs
+++ MediaBrowser.Server.Implementations/Persistence/MediaStreamColumns.cs
@@ -26,6 +26,38 @@ namespace MediaBrowser.Server.Implementations.Persistence
             AddRefFramesCommand();
             AddCodecTagColumn();
             AddCommentColumn();
+            AddNalColumn();
+        }
+
+        private void AddNalColumn()
+        {
+            using (var cmd = _connection.CreateCommand())
+            {
+                cmd.CommandText = "PRAGMA table_info(mediastreams)";
+
+                using (var reader = cmd.ExecuteReader(CommandBehavior.SequentialAccess | CommandBehavior.SingleResult))
+                {
+                    while (reader.Read())
+                    {
+                        if (!reader.IsDBNull(1))
+                        {
+                            var name = reader.GetString(1);
+
+                            if (string.Equals(name, "NalLengthSize", StringComparison.OrdinalIgnoreCase))
+                            {
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+
+            var builder = new StringBuilder();
+
+            builder.AppendLine("alter table mediastreams");
+            builder.AppendLine("add column NalLengthSize TEXT");
+
+            _connection.RunQueries(new[] { builder.ToString() }, _logger);
         }
 
         private void AddCommentColumn()
