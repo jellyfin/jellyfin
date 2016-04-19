913c8d72b31d896c818435d084e302f9d2f80b95
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-04-18 22:47:20 -0400

    move metadata image setting

--- MediaBrowser.WebDashboard/dashboard-ui/scripts/metadataconfigurationpage.js
+++ MediaBrowser.WebDashboard/dashboard-ui/scripts/metadataconfigurationpage.js
@@ -9,6 +9,7 @@
         page.querySelector('#chkSaveLocal').checked = config.SaveLocalMeta;
         $('#selectLanguage', page).val(config.PreferredMetadataLanguage);
         $('#selectCountry', page).val(config.MetadataCountryCode);
+        $('#chkDownloadImagesInAdvance', page).checked(config.DownloadImagesInAdvance);
 
         Dashboard.hideLoadingMsg();
     }
@@ -24,6 +25,7 @@
             config.SaveLocalMeta = form.querySelector('#chkSaveLocal').checked;
             config.PreferredMetadataLanguage = $('#selectLanguage', form).val();
             config.MetadataCountryCode = $('#selectCountry', form).val();
+            config.DownloadImagesInAdvance = $('#chkDownloadImagesInAdvance', form).checked();
 
             ApiClient.updateServerConfiguration(config).then(Dashboard.processServerConfigurationUpdateResult);
         });
