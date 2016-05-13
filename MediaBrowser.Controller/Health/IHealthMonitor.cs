829aa9e855897dd6de448a3950bfe35d6d027f19
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-05-12 22:32:12 -0400

    update shared components

---
+++ MediaBrowser.Controller/Health/IHealthMonitor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Model.Notifications;
+
+namespace MediaBrowser.Controller.Health
+{
+    public interface IHealthMonitor
+    {
+        Task<List<Notification>> GetNotifications(CancellationToken cancellationToken);
+    }
+}
