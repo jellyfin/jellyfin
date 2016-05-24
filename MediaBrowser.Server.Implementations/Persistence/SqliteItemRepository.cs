ea4638f723445c94713db3e755b21157b3f5b295
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-05-24 15:03:04 -0400

    add index

--- MediaBrowser.Server.Implementations/Persistence/SqliteItemRepository.cs
+++ MediaBrowser.Server.Implementations/Persistence/SqliteItemRepository.cs
@@ -245,7 +245,8 @@ namespace MediaBrowser.Server.Implementations.Persistence
             string[] postQueries =
                 {
                 "create index if not exists idx_PresentationUniqueKey on TypedBaseItems(PresentationUniqueKey)",
-                "create index if not exists idx_Type on TypedBaseItems(Type)"
+                "create index if not exists idx_Type on TypedBaseItems(Type)",
+                "create index if not exists idx_TopParentId on TypedBaseItems(TopParentId)"
             };
 
             _connection.RunQueries(postQueries, Logger);
