2bb95563f15a7feae94eda8584a57c0eb690acd3
softworkz <softworkz@hotmail.com>
2016-06-04 06:30:06 +0200

    MovieDbSeriesProvider and MovieDbEpisodeProvider: Add support for trailers

--- MediaBrowser.Providers/TV/TheMovieDb/MovieDbProviderBase.cs
+++ MediaBrowser.Providers/TV/TheMovieDb/MovieDbProviderBase.cs
@@ -210,7 +210,19 @@ namespace MediaBrowser.Providers.TV
 
         public class Videos
         {
-            public List<object> results { get; set; }
+            public List<Video> results { get; set; }
+        }
+
+        public class Video
+        {
+            public string id { get; set; }
+            public string iso_639_1 { get; set; }
+            public string iso_3166_1 { get; set; }
+            public string key { get; set; }
+            public string name { get; set; }
+            public string site { get; set; }
+            public string size { get; set; }
+            public string type { get; set; }
         }
 
         public class RootObject
