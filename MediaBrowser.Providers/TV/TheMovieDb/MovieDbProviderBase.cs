fe7bcf6248fd6bb4d3335c859e1b149f5bff7f83
softworkz <softworkz@hotmail.com>
2016-06-25 01:53:38 +0200

    Adjust language for image results from TMDB when querying complex language codes (> 2 chars, qualified by country)

--- MediaBrowser.Providers/TV/TheMovieDb/MovieDbProviderBase.cs
+++ MediaBrowser.Providers/TV/TheMovieDb/MovieDbProviderBase.cs
@@ -151,7 +151,7 @@ namespace MediaBrowser.Providers.TV
             public string file_path { get; set; }
             public int height { get; set; }
             public string id { get; set; }
-            public object iso_639_1 { get; set; }
+            public string iso_639_1 { get; set; }
             public double vote_average { get; set; }
             public int vote_count { get; set; }
             public int width { get; set; }
