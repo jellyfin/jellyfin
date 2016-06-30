c30576199bd1ac25101aecf89cb8e615cdd5f819
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-06-29 23:29:46 -0400

    remove unused components

--- MediaBrowser.Server.Startup.Common/ApplicationHost.cs
+++ MediaBrowser.Server.Startup.Common/ApplicationHost.cs
@@ -160,7 +160,6 @@ namespace MediaBrowser.Server.Startup.Common
         private IHttpServer HttpServer { get; set; }
         private IDtoService DtoService { get; set; }
         private IImageProcessor ImageProcessor { get; set; }
-        private ISeriesOrderManager SeriesOrderManager { get; set; }
 
         /// <summary>
         /// Gets or sets the media encoder.
@@ -475,9 +474,6 @@ namespace MediaBrowser.Server.Startup.Common
 
             ProviderManager = new ProviderManager(HttpClient, ServerConfigurationManager, LibraryMonitor, LogManager, FileSystemManager, ApplicationPaths, () => LibraryManager, JsonSerializer);
             RegisterSingleInstance(ProviderManager);
-
-            SeriesOrderManager = new SeriesOrderManager();
-            RegisterSingleInstance(SeriesOrderManager);
 
             RegisterSingleInstance<ISearchEngine>(() => new SearchEngine(LogManager, LibraryManager, UserManager));
 
@@ -818,8 +814,6 @@ namespace MediaBrowser.Server.Startup.Common
                                      GetExports<IMetadataSaver>(),
                                      GetExports<IImageSaver>(),
                                      GetExports<IExternalId>());
-
-            SeriesOrderManager.AddParts(GetExports<ISeriesOrderProvider>());
 
             ImageProcessor.AddParts(GetExports<IImageEnhancer>());
 
