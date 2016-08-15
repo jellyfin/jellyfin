caab8299d1c8ebb7db7de1c83fc7a26070309a6e
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-08-15 00:36:17 -0400

    add setting for photo libraries

--- MediaBrowser.Model/Configuration/LibraryOptions.cs
+++ MediaBrowser.Model/Configuration/LibraryOptions.cs
@@ -3,5 +3,11 @@
     public class LibraryOptions
     {
         public bool EnableArchiveMediaFiles { get; set; }
+        public bool EnablePhotos { get; set; }
+
+        public LibraryOptions()
+        {
+            EnablePhotos = true;
+        }
     }
 }
