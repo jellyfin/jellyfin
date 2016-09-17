5d9fe75332d7a47f0c6fab81ecf63236d84aff46
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-09-17 00:22:37 -0400

    fix web socket session creation

--- MediaBrowser.Controller/Session/AuthenticationRequest.cs
+++ MediaBrowser.Controller/Session/AuthenticationRequest.cs
@@ -4,6 +4,7 @@ namespace MediaBrowser.Controller.Session
     public class AuthenticationRequest
     {
         public string Username { get; set; }
+        public string UserId { get; set; }
         public string PasswordSha1 { get; set; }
         public string PasswordMd5 { get; set; }
         public string App { get; set; }
