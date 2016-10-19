d4e678a9929b31f34287bd4c6d9b056a939e508f
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-10-19 02:28:44 -0400

    removed dead code

--- MediaBrowser.Dlna/Ssdp/Extensions.cs
+++ MediaBrowser.Dlna/Ssdp/Extensions.cs
@@ -9,30 +9,6 @@ namespace MediaBrowser.Dlna.Ssdp
 {
     public static class Extensions
     {
-        public static Task<int> ReceiveAsync(this Socket socket, byte[] buffer, int offset, int size)
-        {
-            var tcs = new TaskCompletionSource<int>(socket);
-            var remoteip = new IPEndPoint(IPAddress.Any, 0);
-            var endpoint = (EndPoint)remoteip;
-
-            socket.BeginReceiveFrom(buffer, offset, size, SocketFlags.None, ref endpoint, iar =>
-            {
-                var result = (TaskCompletionSource<int>)iar.AsyncState;
-                var iarSocket = (Socket)result.Task.AsyncState;
-
-                try
-                {
-                    result.TrySetResult(iarSocket.EndReceive(iar));
-                }
-                catch (Exception exc)
-                {
-                    result.TrySetException(exc);
-                }
-            }, tcs);
-
-            return tcs.Task;
-        }
-        
         public static string GetValue(this XElement container, XName name)
         {
             var node = container.Element(name);
