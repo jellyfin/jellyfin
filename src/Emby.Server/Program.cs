3ff10fa9ade58d8b5cf7a0465dac7213874baebc
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-10-25 22:54:12 -0400

    create .net core server solution

---
+++ src/Emby.Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emby.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
