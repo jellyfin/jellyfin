9c813f9aedb3d4f20ff495fc23693591e7dda914
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-10-31 03:42:14 -0400

    update taglib

---
+++ Emby.Photos/StreamFileAbstraction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaBrowser.Model.IO;
+using File = TagLib.File;
+
+namespace Emby.Photos
+{
+    public class StreamFileAbstraction : File.IFileAbstraction
+    {
+        public StreamFileAbstraction(string name, Stream readStream)
+        {
+            // TODO: Fix deadlock when setting an actual writable Stream
+            WriteStream = readStream;
+            ReadStream = readStream;
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public Stream ReadStream { get; private set; }
+
+        public Stream WriteStream { get; private set; }
+
+        public void CloseStream(Stream stream)
+        {
+            stream.Dispose();
+        }
+    }
+}
