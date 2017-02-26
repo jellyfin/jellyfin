5fa3817a7667b5de8822ed436b8a66bd05a1afde
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-02-26 16:47:52 -0500

    update components

--- MediaBrowser.Controller/Entities/MusicVideo.cs
+++ MediaBrowser.Controller/Entities/MusicVideo.cs
@@ -6,19 +6,8 @@ using MediaBrowser.Model.Serialization;
 
 namespace MediaBrowser.Controller.Entities
 {
-    public class MusicVideo : Video, IHasArtist, IHasMusicGenres, IHasBudget, IHasLookupInfo<MusicVideoInfo>
+    public class MusicVideo : Video, IHasArtist, IHasMusicGenres, IHasLookupInfo<MusicVideoInfo>
     {
-        /// <summary>
-        /// Gets or sets the budget.
-        /// </summary>
-        /// <value>The budget.</value>
-        public double? Budget { get; set; }
-
-        /// <summary>
-        /// Gets or sets the revenue.
-        /// </summary>
-        /// <value>The revenue.</value>
-        public double? Revenue { get; set; }
         public List<string> Artists { get; set; }
 
         public MusicVideo()
