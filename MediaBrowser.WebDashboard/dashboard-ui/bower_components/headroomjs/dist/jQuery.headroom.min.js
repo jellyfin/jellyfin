18dc2d6986dadbccd42d40ffcfd6721fb9c5d228
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-04-09 15:48:50 -0400

    update card components

--- MediaBrowser.WebDashboard/dashboard-ui/bower_components/headroomjs/dist/jQuery.headroom.min.js
+++
@@ -1,7 +0,0 @@
-/*!
- * headroom.js v0.7.0 - Give your page some headroom. Hide your header until you need it
- * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/headroom.js
- * License: MIT
- */
-
-!function(a){a&&(a.fn.headroom=function(b){return this.each(function(){var c=a(this),d=c.data("headroom"),e="object"==typeof b&&b;e=a.extend(!0,{},Headroom.options,e),d||(d=new Headroom(this,e),d.init(),c.data("headroom",d)),"string"==typeof b&&d[b]()})},a("[data-headroom]").each(function(){var b=a(this);b.headroom(b.data())}))}(window.Zepto||window.jQuery);
