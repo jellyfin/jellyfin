64379734aad36be3d7fad449500bdbc59f4fda94
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-06-17 18:59:17 -0400

    add subtitle language detection

---
+++ Emby.Common.Implementations/TextEncoding/NLangDetect/ErrorCode.cs
@@ -0,0 +1,15 @@
+﻿namespace NLangDetect.Core
+{
+  public enum ErrorCode
+  {
+    NoTextError,
+    FormatError,
+    FileLoadError,
+    DuplicateLangError,
+    NeedLoadProfileError,
+    CantDetectError,
+    CantOpenTrainData,
+    TrainDataFormatError,
+    InitParamError,
+  }
+}
---
+++ Emby.Common.Implementations/TextEncoding/NLangDetect/NLangDetectException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NLangDetect.Core
+{
+  public class NLangDetectException : Exception
+  {
+    #region Constructor(s)
+
+    public NLangDetectException(string message, ErrorCode errorCode)
+      : base(message)
+    {
+      ErrorCode = errorCode;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public ErrorCode ErrorCode { get; private set; }
+
+    #endregion
+  }
+}
