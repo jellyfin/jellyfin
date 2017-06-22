9c756a99dca182a7e159de749129c4167ef12e2d
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-06-22 15:14:58 -0400

    fixes #2588 - OpenSubtitlesDownloader doesn't respect X-Ratelimit-Requests-Remaining

--- MediaBrowser.Common/Extensions/ResourceNotFoundException.cs
+++ MediaBrowser.Common/Extensions/ResourceNotFoundException.cs
@@ -25,4 +25,25 @@ namespace MediaBrowser.Common.Extensions
 
         }
     }
+
+    public class RateLimitExceededException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RateLimitExceededException" /> class.
+        /// </summary>
+        public RateLimitExceededException()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RateLimitExceededException" /> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public RateLimitExceededException(string message)
+            : base(message)
+        {
+
+        }
+    }
 }
