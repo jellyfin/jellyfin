f323984264f6a53fb6d93333a8bcc0424dd3fece
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-08-16 11:42:36 -0400

    remove unused libs

--- Emby.Server.Implementations/IO/MemoryStreamProvider.cs
+++ Emby.Server.Implementations/IO/MemoryStreamProvider.cs
@@ -1,35 +1,8 @@
 ﻿using System.IO;
 using MediaBrowser.Model.IO;
-using Microsoft.IO;
 
 namespace Emby.Server.Implementations.IO
 {
-    public class RecyclableMemoryStreamProvider : IMemoryStreamFactory
-    {
-        readonly RecyclableMemoryStreamManager _manager = new RecyclableMemoryStreamManager();
-
-        public MemoryStream CreateNew()
-        {
-            return _manager.GetStream();
-        }
-
-        public MemoryStream CreateNew(int capacity)
-        {
-            return _manager.GetStream("RecyclableMemoryStream", capacity);
-        }
-
-        public MemoryStream CreateNew(byte[] buffer)
-        {
-            return _manager.GetStream("RecyclableMemoryStream", buffer, 0, buffer.Length);
-        }
-
-        public bool TryGetBuffer(MemoryStream stream, out byte[] buffer)
-        {
-            buffer = stream.GetBuffer();
-            return true;
-        }
-    }
-
     public class MemoryStreamProvider : IMemoryStreamFactory
     {
         public MemoryStream CreateNew()
