79baaa7601e2923b57e67f25692b84f98fb7c0d5
Andrew Rabert <ar@nullsum.net>
2018-12-10 19:27:54 -0500

    Remove Emby.Server.Connect

--- MediaBrowser.Controller/Connect/IConnectManager.cs
+++ MediaBrowser.Controller/Connect/IConnectManager.cs
@@ -7,34 +7,5 @@ namespace MediaBrowser.Controller.Connect
 {
     public interface IConnectManager
     {
-        /// <summary>
-        /// Gets the wan API address.
-        /// </summary>
-        /// <value>The wan API address.</value>
-        string WanApiAddress { get; }
-
-        /// <summary>
-        /// Links the user.
-        /// </summary>
-        Task<UserLinkResult> LinkUser(User user, string connectUsername);
-
-        /// <summary>
-        /// Removes the link.
-        /// </summary>
-        Task RemoveConnect(User user);
-
-        User GetUserFromExchangeToken(string token);
-
-        /// <summary>
-        /// Authenticates the specified username.
-        /// </summary>
-        Task<ConnectAuthenticationResult> Authenticate(string username, string password);
-
-        /// <summary>
-        /// Determines whether [is authorization token valid] [the specified token].
-        /// </summary>
-        /// <param name="token">The token.</param>
-        /// <returns><c>true</c> if [is authorization token valid] [the specified token]; otherwise, <c>false</c>.</returns>
-        bool IsAuthorizationTokenValid(string token);
     }
 }
