10331576837151399e90d868d53f14c464c03f50
Ben Magee <ben@bmagee.com>
2018-12-12 21:44:36 +0000

    Jellyfin.WebDashboard.Test

---
+++ tests/Jellyfin.WebDashboard.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+namespace Jellyfin.WebDashboard.Test
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
