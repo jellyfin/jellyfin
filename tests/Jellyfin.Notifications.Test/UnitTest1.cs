4b888fa59e1d4069d4ceae670f3945e7fc7ae2bd
Ben Magee <ben@bmagee.com>
2018-12-12 20:56:27 +0000

    Jellyfin.Notifications.Test project

---
+++ tests/Jellyfin.Notifications.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace Jellyfin.Notifications.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
