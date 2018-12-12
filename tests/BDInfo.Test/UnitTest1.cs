51c001d446eea2c1d92d760a549f8048db68e1ca
Ben Magee <ben@bmagee.com>
2018-12-12 20:23:27 +0000

    BDInfo.Test

---
+++ tests/BDInfo.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace BDInfo.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
