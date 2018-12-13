9d9e14e1544001ee1f6765e03bc4c719505de43e
Ben Magee <ben@bmagee.com>
2018-12-13 18:17:05 +0000

    Revert "Revert "Test Projects""
    This reverts commit 8820c1a36d3a6fd69e8fea8f38212c9892339d75.

---
+++ tests/Jellyfin.Providers.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace Jellyfin.Providers.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
