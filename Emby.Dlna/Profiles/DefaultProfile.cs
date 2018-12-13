84e2ef10bcd920f1661da919837aac6f5aee5bb0
Ben Magee <ben@bmagee.com>
2018-12-13 09:18:29 +0000

    Remove Emby branding from DLNA server

--- Emby.Dlna/Profiles/DefaultProfile.cs
+++ Emby.Dlna/Profiles/DefaultProfile.cs
@@ -14,9 +14,9 @@ namespace Emby.Dlna.Profiles
 
             ProtocolInfo = "http-get:*:video/mpeg:*,http-get:*:video/mp4:*,http-get:*:video/vnd.dlna.mpeg-tts:*,http-get:*:video/avi:*,http-get:*:video/x-matroska:*,http-get:*:video/x-ms-wmv:*,http-get:*:video/wtv:*,http-get:*:audio/mpeg:*,http-get:*:audio/mp3:*,http-get:*:audio/mp4:*,http-get:*:audio/x-ms-wma*,http-get:*:audio/wav:*,http-get:*:audio/L16:*,http-get:*image/jpeg:*,http-get:*image/png:*,http-get:*image/gif:*,http-get:*image/tiff:*";
 
-            Manufacturer = "Emby";
+            Manufacturer = "Jellyfin";
             ModelDescription = "UPnP/AV 1.0 Compliant Media Server";
-            ModelName = "Emby Server";
+            ModelName = "Jellyfin Media Player";
             ModelNumber = "01";
             ModelUrl = "https://github.com/jellyfin/jellyfin";
             ManufacturerUrl = "https://github.com/jellyfin/jellyfin";
