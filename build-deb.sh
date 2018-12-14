65201879a13f2c4d0d0ea52e4169a61492b05998
Joshua Boniface <joshua@boniface.me>
2018-12-14 13:47:04 -0500

    Add quoting and consistent variable calls

--- build-deb.sh
+++ build-deb.sh
@@ -13,11 +13,11 @@ image_name="jellyfin-debuild"
 
 cleanup() {
     docker image rm $image_name --force
-    test -d ${package_temporary_dir} && rm -r ${package_temporary_dir}
+    test -d "$package_temporary_dir" && rm -r "$package_temporary_dir"
 }
 trap cleanup EXIT
 
-docker build . -t $image_name -f ./Dockerfile.debian_package
-docker run --rm -v $package_temporary_dir:/temp $image_name cp -r /dist /temp/
-sudo chown -R $current_user $package_temporary_dir
-mv $package_temporary_dir/dist/*.deb ../
+docker build . -t "$image_name" -f ./Dockerfile.debian_package
+docker run --rm -v "$package_temporary_dir:/temp" "$image_name" cp -r /dist /temp/
+sudo chown -R "$current_user" "$package_temporary_dir"
+mv "$package_temporary_dir"/dist/*.deb ../
