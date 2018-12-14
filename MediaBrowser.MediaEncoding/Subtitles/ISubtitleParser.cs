1d7d52ff9e42c3efb4bb2c65e82a4a82faf9decb
Mathieu Velten <matmaul@gmail.com>
2018-12-14 10:40:55 +0100

    Port MediaEncoding and Api.Playback from 10e57ce8d21b4516733894075001819f3cd6db6b

---
+++ MediaBrowser.MediaEncoding/Subtitles/ISubtitleParser.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Threading;
+using MediaBrowser.Model.MediaInfo;
+
+namespace MediaBrowser.MediaEncoding.Subtitles
+{
+    public interface ISubtitleParser
+    {
+        /// <summary>
+        /// Parses the specified stream.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>SubtitleTrackInfo.</returns>
+        SubtitleTrackInfo Parse(Stream stream, CancellationToken cancellationToken);
+    }
+}
