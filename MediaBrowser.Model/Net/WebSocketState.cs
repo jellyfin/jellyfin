03547c0b2f03312c3961efc628f1b4d93b5f3266
Vasily <just.one.man@yandex.ru>
2018-12-25 14:57:09 +0300

    Check out removed files from last GPL-known state
    Checkout IHasTrailers.cs from 33703bf8d9927c4d5fba42a36f992fe2295f6ec1
    Checkout IHasMetadata.cs from 7264a8d08f1a3f0749989ab73e46587a3f4c985b
    Checkout ChapterInfoDto.cs from 109dbba080207263ed9fcf210647843d7bc153dd
    Checkout IHasUserData.cs from 1ac408131d0d7d48ed14465c852d204395a73656
    Checkout NotificationServiceInfo.cs from 13083b59b1b50fcb0b4c5ce00f841cb662306646
    Checkout ProgramQuery.cs from 6dba63054435c70afa85bcdc2f5e13f1456815c3
    Checkout RecommendedProgramQuery.cs from 2f6b00c87af7df842e611e4b0554de024270e9aa
    Checkout SessionInfoDto.cs from b31cae80649a961a501582149189885c9112d802
    Checkout IHasSpecialFeatures.cs from 4cacd23473e3f4668987e7c711f89bcc36c70cf4
    Checkout WebSocketState.cs from a292f71ce4706e130b7fad8af3d1dc66a70113e5
    Checkout AuthenticationResult.cs from 03aae2ee82919333ff7075c2420fc77215bbc20f
    Checkout IWebSocket.cs from b961f0ed8e9c0bfbb2f3dfe838aad2e90960547f

---
+++ MediaBrowser.Model/Net/WebSocketState.cs
@@ -0,0 +1,38 @@
+﻿
+namespace MediaBrowser.Model.Net
+{
+    /// <summary>
+    /// Enum WebSocketState
+    /// </summary>
+    public enum WebSocketState
+    {
+        /// <summary>
+        /// The none
+        /// </summary>
+        None,
+        /// <summary>
+        /// The connecting
+        /// </summary>
+        Connecting,
+        /// <summary>
+        /// The open
+        /// </summary>
+        Open,
+        /// <summary>
+        /// The close sent
+        /// </summary>
+        CloseSent,
+        /// <summary>
+        /// The close received
+        /// </summary>
+        CloseReceived,
+        /// <summary>
+        /// The closed
+        /// </summary>
+        Closed,
+        /// <summary>
+        /// The aborted
+        /// </summary>
+        Aborted
+    }
+}
