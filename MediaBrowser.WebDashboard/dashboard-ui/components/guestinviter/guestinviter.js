aef5064bc5bd9f583aeed4b02005a15dce05545f
Claus Vium <clausvium@gmail.com>
2019-01-04 10:16:00 +0100

    Changed Connect invite UI to simple invite

--- MediaBrowser.WebDashboard/dashboard-ui/components/guestinviter/guestinviter.js
+++ MediaBrowser.WebDashboard/dashboard-ui/components/guestinviter/guestinviter.js
@@ -1,58 +1,80 @@
-define(["dialogHelper", "loading", "require", "emby-input", "emby-button", "emby-checkbox", "paper-icon-button-light", "formDialogStyle", "emby-linkbutton"], function(dialogHelper, loading, require) {
+define(["dialogHelper", "loading", "require", "emby-input", "emby-button", "emby-checkbox", "paper-icon-button-light", "formDialogStyle", "emby-linkbutton"], function (dialogHelper, loading, require) {
     "use strict";
 
     function renderLibrarySharingList(context, result) {
         var folderHtml = "";
-        folderHtml += result.Items.map(function(i) {
-            var currentHtml = "";
-            return currentHtml += '<label><input is="emby-checkbox" class="chkShareFolder" type="checkbox" data-folderid="' + i.Id + '" checked="checked"/><span>' + i.Name + "</span></label>"
-        }).join(""), context.querySelector(".librarySharingList").innerHTML = folderHtml
+        folderHtml += result.Items.map(function (i__q) {
+            return '<label><input is="emby-checkbox" class="chkShareFolder" type="checkbox" data-folderid="' + i__q.Id + '" checked="checked"/><span>' + i__q.Name + "</span></label>";
+        }).join("");
+        context.querySelector(".librarySharingList").innerHTML = folderHtml;
     }
-
+    
     function inviteUser(dlg) {
         loading.show();
-        var shareExcludes = Array.prototype.filter.call(dlg.querySelectorAll(".chkShareFolder"), function(i) {
-            return i.checked
-        }).map(function(i) {
-            return i.getAttribute("data-folderid")
+        var shareExcludes = Array.prototype.filter.call(dlg.querySelectorAll(".chkShareFolder"), function (i__w) {
+            return i__w.checked;
+        }).map(function (i__e) {
+            return i__e.getAttribute("data-folderid");
         });
-        require(["connectHelper"], function(connectHelper) {
-            connectHelper.inviteGuest({
-                apiClient: ApiClient,
-                guestOptions: {
-                    ConnectUsername: dlg.querySelector("#txtConnectUsername").value,
-                    EnabledLibraries: shareExcludes.join(","),
-                    SendingUserId: Dashboard.getCurrentUserId(),
-                    EnableLiveTv: !1
-                }
-            }).then(function() {
-                loading.hide(), dlg.submitted = !0, dialogHelper.close(dlg)
-            })
-        })
+        
+        // TODO cvium
+        loading.hide();
+        dlg.submitted = true;
+        dialogHelper.close(dlg);
+        // require(["connectHelper"], function (connectHelper) {
+        //     connectHelper.inviteGuest({
+        //         apiClient: ApiClient,
+        //         guestOptions: {
+        //             ConnectUsername: dlg.querySelector("#txtConnectUsername").value,
+        //             EnabledLibraries: shareExcludes.join(","),
+        //             SendingUserId: Dashboard.getCurrentUserId(),
+        //             EnableLiveTv: false
+        //         }
+        //     }).then(function () {
+        //         loading.hide();
+        //         dlg.submitted = true;
+        //         dialogHelper.close(dlg);
+        //     });
+        // });
     }
+
     return {
-        show: function() {
-            return new Promise(function(resolve, reject) {
-                require(["text!./guestinviter.template.html"], function(template) {
+        show: function () {
+            return new Promise(function (resolve, reject) {
+                require(["text!./guestinviter.template.html"], function (template) {
                     var dlg = dialogHelper.createDialog({
-                        removeOnClose: !0,
+                        removeOnClose: true,
                         size: "small"
                     });
-                    dlg.classList.add("ui-body-a"), dlg.classList.add("background-theme-a"), dlg.classList.add("formDialog");
+                    dlg.classList.add("ui-body-a");
+                    dlg.classList.add("background-theme-a");
+                    dlg.classList.add("formDialog");
                     var html = "";
-                    html += Globalize.translateDocument(template), dlg.innerHTML = html, dialogHelper.open(dlg), dlg.addEventListener("close", function() {
-                        dlg.submitted ? resolve() : reject()
-                    }), dlg.querySelector(".btnCancel").addEventListener("click", function(e) {
-                        dialogHelper.close(dlg)
-                    }), dlg.querySelector("form").addEventListener("submit", function(e) {
-                        return inviteUser(dlg), e.preventDefault(), !1
-                    }), ApiClient.getJSON(ApiClient.getUrl("Library/MediaFolders", {
-                        IsHidden: !1
-                    })).then(function(result) {
-                        renderLibrarySharingList(dlg, result)
-                    })
-                })
-            })
+                    html += Globalize.translateDocument(template);
+                    dlg.innerHTML = html;
+                    dialogHelper.open(dlg);
+                    dlg.addEventListener("close", function () {
+                        if (dlg.submitted) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    });
+                    dlg.querySelector(".btnCancel").addEventListener("click", function (e__r) {
+                        dialogHelper.close(dlg);
+                    });
+                    dlg.querySelector("form").addEventListener("submit", function (e__t) {
+                        inviteUser(dlg);
+                        e__t.preventDefault();
+                        return false;
+                    });
+                    ApiClient.getJSON(ApiClient.getUrl("Library/MediaFolders", {
+                        IsHidden: false
+                    })).then(function (result) {
+                        renderLibrarySharingList(dlg, result);
+                    });
+                });
+            });
         }
-    }
+    };
 });
