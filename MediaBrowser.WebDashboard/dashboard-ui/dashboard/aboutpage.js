4cbacb6be199a1be2ee784804a826d126e539ff6
dkanada <dkanada@users.noreply.github.com>
2019-01-09 19:27:18 +0900

    remove dashboard-ui files

--- MediaBrowser.WebDashboard/dashboard-ui/dashboard/aboutpage.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-    "use strict";
-    return function(view, params) {
-        view.addEventListener("viewbeforeshow", function(e) {
-            var elem = view.querySelector("#appVersionNumber");
-            elem.innerHTML = elem.innerHTML.replace("{0}", ConnectionManager.appVersion())
-        })
-    }
-});
