4ed79a75f119efa8dd67942371d05e0044387398
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-01-12 15:56:06 +0100

    Updated docker build script.

--- deployment/docker/build.sh
+++ deployment/docker/build.sh
@@ -9,5 +9,3 @@ build_jellyfin_docker ../.. ../../Dockerfile jellyfin:amd64-${VERSION}
 build_jellyfin_docker ../.. Dockerfile.arm64v8 jellyfin:arm64v8-${VERSION} arm64v8
 
 build_jellyfin_docker ../.. Dockerfile.arm32v7 jellyfin:arm32v7-${VERSION} arm32v7
-
-
