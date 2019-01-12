74507099ad2239e33e22486d392167eada57624f
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-01-12 01:49:46 +0100

    Added git submodules command. Converted RPM to build from local tree. Added all the execution bits to the build scripts.

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+git submodule update --init --recursive
+
+pushd ../Jellyfin.Versioning
+#TODO Uncomment the next line with PR is merged.
+#./update-version
+popd
+
 # Execute all build.sh and package.sh and sign.sh scripts in every folder. In that order. Script should check for artifacts themselves.
 echo "Running for platforms '$@'."
 for directory in */ ; do
@@ -8,13 +15,13 @@ for directory in */ ; do
         echo "Processing ${platform}"
         pushd "$platform"
         if [ -f build.sh ]; then
-            echo ./build.sh 
+            ./build.sh 
         fi  
         if [ -f package.sh ]; then
-            echo ./package.sh
+            ./package.sh
         fi
         if [ -f sign.sh ]; then
-            echo ./sign.sh
+            ./sign.sh
         fi
         popd
     else
--- deployment/fedora-package-x64/package.sh
+++ deployment/fedora-package-x64/package.sh
@@ -15,6 +15,7 @@ set -o nounset
 
 package_temporary_dir="`pwd`/pkg-dist-tmp"
 output_dir="`pwd`/pkg-dist"
+pkg_src_dir="`pwd`/pkg-src"
 current_user="`whoami`"
 image_name="jellyfin-rpmbuild"
 
@@ -22,16 +23,64 @@ cleanup() {
     set +o errexit
     docker image rm $image_name --force
     rm -rf "$package_temporary_dir"
+    rm -rf "$pkg_src_dir/jellyfin-${VERSION}.tar.gz"
 }
 trap cleanup EXIT INT
-
-docker build ../.. -t "$image_name" -f ./Dockerfile.fedora_package
+GNU_TAR=1
 mkdir -p "$package_temporary_dir"
+echo "Bundling all sources for RPM build."
+tar \
+--transform "s,^\.,jellyfin-${VERSION}" \
+--exclude='.git*' \
+--exclude='**/.git' \
+--exclude='**/.hg' \
+--exclude='**/.vs' \
+--exclude='**/.vscode' \
+--exclude='deployment' \
+--exclude='**/bin' \
+--exclude='**/obj' \
+--exclude='**/.nuget' \
+--exclude='*.deb' \
+--exclude='*.rpm' \
+-Jcvf \
+"$package_temporary_dir/jellyfin-${VERSION}.tar.xz" \
+-C "../.." \
+./ || true && GNU_TAR=0
+
+if [ $GNU_TAR -eq 0 ]; then
+    echo "The installed tar binary did not support --transform. Using workaround."
+    mkdir -p "$package_temporary_dir/jellyfin-${VERSION}"
+    # Not GNU tar
+    tar \
+    --exclude='.git*' \
+    --exclude='**/.git' \
+    --exclude='**/.hg' \
+    --exclude='**/.vs' \
+    --exclude='**/.vscode' \
+    --exclude='deployment' \
+    --exclude='**/bin' \
+    --exclude='**/obj' \
+    --exclude='**/.nuget' \
+    --exclude='*.deb' \
+    --exclude='*.rpm' \
+    -zcf \
+    "$package_temporary_dir/jellyfin-${VERSION}/jellyfin.tar.gz" \
+    -C "../.." \
+    ./
+    echo "Extracting filtered package."
+    tar -xzf "$package_temporary_dir/jellyfin-${VERSION}/jellyfin.tar.gz" -C "$package_temporary_dir/jellyfin-${VERSION}"
+    echo "Removing filtered package."
+    rm "$package_temporary_dir/jellyfin-${VERSION}/jellyfin.tar.gz"
+    echo "Repackaging package into final tarball."
+    tar -zcf "$pkg_src_dir/jellyfin-${VERSION}.tar.gz" -C "$package_temporary_dir" "jellyfin-${VERSION}"
+fi
+
+docker build ../.. -t "$image_name" -f ./Dockerfile
 mkdir -p "$output_dir"
-docker run --rm -v "$package_temporary_dir:/temp" "$image_name" sh -c 'find / -maxdepth 1 -type f -name "jellyfin*" -exec mv {} /temp \;'
+docker run --rm -v "$package_temporary_dir:/temp" "$image_name" sh -c 'find /build/rpmbuild -maxdepth 3 -type f -name "jellyfin*.rpm" -exec mv {} /temp \;'
 chown -R "$current_user" "$package_temporary_dir"
 if [ $? -ne 0 ]; then
 	# Some platforms need this to chown the file properly. (Platforms with native docker, not just the client)
     sudo chown -R "$current_user" "$package_temporary_dir"
 fi
-mv "$package_temporary_dir"/* "$output_dir"
+mv "$package_temporary_dir"/*.rpm "$output_dir"
