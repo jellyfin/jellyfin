cc719fd77cea3d56ef17ba4fa52abbade4bd609d
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-01-13 20:29:23 +0100

    EditorConfig reformat: BDInfo, DvdLib, Emby.Dlna

--- DvdLib/Properties/AssemblyInfo.cs
+++ DvdLib/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
