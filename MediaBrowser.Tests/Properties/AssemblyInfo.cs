e4c155455b068cc8b3813a7ab12f262d076dc614
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-01-13 20:31:48 +0100

    EditorConfig reformat: MediaBrowser.Providers, MediaBrowser.Tests, SocketHttpListener

--- MediaBrowser.Tests/Properties/AssemblyInfo.cs
+++ MediaBrowser.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MediaBrowser.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ï¿½  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
