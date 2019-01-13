82b03a522f0c986e059290b1d7c984becb0bf0ba
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-01-13 20:26:44 +0100

    Visual Studio Reformat: MediaBrowser.WebDashboard

--- MediaBrowser.WebDashboard/ServerEntryPoint.cs
+++ MediaBrowser.WebDashboard/ServerEntryPoint.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using MediaBrowser.Common;
 using MediaBrowser.Controller.Plugins;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MediaBrowser.WebDashboard
 {
