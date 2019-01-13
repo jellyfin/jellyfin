d7b7be395b76c7fcc49d3baffeb59d51a9b36378
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-01-13 23:24:43 +0100

    Moved ARM dockerfiles back to root.

--- deployment/docker/build.sh
+++ deployment/docker/build.sh
@@ -6,6 +6,6 @@ VERSION=`get_version ../..`
 
 build_jellyfin_docker ../.. ../../Dockerfile jellyfin:amd64-${VERSION}
 
-build_jellyfin_docker ../.. Dockerfile.arm64v8 jellyfin:arm64v8-${VERSION} arm64v8
+build_jellyfin_docker ../.. ../../Dockerfile.arm64v8 jellyfin:arm64v8-${VERSION} arm64v8
 
-build_jellyfin_docker ../.. Dockerfile.arm32v7 jellyfin:arm32v7-${VERSION} arm32v7
+build_jellyfin_docker ../.. ../../Dockerfile.arm32v7 jellyfin:arm32v7-${VERSION} arm32v7
