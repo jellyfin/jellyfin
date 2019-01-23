cc3b1e5cc8243745f18719ee633c599580aa7637
Thomas Büttner <thomas@vergesslicher.tech>
2019-01-23 21:14:10 +0100

    Fix package build for fedora

--- deployment/fedora-package-x64/clean.sh
+++ deployment/fedora-package-x64/clean.sh
@@ -4,4 +4,14 @@ source ../common.build.sh
 
 VERSION=`get_version ../..`
 
-clean_jellyfin ../.. Release `pwd`/dist/jellyfin_${VERSION}
+package_temporary_dir="`pwd`/pkg-dist-tmp"
+pkg_src_dir="`pwd`/pkg-src"
+image_name="jellyfin-rpmbuild"
+docker_sudo=""
+if ! $(id -Gn | grep -q 'docker') && [ ! $EUID -eq 0 ]; then
+    docker_sudo=sudo
+fi
+
+$docker_sudo docker image rm $image_name --force
+rm -rf "$package_temporary_dir"
+rm -rf "$pkg_src_dir/jellyfin-${VERSION}.tar.gz"
