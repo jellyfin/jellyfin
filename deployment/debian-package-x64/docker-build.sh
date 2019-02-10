fb256b7aa0cf500c54fd1a1fa8295a0dac9520b1
Joshua Boniface <joshua@boniface.me>
2019-02-09 19:32:06 -0500

    Fix control file for Microsoft Docker setup

--- deployment/debian-package-x64/docker-build.sh
+++ deployment/debian-package-x64/docker-build.sh
@@ -8,6 +8,9 @@ set -o xtrace
 # Move to source directory
 pushd ${SOURCE_DIR}
 
+# Remove build-dep for dotnet-sdk-2.2, since it's not a package in this image
+sed -i '/dotnet-sdk-2.2,/d' debian/control
+
 # Build DEB
 dpkg-buildpackage -us -uc
 
