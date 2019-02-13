5a054e5150f18ba6e23f463f024c305acb39585e
Claus Vium <clausvium@gmail.com>
2019-02-13 12:20:13 +0100

    Remove useless doc

--- MediaBrowser.Providers/TV/TheTVDB/TvdbUtils.cs
+++ MediaBrowser.Providers/TV/TheTVDB/TvdbUtils.cs
@@ -1,23 +1,12 @@
 using System;
 using System.ComponentModel;
 using MediaBrowser.Model.Entities;
-using TvDbSharper.Dto;
-
 namespace MediaBrowser.Providers.TV.TheTVDB
 {
-    /// <summary>
-    /// Class TVUtils
-    /// </summary>
     public static class TvdbUtils
     {
-        /// <summary>
-        /// The TVDB API key
-        /// </summary>
         public static readonly string TvdbApiKey = "OG4V3YJ3FAP7FP2K";
         public static readonly string TvdbBaseUrl = "https://www.thetvdb.com/";
-        /// <summary>
-        /// The banner URL
-        /// </summary>
         public static readonly string BannerUrl = TvdbBaseUrl + "banners/";
 
         public static ImageType GetImageTypeFromKeyType(string keyType)
