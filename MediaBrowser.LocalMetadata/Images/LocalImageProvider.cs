5d560384c2bcc3615f0171c8bb0dd41486a1515f
Claus Vium <clausvium@gmail.com>
2019-02-24 16:16:14 +0100

    Re-add a linebreak that went missing

--- MediaBrowser.LocalMetadata/Images/LocalImageProvider.cs
+++ MediaBrowser.LocalMetadata/Images/LocalImageProvider.cs
@@ -63,6 +63,7 @@ namespace MediaBrowser.LocalMetadata.Images
             {
                 return new List<FileSystemMetadata>();
             }
+
             var path = item.ContainingFolderPath;
 
             // Exit if the cache dir does not exist, alternative solution is to create it, but that's a lot of empty dirs...
