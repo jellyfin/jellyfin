250e0c75dfaebca54e93be6c11c70cb0d19e589a
Joshua Boniface <joshua@boniface.me>
2019-04-17 22:31:06 -0400

    Add MethodNotAllowedException with code 405

--- Emby.Server.Implementations/HttpServer/HttpListenerHost.cs
+++ Emby.Server.Implementations/HttpServer/HttpListenerHost.cs
@@ -203,6 +203,7 @@ namespace Emby.Server.Implementations.HttpServer
                 case DirectoryNotFoundException _:
                 case FileNotFoundException _:
                 case ResourceNotFoundException _: return 404;
+                case MethodNotAllowedException _: return 405;
                 case RemoteServiceUnavailableException _: return 502;
                 default: return 500;
             }
