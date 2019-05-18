1bf3a26a6194c6a5878f1e4c98ed953103ac7b71
Joshua M. Boniface <joshua@boniface.me>
2019-05-17 23:12:21 -0400

    Bump version for 10.3.3

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.3.2")]
-[assembly: AssemblyFileVersion("10.3.2")]
+[assembly: AssemblyVersion("10.3.3")]
+[assembly: AssemblyFileVersion("10.3.3")]
