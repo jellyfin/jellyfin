903e7607b85f4060086b42bc17cf5d95f400cbe8
Bond_009 <bond.009@outlook.com>
2019-07-07 18:28:58 +0200

    Remove unused using references

--- Emby.Server.Implementations/HttpClientManager/HttpClientManager.cs
+++ Emby.Server.Implementations/HttpClientManager/HttpClientManager.cs
@@ -4,8 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MediaBrowser.Common.Configuration;
