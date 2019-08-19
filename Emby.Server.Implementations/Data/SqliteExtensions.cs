3fd489d1cb85d654b4b32d2ffd901832a38adbe9
Bond_009 <bond.009@outlook.com>
2019-08-19 17:03:21 +0200

    Upgrade SQLitePCL to v2

--- Emby.Server.Implementations/Data/SqliteExtensions.cs
+++ Emby.Server.Implementations/Data/SqliteExtensions.cs
@@ -18,10 +18,6 @@ namespace Emby.Server.Implementations.Data
 
             connection.RunInTransaction(conn =>
             {
-                //foreach (var query in queries)
-                //{
-                //    conn.Execute(query);
-                //}
                 conn.ExecuteAll(string.Join(";", queries));
             });
         }
@@ -38,7 +34,8 @@ namespace Emby.Server.Implementations.Data
 
         public static Guid ReadGuidFromBlob(this IResultSetValue result)
         {
-            return new Guid(result.ToBlob());
+            // TODO: Remove ToArray when upgrading to netstandard2.1
+            return new Guid(result.ToBlob().ToArray());
         }
 
         public static string ToDateTimeParamValue(this DateTime dateValue)
