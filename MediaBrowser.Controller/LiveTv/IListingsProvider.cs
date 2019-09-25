c5995af493542d898450359853c5fbb7a0b4bc53
Bond_009 <bond.009@outlook.com>
2019-09-25 17:19:48 +0200

    Some style fixes

--- MediaBrowser.Controller/LiveTv/IListingsProvider.cs
+++ MediaBrowser.Controller/LiveTv/IListingsProvider.cs
@@ -10,10 +10,15 @@ namespace MediaBrowser.Controller.LiveTv
     public interface IListingsProvider
     {
         string Name { get; }
+
         string Type { get; }
+
         Task<IEnumerable<ProgramInfo>> GetProgramsAsync(ListingsProviderInfo info, string channelId, DateTime startDateUtc, DateTime endDateUtc, CancellationToken cancellationToken);
+
         Task Validate(ListingsProviderInfo info, bool validateLogin, bool validateListings);
+
         Task<List<NameIdPair>> GetLineups(ListingsProviderInfo info, string country, string location);
+
         Task<List<ChannelInfo>> GetChannels(ListingsProviderInfo info, CancellationToken cancellationToken);
     }
 }
