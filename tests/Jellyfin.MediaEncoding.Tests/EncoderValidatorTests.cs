05a1510b31ac43fa0a4dec6c1b3a87667984c707
Bond_009 <bond.009@outlook.com>
2019-09-28 19:49:41 +0200

    Add more test data

--- tests/Jellyfin.MediaEncoding.Tests/EncoderValidatorTests.cs
+++ tests/Jellyfin.MediaEncoding.Tests/EncoderValidatorTests.cs
@@ -14,6 +14,7 @@ namespace Jellyfin.MediaEncoding.Tests
             public IEnumerator<object[]> GetEnumerator()
             {
                 yield return new object[] { EncoderValidatorTestsData.FFmpegV42Output, new Version(4, 2) };
+                yield return new object[] { EncoderValidatorTestsData.FFmpegV414Output, new Version(4, 1, 4) };
                 yield return new object[] { EncoderValidatorTestsData.FFmpegV404Output, new Version(4, 0, 4) };
             }
 
@@ -29,6 +30,7 @@ namespace Jellyfin.MediaEncoding.Tests
 
         [Theory]
         [InlineData(EncoderValidatorTestsData.FFmpegV42Output, true)]
+        [InlineData(EncoderValidatorTestsData.FFmpegV414Output, true)]
         [InlineData(EncoderValidatorTestsData.FFmpegV404Output, true)]
         public void ValidateVersionInternalTest(string versionOutput, bool valid)
         {
