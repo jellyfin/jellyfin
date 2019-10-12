fffc5e3a6f240d74bfe868b09981ac45c26d423c
Joshua Boniface <joshua@boniface.me>
2019-10-11 20:12:52 -0400

    Bump version to 10.5.0 for next stable release

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.4.0")]
-[assembly: AssemblyFileVersion("10.4.0")]
+[assembly: AssemblyVersion("10.5.0")]
+[assembly: AssemblyFileVersion("10.5.0")]
