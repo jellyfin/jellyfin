a09fb4a20c806b9364150734cf26f11bbbfe148c
Andrew Mahone <andrew.mahone@gmail.com>
2019-11-07 11:22:07 -0500

    Update Emby.Server.Implementations/Data/SqliteItemRepository.cs
    formatting
    
    Co-Authored-By: Bond-009 <bond.009@outlook.com>

--- Emby.Server.Implementations/Data/SqliteItemRepository.cs
+++ Emby.Server.Implementations/Data/SqliteItemRepository.cs
@@ -58,6 +58,7 @@ namespace Emby.Server.Implementations.Data
                 queryPrefixText.Append(column);
                 queryPrefixText.Append(',');
             }
+
             queryPrefixText.Length -= 1;
             queryPrefixText.Append(") values ");
             _mediaAttachmentInsertPrefix = queryPrefixText.ToString();
