c6855e6a2a347db17f2b8b2f18dd89d5b47a8816
Andrew Mahone <andrew.mahone@gmail.com>
2019-11-07 11:53:39 -0500

    Simplify AttachmentExtractor instantiation.

--- Emby.Server.Implementations/ApplicationHost.cs
+++ Emby.Server.Implementations/ApplicationHost.cs
@@ -282,8 +282,6 @@ namespace Emby.Server.Implementations
 
         private ISubtitleEncoder SubtitleEncoder { get; set; }
 
-        private IAttachmentExtractor AttachmentExtractor { get; set; }
-
         private ISessionManager SessionManager { get; set; }
 
         private ILiveTvManager LiveTvManager { get; set; }
@@ -906,9 +904,7 @@ namespace Emby.Server.Implementations
 
             serviceCollection.AddSingleton(typeof(IResourceFileManager), typeof(ResourceFileManager));
 
-            AttachmentExtractor = new MediaBrowser.MediaEncoding.Attachments.AttachmentExtractor(LibraryManager, LoggerFactory, ApplicationPaths, FileSystemManager, MediaEncoder, MediaSourceManager, ProcessFactory);
-
-            serviceCollection.AddSingleton(AttachmentExtractor);
+            serviceCollection.AddSingleton(typeof(MediaBrowser.Controller.MediaEncoding.IAttachmentExtractor),typeof(MediaBrowser.MediaEncoding.Attachments.AttachmentExtractor));
 
             _displayPreferencesRepository.Initialize();
 
