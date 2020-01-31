71abf1d3ce915289cf952a6d581a50d83fa9b259
Bond_009 <Bond.009@outlook.com>
2020-01-31 22:46:10 +0100

    Enable TreatWarningsAsErrors and Nullable for test projects

--- MediaBrowser.Controller/Net/IAuthService.cs
+++ MediaBrowser.Controller/Net/IAuthService.cs
@@ -1,3 +1,5 @@
+#nullable enable
+
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Model.Services;
 using Microsoft.AspNetCore.Http;
@@ -7,6 +9,6 @@ namespace MediaBrowser.Controller.Net
     public interface IAuthService
     {
         void Authenticate(IRequest request, IAuthenticationAttributes authAttribtues);
-        User Authenticate(HttpRequest request, IAuthenticationAttributes authAttribtues);
+        User? Authenticate(HttpRequest request, IAuthenticationAttributes authAttribtues);
     }
 }
