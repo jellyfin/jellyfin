cefac4f886e82a642380586b3d732e729cd76dd1
Bond-009 <bond.009@outlook.com>
2020-02-06 13:16:22 +0100

    Update Emby.Server.Implementations/Session/SessionManager.cs
    Co-Authored-By: dkanada <dkanada@users.noreply.github.com>

--- Emby.Server.Implementations/Session/SessionManager.cs
+++ Emby.Server.Implementations/Session/SessionManager.cs
@@ -161,7 +161,7 @@ namespace Emby.Server.Implementations.Session
         }
 
         /// <summary>
-        /// Releases unmanaged and - optionally - managed resources.
+        /// Releases unmanaged and optionally managed resources.
         /// </summary>
         /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
         protected virtual void Dispose(bool disposing)
