19a609a88980b2ac21050eeb991e2abb55675e9c
dkanada <dkanada@users.noreply.github.com>
2020-02-24 00:24:03 +0900

    update musicbrainz options

--- MediaBrowser.Providers/Plugins/MusicBrainz/Configuration/PluginConfiguration.cs
+++ MediaBrowser.Providers/Plugins/MusicBrainz/Configuration/PluginConfiguration.cs
@@ -5,12 +5,12 @@ namespace MediaBrowser.Providers.Plugins.MusicBrainz
 {
     public class PluginConfiguration : BasePluginConfiguration
     {
-        public bool Enable { get; set; } = false;
-
-        public bool ReplaceArtistName { get; set; } = false;
-
         public string Server { get; set; } = "https://www.musicbrainz.org";
 
         public long RateLimit { get; set; } = 1000u;
+
+        public bool Enable { get; set; }
+
+        public bool ReplaceArtistName { get; set; }
     }
 }
