107974e3f8ad1c4461bd289ef9cf4d5fb1e619df
Narfinger <narfinger@noreply.github.com>
2020-02-27 11:31:57 +0900

    moves shows tests to Theory and InlineData format

--- tests/Jellyfin.Naming.Tests/TV/EpisodeWithoutSeasonTests.cs
+++ tests/Jellyfin.Naming.Tests/TV/EpisodeWithoutSeasonTests.cs
@@ -6,42 +6,13 @@ namespace Jellyfin.Naming.Tests.TV
 {
     public class EpisodeWithoutSeasonTests
     {
-        // FIXME
-        // [Fact]
-        public void TestWithoutSeason1()
-        {
-            Test(@"/server/anything_ep02.mp4", "anything", null, 2);
-        }
-
-        // FIXME
-        // [Fact]
-        public void TestWithoutSeason2()
-        {
-            Test(@"/server/anything_ep_02.mp4", "anything", null, 2);
-        }
-
-        // FIXME
-        // [Fact]
-        public void TestWithoutSeason3()
-        {
-            Test(@"/server/anything_part.II.mp4", "anything", null, null);
-        }
-
-        // FIXME
-        // [Fact]
-        public void TestWithoutSeason4()
-        {
-            Test(@"/server/anything_pt.II.mp4", "anything", null, null);
-        }
-
-        // FIXME
-        // [Fact]
-        public void TestWithoutSeason5()
-        {
-            Test(@"/server/anything_pt_II.mp4", "anything", null, null);
-        }
-
-        private void Test(string path, string seriesName, int? seasonNumber, int? episodeNumber)
+        // TODO: [Theory]
+        // TODO: [InlineData(@"/server/anything_ep02.mp4", "anything", null, 2)]
+        // TODO: [InlineData(@"/server/anything_ep_02.mp4", "anything", null, 2)]
+        // TODO: [InlineData(@"/server/anything_part.II.mp4", "anything", null, null)]
+        // TODO: [InlineData(@"/server/anything_pt.II.mp4", "anything", null, null)]
+        // TODO: [InlineData(@"/server/anything_pt_II.mp4", "anything", null, null)]
+        public void Test(string path, string seriesName, int? seasonNumber, int? episodeNumber)
         {
             var options = new NamingOptions();
 
