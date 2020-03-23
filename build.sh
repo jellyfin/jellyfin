cf6dc609b72fd9b388c987ae38c0a8bf95168d15
Joshua M. Boniface <joshua@boniface.me>
2020-03-23 17:54:17 -0400

    Fix up single-segment platform names

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ usage() {
 list_platforms() {
     declare -a platforms
     platforms=(
-        $( find deployment -maxdepth 1 -mindepth 1 -name "build.*" | awk -F'.' '{ $1=""; print $2 "." $3 }' | sort )
+        $( find deployment -maxdepth 1 -mindepth 1 -name "build.*" | awk -F'.' '{ $1=""; printf $2; if ($3 != ""){ printf "." $3; } print ""; }' | sort )
     )
     echo -e "Valid platforms:"
     echo
