30c1170a55c9fa191d3dd26a6229d0776b366ec4
ferferga <ferferga.fer@gmail.com>
2020-03-26 20:29:00 +0100

    Remove comments

--- Emby.Server.Implementations/ScheduledTasks/Tasks/PluginUpdateTask.cs
+++ Emby.Server.Implementations/ScheduledTasks/Tasks/PluginUpdateTask.cs
@@ -97,25 +97,18 @@ namespace Emby.Server.Implementations.ScheduledTasks
             progress.Report(100);
         }
 
-        /// <inheritdoc />
         public string Name => _localization.GetLocalizedString("TaskUpdatePlugins");
 
-        /// <inheritdoc />
         public string Description => _localization.GetLocalizedString("TaskUpdatePluginsDescription");
 
-        /// <inheritdoc />
         public string Category => _localization.GetLocalizedString("TasksApplication");
 
-        /// <inheritdoc />
         public string Key => "PluginUpdates";
 
-        /// <inheritdoc />
         public bool IsHidden => false;
 
-        /// <inheritdoc />
         public bool IsEnabled => true;
 
-        /// <inheritdoc />
         public bool IsLogged => true;
     }
 }
