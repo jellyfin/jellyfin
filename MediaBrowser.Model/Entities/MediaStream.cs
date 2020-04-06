8b7d80a098c59f609aa2b1eda9fd7d1e0382843f
ZadenRB <zaden.ruggieroboune@gmail.com>
2020-04-06 11:01:36 -0600

    Indentation consistency

--- MediaBrowser.Model/Entities/MediaStream.cs
+++ MediaBrowser.Model/Entities/MediaStream.cs
@@ -176,11 +176,11 @@ namespace MediaBrowser.Model.Entities
                     if (!string.IsNullOrEmpty(Title))
                     {
                         return attributes.AsEnumerable()
-                        // keep Tags that are not already in Title
-                        .Where(tag => Title.IndexOf(tag, StringComparison.OrdinalIgnoreCase) == -1)
-                        // attributes concatenation, starting with Title
-                        .Aggregate(new StringBuilder(Title), (builder, attr) => builder.Append(" - ").Append(attr))
-                        .ToString();
+                            // keep Tags that are not already in Title
+                            .Where(tag => Title.IndexOf(tag, StringComparison.OrdinalIgnoreCase) == -1)
+                            // attributes concatenation, starting with Title
+                            .Aggregate(new StringBuilder(Title), (builder, attr) => builder.Append(" - ").Append(attr))
+                            .ToString();
                     }
 
                     return string.Join(" - ", attributes.ToArray());
