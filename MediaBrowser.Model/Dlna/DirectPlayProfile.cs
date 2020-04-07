b43a8a56dc143c9355fc7b84b11f4cc484a3d57d
dkanada <dkanada@users.noreply.github.com>
2020-04-08 01:04:31 +0900

    Merge pull request #2796 from JustAMan/fix-transcode-reasons
    Make codec check in profile examine profile type first
    
    (cherry picked from commit aeedd06f51b27074f7e08470e8a3e7e3a7b75e7c)
    Signed-off-by: Joshua M. Boniface <joshua@boniface.me>

--- MediaBrowser.Model/Dlna/DirectPlayProfile.cs
+++ MediaBrowser.Model/Dlna/DirectPlayProfile.cs
@@ -26,12 +26,12 @@ namespace MediaBrowser.Model.Dlna
 
         public bool SupportsVideoCodec(string codec)
         {
-            return ContainerProfile.ContainsContainer(VideoCodec, codec);
+            return Type == DlnaProfileType.Video && ContainerProfile.ContainsContainer(VideoCodec, codec);
         }
 
         public bool SupportsAudioCodec(string codec)
         {
-            return ContainerProfile.ContainsContainer(AudioCodec, codec);
+            return (Type == DlnaProfileType.Audio || Type == DlnaProfileType.Video) && ContainerProfile.ContainsContainer(AudioCodec, codec);
         }
     }
 }
