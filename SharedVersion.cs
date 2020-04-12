16549dead90b6830a0ad5821f64346c6d6435f35
Joshua M. Boniface <joshua@boniface.me>
2020-04-12 19:24:56 -0400

    Bump version to 10.5.4

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.5.3")]
-[assembly: AssemblyFileVersion("10.5.3")]
+[assembly: AssemblyVersion("10.5.4")]
+[assembly: AssemblyFileVersion("10.5.4")]
