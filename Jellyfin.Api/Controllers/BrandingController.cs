efb6cd8fcf5f7a60cb94beeec587d22802ced818
Felix Ruhnow <felix.ruhnow@gmail.com>
2020-04-18 22:27:38 +0200

    Migrated BrandingService.cs to ASP.NET WebApi.

---
+++ Jellyfin.Api/Controllers/BrandingController.cs
@@ -0,0 +1,42 @@
+using Jellyfin.Api.Models.Branding;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Controller.Configuration;
+
+namespace Jellyfin.Api.Controllers
+{
+    /// <summary>
+    /// The branding controller.
+    /// </summary>
+    public class BrandingController : BaseJellyfinApiController
+    {
+        private IServerConfigurationManager _serverConfigurationManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BrandingController"/> class.
+        /// </summary>
+        /// <param name="serverConfigurationManager">Instance of ServerConfigurationManager.</param>
+        public BrandingController(IServerConfigurationManager serverConfigurationManager)
+        {
+            this._serverConfigurationManager = serverConfigurationManager;
+        }
+
+        /// <summary>
+        /// Endpoint for getting a servers branding settings.
+        /// </summary>
+        /// <returns>Branding settings of the server.</returns>
+        public BrandingDto GetBrandingOptions()
+        {
+            return this._serverConfigurationManager.GetConfiguration<BrandingDto>("branding");
+        }
+
+        /// <summary>
+        /// Endpoint for getting a servers branding css.
+        /// </summary>
+        /// <returns>String representation of the server.</returns>
+        public string GetBrandingCss()
+        {
+            var result = this._serverConfigurationManager.GetConfiguration<BrandingDto>("branding");
+            return result.CustomCss ?? string.Empty;
+        }
+    }
+}
