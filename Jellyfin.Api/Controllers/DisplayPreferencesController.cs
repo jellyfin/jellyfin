86d68e23e7af367152edc36977a9a39431bd2641
crobibero <cody@robibe.ro>
2020-04-19 12:06:18 -0600

    Add DisplayPreferencesController

---
+++ Jellyfin.Api/Controllers/DisplayPreferencesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jellyfin.Api.Controllers
+{
+    /// <summary>
+    /// Display Preferences Controller.
+    /// </summary>
+    public class DisplayPreferencesController : BaseJellyfinApiController
+    {
+    }
+}
