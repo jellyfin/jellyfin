407de0209ef0f78d5a8ec3f9af2557010124d9cb
artiume <siderite@gmail.com>
2020-05-25 18:01:00 -0400

    Update MediaEncoder.cs

--- MediaBrowser.MediaEncoding/Encoder/MediaEncoder.cs
+++ MediaBrowser.MediaEncoding/Encoder/MediaEncoder.cs
@@ -258,7 +258,7 @@ namespace MediaBrowser.MediaEncoding.Encoder
             //_logger.Info("Supported decoders: {0}", string.Join(",", list.ToArray()));
         }
 
-        private List<string> _hwaccels = new List<string>();
+        private List<string> _hwaccels = Array.Empty<string>();
         public void SetAvailableHwaccels(IEnumerable<string> list)
         {
             _hwaccels = list.ToList();
