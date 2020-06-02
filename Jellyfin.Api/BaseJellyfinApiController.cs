638cfa32abc3ffd61119f774ffc5a0f5a490d3e9
David <daullmer@gmail.com>
2020-06-02 15:07:07 +0200

    Move SearchService to new API endpoint

--- Jellyfin.Api/BaseJellyfinApiController.cs
+++ Jellyfin.Api/BaseJellyfinApiController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Jellyfin.Api
@@ -9,5 +10,23 @@ namespace Jellyfin.Api
     [Route("[controller]")]
     public class BaseJellyfinApiController : ControllerBase
     {
+        /// <summary>
+        /// Splits a string at a seperating character into an array of substrings.
+        /// </summary>
+        /// <param name="value">The string to split.</param>
+        /// <param name="separator">The char that seperates the substrings.</param>
+        /// <param name="removeEmpty">Option to remove empty substrings from the array.</param>
+        /// <returns>An array of the substrings.</returns>
+        internal static string[] Split(string value, char separator, bool removeEmpty)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return Array.Empty<string>();
+            }
+
+            return removeEmpty
+                ? value.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries)
+                : value.Split(separator);
+        }
     }
 }
