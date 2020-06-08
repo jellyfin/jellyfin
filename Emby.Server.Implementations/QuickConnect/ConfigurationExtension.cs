001c78573eb132dadad1fcd8162d2966fbf0d402
ConfusedPolarBear <33811686+ConfusedPolarBear@users.noreply.github.com>
2020-06-08 17:14:20 -0500

    Add XML documentation

--- Emby.Server.Implementations/QuickConnect/ConfigurationExtension.cs
+++ Emby.Server.Implementations/QuickConnect/ConfigurationExtension.cs
@@ -1,20 +1,33 @@
-#pragma warning disable CS1591
-
 using System.Collections.Generic;
 using MediaBrowser.Common.Configuration;
 
 namespace Emby.Server.Implementations.QuickConnect
 {
+    /// <summary>
+    /// Configuration extension to support persistent quick connect configuration
+    /// </summary>
     public static class ConfigurationExtension
     {
+        /// <summary>
+        /// Return the current quick connect configuration
+        /// </summary>
+        /// <param name="manager">Configuration manager</param>
+        /// <returns></returns>
         public static QuickConnectConfiguration GetQuickConnectConfiguration(this IConfigurationManager manager)
         {
             return manager.GetConfiguration<QuickConnectConfiguration>("quickconnect");
         }
     }
 
+    /// <summary>
+    /// Configuration factory for quick connect
+    /// </summary>
     public class QuickConnectConfigurationFactory : IConfigurationFactory
     {
+        /// <summary>
+        /// Returns the current quick connect configuration
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<ConfigurationStore> GetConfigurations()
         {
             return new ConfigurationStore[]
--- Emby.Server.Implementations/QuickConnect/QuickConnectConfiguration.cs
+++ Emby.Server.Implementations/QuickConnect/QuickConnectConfiguration.cs
@@ -1,15 +1,22 @@
-#pragma warning disable CS1591
-
 using MediaBrowser.Model.QuickConnect;
 
 namespace Emby.Server.Implementations.QuickConnect
 {
+    /// <summary>
+    /// Persistent quick connect configuration
+    /// </summary>
     public class QuickConnectConfiguration
     {
+        /// <summary>
+        /// Quick connect configuration object
+        /// </summary>
         public QuickConnectConfiguration()
         {
         }
 
+        /// <summary>
+        /// Persistent quick connect availability state
+        /// </summary>
         public QuickConnectState State { get; set; }
     }
 }
