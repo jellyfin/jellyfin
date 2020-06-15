e6c197b96991a619d58e816ee56c23644c4a1de1
Max Git <rotvel@gmail.com>
2020-06-16 01:09:41 +0200

    Cleanup

--- MediaBrowser.MediaEncoding/Encoder/EncoderValidator.cs
+++ MediaBrowser.MediaEncoding/Encoder/EncoderValidator.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
