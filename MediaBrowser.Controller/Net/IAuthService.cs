1c78482b480034738516596248955e3e09756dd6
David <daullmer@gmail.com>
2020-06-20 18:02:03 +0200

    Use authorization code from api-migration to fix startup wizard

--- MediaBrowser.Controller/Net/IAuthService.cs
+++ MediaBrowser.Controller/Net/IAuthService.cs
@@ -11,5 +11,12 @@ namespace MediaBrowser.Controller.Net
         void Authenticate(IRequest request, IAuthenticationAttributes authAttribtues);
 
         User? Authenticate(HttpRequest request, IAuthenticationAttributes authAttribtues);
+
+        /// <summary>
+        /// Authenticate request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>Authorization information. Null if unauthenticated.</returns>
+        AuthorizationInfo Authenticate(HttpRequest request);
     }
 }
