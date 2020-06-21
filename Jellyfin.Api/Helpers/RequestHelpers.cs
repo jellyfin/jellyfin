f35774170fedd24697604923bd0d516b5dad9cd2
crobibero <cody@robibe.ro>
2020-06-21 16:12:21 -0600

    Move LiveTvService.cs to Jellyfin.Api

--- Jellyfin.Api/Helpers/RequestHelpers.cs
+++ Jellyfin.Api/Helpers/RequestHelpers.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
 using Jellyfin.Data.Enums;
 using MediaBrowser.Controller.Net;
 using MediaBrowser.Controller.Session;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Querying;
 using Microsoft.AspNetCore.Http;
 
 namespace Jellyfin.Api.Helpers
@@ -18,7 +21,7 @@ namespace Jellyfin.Api.Helpers
         /// <param name="separator">The char that separates the substrings.</param>
         /// <param name="removeEmpty">Option to remove empty substrings from the array.</param>
         /// <returns>An array of the substrings.</returns>
-        internal static string[] Split(string value, char separator, bool removeEmpty)
+        internal static string[] Split(string? value, char separator, bool removeEmpty)
         {
             if (string.IsNullOrWhiteSpace(value))
             {
@@ -72,6 +75,79 @@ namespace Jellyfin.Api.Helpers
             }
 
             return session;
+        }
+
+        /// <summary>
+        /// Gets the item fields.
+        /// </summary>
+        /// <param name="fields">The item field string.</param>
+        /// <returns>Array of parsed item fields.</returns>
+        internal static ItemFields[] GetItemFields(string fields)
+        {
+            if (string.IsNullOrEmpty(fields))
+            {
+                return Array.Empty<ItemFields>();
+            }
+
+            return Split(fields, ',', true)
+                .Select(v =>
+                {
+                    if (Enum.TryParse(v, true, out ItemFields value))
+                    {
+                        return (ItemFields?)value;
+                    }
+
+                    return null;
+                })
+                .Where(i => i.HasValue)
+                .Select(i => i!.Value)
+                .ToArray();
+        }
+
+        internal static Guid[] GetGuids(string? value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return Array.Empty<Guid>();
+            }
+
+            return Split(value, ',', true)
+                .Select(i => new Guid(i))
+                .ToArray();
+        }
+
+        internal static ValueTuple<string, SortOrder>[] GetOrderBy(string? sortBy, string? requestedSortOrder)
+        {
+            var val = sortBy;
+
+            if (string.IsNullOrEmpty(val))
+            {
+                return Array.Empty<ValueTuple<string, SortOrder>>();
+            }
+
+            var vals = val.Split(',');
+            if (string.IsNullOrWhiteSpace(requestedSortOrder))
+            {
+                requestedSortOrder = "Ascending";
+            }
+
+            var sortOrders = requestedSortOrder.Split(',');
+
+            var result = new ValueTuple<string, SortOrder>[vals.Length];
+
+            for (var i = 0; i < vals.Length; i++)
+            {
+                var sortOrderIndex = sortOrders.Length > i ? i : 0;
+
+                var sortOrderValue = sortOrders.Length > sortOrderIndex ? sortOrders[sortOrderIndex] : null;
+                var sortOrder = string.Equals(sortOrderValue, "Descending", StringComparison.OrdinalIgnoreCase)
+                    ? SortOrder.Descending
+                    : SortOrder.Ascending;
+
+                result[i] = new ValueTuple<string, SortOrder>(vals[i], sortOrder);
+            }
+
+            return result;
         }
     }
 }
---
+++ Jellyfin.Api/Models/LiveTvDtos/ChannelMappingOptionsDto.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using MediaBrowser.Controller.LiveTv;
+using MediaBrowser.Model.Dto;
+
+namespace Jellyfin.Api.Models.LiveTvDtos
+{
+    /// <summary>
+    /// Channel mapping options dto.
+    /// </summary>
+    public class ChannelMappingOptionsDto
+    {
+        /// <summary>
+        /// Gets or sets list of tuner channels.
+        /// </summary>
+        public List<TunerChannelMapping> TunerChannels { get; set; }
+
+        /// <summary>
+        /// Gets or sets list of provider channels.
+        /// </summary>
+        public List<NameIdPair> ProviderChannels { get; set; }
+
+        /// <summary>
+        /// Gets or sets list of mappings.
+        /// </summary>
+        public NameValuePair[] Mappings { get; set; }
+
+        /// <summary>
+        /// Gets or sets provider name.
+        /// </summary>
+        public string? ProviderName { get; set; }
+    }
+}
