72ffe7245b3b2f3ee931c4931b23bc5b25c2039e
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-07-05 19:48:02 +0100

    Update DeviceIcon.cs

--- Emby.Dlna/Common/DeviceIcon.cs
+++ Emby.Dlna/Common/DeviceIcon.cs
@@ -10,9 +10,9 @@ namespace Emby.Dlna.Common
 
         public string MimeType { get; set; } = string.Empty;
 
-        public int Width { get; set; } = 0;
+        public int Width { get; set; }
 
-        public int Height { get; set; } = 0;
+        public int Height { get; set; }
 
         public string Depth { get; set; } = string.Empty;
 
