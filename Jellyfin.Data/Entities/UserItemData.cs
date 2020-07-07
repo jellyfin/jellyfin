85de563de8d3808d3dd8c92b9e24b6c8b4f8ae55
Patrick Barron <barronpm@gmail.com>
2020-07-07 14:42:20 -0400

    Add documentation.

--- Jellyfin.Data/Entities/UserItemData.cs
+++ Jellyfin.Data/Entities/UserItemData.cs
@@ -3,35 +3,80 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Jellyfin.Data.Entities
 {
+    /// <summary>
+    /// An entity representing a user's metadata, stream settings, and watch state for a particular item.
+    /// </summary>
     public class UserItemData
     {
+        /// <summary>
+        /// The minimum rating value to be considered "liked".
+        /// </summary>
         public const double MinLikeValue = 6.5;
 
+        /// <summary>
+        /// Gets or sets the id of the user item data.
+        /// </summary>
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        public int Id { get; protected set; }
 
+        /// <summary>
+        /// Gets or sets the id of the user this entity is attached to.
+        /// </summary>
         public Guid UserId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the id of the item this entity is attached to.
+        /// </summary>
         public Guid ItemId { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether this media is played.
+        /// </summary>
         public bool IsPlayed { get; set; }
 
+        /// <summary>
+        /// Gets or sets the number of times this media has been played.
+        /// </summary>
         public int PlayCount { get; set; }
 
+        /// <summary>
+        /// Gets or sets the playback position ticks.
+        /// </summary>
         public long PlaybackPositionTicks { get; set; }
 
+        /// <summary>
+        /// Gets or sets the last played date.
+        /// </summary>
         public DateTime? LastPlayedDate { get; set; }
 
+        /// <summary>
+        /// Gets or sets the video stream index.
+        /// </summary>
         public int? VideoStreamIndex { get; set; }
 
+        /// <summary>
+        /// Gets or sets the audio stream index.
+        /// </summary>
         public int? AudioStreamIndex { get; set; }
 
+        /// <summary>
+        /// Gets or sets the subtitle stream index.
+        /// </summary>
         public int? SubtitleStreamIndex { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether this media is favorited by the user.
+        /// </summary>
         public bool IsFavorite { get; set; }
 
+        /// <summary>
+        /// Gets or sets the rating.
+        /// </summary>
         public float? Rating { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the user likes this media.
+        /// </summary>
         [NotMapped]
         public bool? Likes
         {
--- MediaBrowser.Controller/Library/IUserDataManager.cs
+++ MediaBrowser.Controller/Library/IUserDataManager.cs
@@ -18,21 +18,47 @@ namespace MediaBrowser.Controller.Library
         /// </summary>
         event EventHandler<UserDataSaveEventArgs> UserDataSaved;
 
+        /// <summary>
+        /// Gets the user item data for a specified user and item.
+        /// </summary>
+        /// <param name="userId">The user's id.</param>
+        /// <param name="itemId">The item's id.</param>
+        /// <returns>The user item data for that user and item.</returns>
         UserItemData GetUserItemData(Guid userId, Guid itemId);
 
+        /// <summary>
+        /// Saves the provided user item data.
+        /// </summary>
+        /// <param name="itemData">The user item data.</param>
+        /// <param name="reason">The save reason.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
         void SaveUserItemData(UserItemData itemData, UserDataSaveReason reason, CancellationToken cancellationToken);
 
         /// <summary>
         /// Gets the user data dto.
         /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="item">The item.</param>
         /// <returns>A dto containing the user's item data.</returns>
         UserItemDataDto GetUserDataDto(User user, BaseItem item);
 
+        /// <summary>
+        /// Gets a user item data dto for the provided user and item.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="item">The item.</param>
+        /// <param name="itemDto">The item's dto.</param>
+        /// <param name="dtoOptions">The dto options.</param>
+        /// <returns>A dto for the user item data.</returns>
         UserItemDataDto GetUserDataDto(User user, BaseItem item, BaseItemDto itemDto, DtoOptions dtoOptions);
 
         /// <summary>
-        /// Updates playstate for an item and returns true or false indicating if it was played to completion.
+        /// Updates the play state for an item.
         /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="data">The user item data.</param>
+        /// <param name="positionTicks">The new position tick value.</param>
+        /// <returns><c>true</c> if the item was played to completion.</returns>
         bool UpdatePlayState(BaseItem item, UserItemData data, long? positionTicks);
     }
 }
