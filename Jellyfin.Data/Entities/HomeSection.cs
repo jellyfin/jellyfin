3c7eb6b324ba9cf20244f136ee743bef91d4a8f3
Patrick Barron <barronpm@gmail.com>
2020-07-09 22:28:59 -0400

    Document HomeSection.cs

--- Jellyfin.Data/Entities/HomeSection.cs
+++ Jellyfin.Data/Entities/HomeSection.cs
@@ -1,21 +1,38 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Jellyfin.Data.Enums;
 
 namespace Jellyfin.Data.Entities
 {
+    /// <summary>
+    /// An entity representing a section on the user's home page.
+    /// </summary>
     public class HomeSection
     {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <remarks>
+        /// Identity. Required.
+        /// </remarks>
         [Key]
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; protected set; }
 
+        /// <summary>
+        /// Gets or sets the Id of the associated display preferences.
+        /// </summary>
         public int DisplayPreferencesId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the order.
+        /// </summary>
         public int Order { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
         public HomeSectionType Type { get; set; }
     }
 }
