0b82b64c399e56d1dacd460023978eaaed88b9aa
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-07-13 00:04:05 +0100

    Update HttpException.cs
    Add a field to hold onto webservice error response strings.

--- MediaBrowser.Model/Net/HttpException.cs
+++ MediaBrowser.Model/Net/HttpException.cs
@@ -15,6 +15,11 @@ namespace MediaBrowser.Model.Net
         public HttpStatusCode? StatusCode { get; set; }
 
         /// <summary>
+        /// Gets or sets the webservice response text that accompanied the error code.
+        /// </summary>
+        public string ResponseText { get; set; };
+        
+        /// <summary>
         /// Gets or sets a value indicating whether this instance is timed out.
         /// </summary>
         /// <value><c>true</c> if this instance is timed out; otherwise, <c>false</c>.</value>
