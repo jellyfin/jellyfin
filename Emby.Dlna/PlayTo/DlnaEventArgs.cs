8e8c4f37f033f9297a674bb66b8577a4c383b9dc
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-07-13 00:48:59 +0100

    Update DlnaEventArgs.cs
    Added XML comments.

--- Emby.Dlna/PlayTo/DlnaEventArgs.cs
+++ Emby.Dlna/PlayTo/DlnaEventArgs.cs
@@ -2,16 +2,28 @@ using System;
 
 namespace Emby.Dlna.PlayTo
 {
+    /// <summary>
+    /// Argument class for Dlna events.
+    /// </summary>
     public class DlnaEventArgs
     {
+        /// <summary>
+        /// Constructor for DlnaEventArgs.
+        /// </summary>
         public DlnaEventArgs(string id, string response)
         {
             Id = id;
             Response = response;
         }
 
+        /// <summary>
+        /// Gets the DLNA id of this request.
+        /// </summary>
         public string Id { get; }
 
+        /// <summary>
+        /// Gets the DNLA response string.
+        /// </summary>        
         public string Response { get; }
     }
 }
