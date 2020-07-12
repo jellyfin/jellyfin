eaee93b28d8c16d6040c9a570278b6488a0484ec
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-07-13 00:27:33 +0100

    Update DlnaEventArgs.cs
    Removed multiple usings.

--- Emby.Dlna/PlayTo/DlnaEventArgs.cs
+++ Emby.Dlna/PlayTo/DlnaEventArgs.cs
@@ -1,35 +1,8 @@
-﻿#pragma warning disable CS1591
-
 using System;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Emby.Dlna.Main;
-using MediaBrowser.Common.Extensions;
-using MediaBrowser.Common.Net;
-using MediaBrowser.Controller;
-using MediaBrowser.Controller.Configuration;
-using MediaBrowser.Controller.Dlna;
-using MediaBrowser.Controller.Drawing;
-using MediaBrowser.Controller.Library;
-using MediaBrowser.Controller.MediaEncoding;
-using MediaBrowser.Controller.Net;
-using MediaBrowser.Controller.Plugins;
-using MediaBrowser.Controller.Session;
-using MediaBrowser.Model.Dlna;
-using MediaBrowser.Model.Events;
-using MediaBrowser.Model.Globalization;
-using MediaBrowser.Model.Services;
-using MediaBrowser.Model.Session;
-using Microsoft.Extensions.Logging;
 
 namespace Emby.Dlna.PlayTo
 {
-    public class DlnaEventArgs // : IReturnVoid
+    public class DlnaEventArgs
     {
         public DlnaEventArgs(string id, string response)
         {
