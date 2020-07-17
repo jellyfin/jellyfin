6b4cea604ce9128b6d306e010035c14f6cb75ec4
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-07-17 16:19:06 +0100

    Suggested changes and removed some intellisense messages.

--- MediaBrowser.Model/Notifications/NotificationOption.cs
+++ MediaBrowser.Model/Notifications/NotificationOption.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CA1819 // Properties should not return arrays
 #pragma warning disable CS1591
 
 using System;
@@ -9,7 +10,6 @@ namespace MediaBrowser.Model.Notifications
         public NotificationOption(string type)
         {
             Type = type;
-
             DisabledServices = Array.Empty<string>();
             DisabledMonitorUsers = Array.Empty<string>();
             SendToUsers = Array.Empty<string>();
@@ -17,21 +17,20 @@ namespace MediaBrowser.Model.Notifications
 
         public NotificationOption()
         {
-            Type = string.Empty;
             DisabledServices = Array.Empty<string>();
             DisabledMonitorUsers = Array.Empty<string>();
             SendToUsers = Array.Empty<string>();
         }
 
-        public string Type { get; set; }
+        public string? Type { get; set; }
 
         /// <summary>
-        /// User Ids to not monitor (it's opt out).
+        /// Gets or sets user Ids to not monitor (it's opt out).
         /// </summary>
         public string[] DisabledMonitorUsers { get; set; }
 
         /// <summary>
-        /// User Ids to send to (if SendToUserMode == Custom)
+        /// Gets or sets user Ids to send to (if SendToUserMode == Custom).
         /// </summary>
         public string[] SendToUsers { get; set; }
 
