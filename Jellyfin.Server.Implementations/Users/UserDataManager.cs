66e4bd63e57504011e74e8c2470bf89803099b58
Patrick Barron <barronpm@gmail.com>
2020-07-28 21:58:23 -0400

    Remove unused using directive.

--- Jellyfin.Server.Implementations/Users/UserDataManager.cs
+++ Jellyfin.Server.Implementations/Users/UserDataManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using System.Threading;
 using Jellyfin.Data.Entities;
 using MediaBrowser.Controller.Configuration;
