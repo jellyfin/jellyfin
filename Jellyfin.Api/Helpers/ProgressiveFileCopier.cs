5c4b342323bdf637738c3c87efcce625ae748f34
crobibero <cody@robibe.ro>
2020-07-29 14:21:32 -0600

    fix boolean

--- Jellyfin.Api/Helpers/ProgressiveFileCopier.cs
+++ Jellyfin.Api/Helpers/ProgressiveFileCopier.cs
@@ -84,7 +84,7 @@ namespace Jellyfin.Api.Helpers
                 var allowAsyncFileRead = false;
 
                 // use non-async filestream along with read due to https://github.com/dotnet/corefx/issues/6039
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
                     fileOptions |= FileOptions.Asynchronous;
                     allowAsyncFileRead = true;
