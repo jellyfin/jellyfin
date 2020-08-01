c5d8c19f43bc87c4b624652c2abb06e6ad4de2e7
crobibero <cody@robibe.ro>
2020-07-31 20:43:25 -0600

    add basic logger

--- Jellyfin.Api/WebSockets/LegacyWebSocketHandler.cs
+++ Jellyfin.Api/WebSockets/LegacyWebSocketHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Api.WebSockets
 {
@@ -9,13 +10,17 @@ namespace Jellyfin.Api.WebSockets
     /// </summary>
     public class LegacyWebSocketHandler : BaseWebSocketHandler
     {
+        private readonly ILogger<LegacyWebSocketHandler> _logger;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="LegacyWebSocketHandler"/> class.
         /// </summary>
         /// <param name="webSocketConnectionManager">Instance of the <see cref="WebSocketConnectionManager"/> singleton.</param>
-        public LegacyWebSocketHandler(WebSocketConnectionManager webSocketConnectionManager)
+        /// <param name="logger">Instance of the <see cref="ILogger{LegacyWebSocketHandler}"/> interface.</param>
+        public LegacyWebSocketHandler(WebSocketConnectionManager webSocketConnectionManager, ILogger<LegacyWebSocketHandler> logger)
             : base(webSocketConnectionManager)
         {
+            _logger = logger;
         }
 
         /// <inheritdoc />
@@ -24,6 +29,7 @@ namespace Jellyfin.Api.WebSockets
             await base.OnConnected(socket).ConfigureAwait(false);
 
             var socketId = WebSocketConnectionManager.GetId(socket);
+            _logger.LogInformation("{socketId} is now connected", socketId);
             await SendMessageToAllAsync($"{socketId} is now connected").ConfigureAwait(false);
         }
 
@@ -31,7 +37,9 @@ namespace Jellyfin.Api.WebSockets
         public override async Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
         {
             var socketId = WebSocketConnectionManager.GetId(socket);
-            var message = $"{socketId} said: {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+            var messageStr = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            var message = $"{socketId} said: {messageStr}";
+            _logger.LogInformation("{socketId} said {message}", messageStr);
 
             await SendMessageToAllAsync(message).ConfigureAwait(false);
         }
