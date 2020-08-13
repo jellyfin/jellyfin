3ee940f7b306884b748fa06d04f80e8db241bfba
Patrick Barron <barronpm@gmail.com>
2020-08-13 19:14:03 -0400

    Clean up GenericEventArgs

--- MediaBrowser.Model/Events/GenericEventArgs.cs
+++ MediaBrowser.Model/Events/GenericEventArgs.cs
@@ -5,15 +5,9 @@ namespace MediaBrowser.Model.Events
     /// <summary>
     /// Provides a generic EventArgs subclass that can hold any kind of object.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The type of this event.</typeparam>
     public class GenericEventArgs<T> : EventArgs
     {
-        /// <summary>
-        /// Gets or sets the argument.
-        /// </summary>
-        /// <value>The argument.</value>
-        public T Argument { get; set; }
-
         /// <summary>
         /// Initializes a new instance of the <see cref="GenericEventArgs{T}"/> class.
         /// </summary>
@@ -22,5 +16,11 @@ namespace MediaBrowser.Model.Events
         {
             Argument = arg;
         }
+
+        /// <summary>
+        /// Gets the argument.
+        /// </summary>
+        /// <value>The argument.</value>
+        public T Argument { get; }
     }
 }
