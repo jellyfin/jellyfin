285af300f50c8380ef04974ab29881cf0e5d600b
Patrick Barron <barronpm@gmail.com>
2020-08-13 20:29:14 -0400

    Create IEventConsumer and IEventManager

---
+++ MediaBrowser.Controller/Events/IEventConsumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MediaBrowser.Controller.Events
+{
+    public interface IEventConsumer<in T>
+        where T : EventArgs
+    {
+        Task OnEvent(T eventArgs);
+    }
+}
---
+++ MediaBrowser.Controller/Events/IEventManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MediaBrowser.Controller.Events
+{
+    /// <summary>
+    /// An interface that handles eventing.
+    /// </summary>
+    public interface IEventManager
+    {
+        /// <summary>
+        /// Publishes an event.
+        /// </summary>
+        /// <param name="eventArgs">The event arguments.</param>
+        /// <typeparam name="T">The type of event.</typeparam>
+        /// <returns>A task representing the publishing of the event.</returns>
+        Task Publish<T>(T eventArgs)
+            where T : EventArgs;
+    }
+}
