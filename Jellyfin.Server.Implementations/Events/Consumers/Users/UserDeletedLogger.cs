4d18dbf9de7346c6620a6f9fad7e8f09c13dc68c
Patrick Barron <barronpm@gmail.com>
2020-08-15 03:35:42 -0400

    Document UserDeletedLogger

--- Jellyfin.Server.Implementations/Events/Consumers/Users/UserDeletedLogger.cs
+++ Jellyfin.Server.Implementations/Events/Consumers/Users/UserDeletedLogger.cs
@@ -9,17 +9,26 @@ using MediaBrowser.Model.Globalization;
 
 namespace Jellyfin.Server.Implementations.Events.Consumers.Users
 {
+    /// <summary>
+    /// Adds an entry to the activity log when a user is deleted.
+    /// </summary>
     public class UserDeletedLogger : IEventConsumer<UserDeletedEventArgs>
     {
         private readonly ILocalizationManager _localizationManager;
         private readonly IActivityManager _activityManager;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserDeletedLogger"/> class.
+        /// </summary>
+        /// <param name="localizationManager">The localization manager.</param>
+        /// <param name="activityManager">The activity manager.</param>
         public UserDeletedLogger(ILocalizationManager localizationManager, IActivityManager activityManager)
         {
             _localizationManager = localizationManager;
             _activityManager = activityManager;
         }
 
+        /// <inheritdoc />
         public async Task OnEvent(UserDeletedEventArgs eventArgs)
         {
             await _activityManager.CreateAsync(new ActivityLog(
