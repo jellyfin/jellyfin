b549633c9d94ff5fa943a41a9ff12af0e079528d
Bond_009 <bond.009@outlook.com>
2020-08-21 22:19:16 +0200

    Fix build

--- MediaBrowser.Controller/Playlists/IPlaylistManager.cs
+++ MediaBrowser.Controller/Playlists/IPlaylistManager.cs
@@ -29,7 +29,7 @@ namespace MediaBrowser.Controller.Playlists
         /// <param name="itemIds">The item ids.</param>
         /// <param name="userId">The user identifier.</param>
         /// <returns>Task.</returns>
-        Task AddToPlaylistAsync(string playlistId, ICollection<Guid> itemIds, Guid userId);
+        Task AddToPlaylistAsync(Guid playlistId, ICollection<Guid> itemIds, Guid userId);
 
         /// <summary>
         /// Removes from playlist.
