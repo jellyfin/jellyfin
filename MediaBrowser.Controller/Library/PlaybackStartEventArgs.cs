2eb9a3670bc63c06c464c487eb3d7b59de310dfe
Patrick Barron <barronpm@gmail.com>
2020-08-24 20:34:42 -0400

    Document PlaybackStartEventArgs

--- MediaBrowser.Controller/Library/PlaybackStartEventArgs.cs
+++ MediaBrowser.Controller/Library/PlaybackStartEventArgs.cs
@@ -1,5 +1,8 @@
 ﻿namespace MediaBrowser.Controller.Library
 {
+    /// <summary>
+    /// An event that occurs when playback is started.
+    /// </summary>
     public class PlaybackStartEventArgs : PlaybackProgressEventArgs
     {
     }
