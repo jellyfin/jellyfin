0f6ea123ea1ead19e6efc915e6af62dc4a198329
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-09-05 20:55:42 +0100

    Update ApplicationHost.cs

--- Emby.Server.Implementations/ApplicationHost.cs
+++ Emby.Server.Implementations/ApplicationHost.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
