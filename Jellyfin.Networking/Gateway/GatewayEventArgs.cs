c6a12d04330193a3f1f57f021ff71b8dc1901ea0
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-09-07 22:38:07 +0100

    Create GatewayEventArgs.cs

---
+++ Jellyfin.Networking/Gateway/GatewayEventArgs.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Jellyfin.Networking.Gateway
+{
+    /// <summary>
+    /// EventArgs class.
+    /// </summary>
+    public class GatewayEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GatewayEventArgs"/> class.
+        /// </summary>
+        /// <param name="gateway">Gateway address.</param>
+        /// <param name="status">Status of the ping.</param>
+        public GatewayEventArgs(IPAddress gateway, IPStatus status)
+        {
+            Status = status;
+            Gateway = gateway;
+        }
+
+        /// <summary>
+        /// Gets the status.
+        /// </summary>
+        public IPStatus Status { get; }
+
+        /// <summary>
+        /// Gets the gateway.
+        /// </summary>
+        public IPAddress Gateway { get; }
+    }
+}
