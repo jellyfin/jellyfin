9c4c60689512749ee33b09a518e3b05fad911c5c
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-09-08 09:37:24 +0100

    Add files via upload

---
+++ MediaBrowser.Model/Configuration/PathSubstitution.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+#pragma warning disable CS1591
+#pragma warning disable CA1819
+
+using System;
+using System.Collections.Generic;
+using MediaBrowser.Model.Dto;
+using MediaBrowser.Model.Updates;
+
+namespace MediaBrowser.Model.Configuration
+{
+
+    public class PathSubstitution
+    {
+        public string From { get; set; } = string.Empty;
+
+        public string To { get; set; } = string.Empty;
+    }
+}
