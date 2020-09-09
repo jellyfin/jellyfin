a050141bee101642c8469e4da4801d7ef3fef8cb
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-09-09 11:35:22 +0100

    Add files via upload
    Commented code

--- MediaBrowser.Model/Dlna/Argument.cs
+++ MediaBrowser.Model/Dlna/Argument.cs
@@ -1,13 +1,23 @@
-#pragma warning disable CS1591
-
 namespace MediaBrowser.Model.Dlna
 {
+    /// <summary>
+    /// DLNA Query parameter type, used when quering DLNA devices via SOAP.
+    /// </summary>
     public class Argument
     {
+        /// <summary>
+        /// Gets or sets name of the DLNA argument.
+        /// </summary>
         public string Name { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Gets or sets the direction of the parameter.
+        /// </summary>
         public string Direction { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Gets or sets the related DLNA state variable for this argument.
+        /// </summary>
         public string RelatedStateVariable { get; set; } = string.Empty;
     }
 }
