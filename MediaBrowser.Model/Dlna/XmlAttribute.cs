68de105dc21f4d1114a3b1db81f177caae747ef6
Jim Cartlidge <jimcartlidge@yahoo.co.uk>
2020-09-13 13:49:11 +0100

    Comments part 1

--- MediaBrowser.Model/Dlna/XmlAttribute.cs
+++ MediaBrowser.Model/Dlna/XmlAttribute.cs
@@ -5,11 +5,20 @@ using System.Xml.Serialization;
 
 namespace MediaBrowser.Model.Dlna
 {
+    /// <summary>
+    /// Defines the <see cref="XmlAttribute" />.
+    /// </summary>
     public class XmlAttribute
     {
+        /// <summary>
+        /// Gets or sets the name of the attribute.
+        /// </summary>
         [XmlAttribute("name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the value of the attribute.
+        /// </summary>
         [XmlAttribute("value")]
         public string Value { get; set; }
     }
