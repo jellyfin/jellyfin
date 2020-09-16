2dbf73b98962bcab1de06f8699e621e4e75a96fb
Bond_009 <bond.009@outlook.com>
2020-09-16 14:16:44 +0200

    Minor improvements

--- Jellyfin.Api/Controllers/DynamicHlsController.cs
+++ Jellyfin.Api/Controllers/DynamicHlsController.cs
@@ -1144,30 +1144,30 @@ namespace Jellyfin.Api.Controllers
 
             var builder = new StringBuilder();
 
-            builder.AppendLine("#EXTM3U");
-            builder.AppendLine("#EXT-X-PLAYLIST-TYPE:VOD");
-            builder.AppendLine("#EXT-X-VERSION:3");
-            builder.AppendLine("#EXT-X-TARGETDURATION:" + Math.Ceiling(segmentLengths.Length > 0 ? segmentLengths.Max() : state.SegmentLength).ToString(CultureInfo.InvariantCulture));
-            builder.AppendLine("#EXT-X-MEDIA-SEQUENCE:0");
+            builder.AppendLine("#EXTM3U")
+                .AppendLine("#EXT-X-PLAYLIST-TYPE:VOD")
+                .AppendLine("#EXT-X-VERSION:3")
+                .Append("#EXT-X-TARGETDURATION:")
+                .Append(Math.Ceiling(segmentLengths.Length > 0 ? segmentLengths.Max() : state.SegmentLength))
+                .AppendLine()
+                .AppendLine("#EXT-X-MEDIA-SEQUENCE:0");
 
-            var queryString = Request.QueryString;
             var index = 0;
-
             var segmentExtension = GetSegmentFileExtension(streamingRequest.SegmentContainer);
+            var queryString = Request.QueryString;
 
             foreach (var length in segmentLengths)
             {
-                builder.AppendLine("#EXTINF:" + length.ToString("0.0000", CultureInfo.InvariantCulture) + ", nodesc");
-                builder.AppendLine(
-                    string.Format(
-                        CultureInfo.InvariantCulture,
-                        "hls1/{0}/{1}{2}{3}",
-                        name,
-                        index.ToString(CultureInfo.InvariantCulture),
-                        segmentExtension,
-                        queryString));
-
-                index++;
+                builder.Append("#EXTINF:")
+                    .Append(length.ToString("0.0000", CultureInfo.InvariantCulture))
+                    .Append(", nodesc")
+                    .Append("hls1/")
+                    .Append(name)
+                    .Append('/')
+                    .Append(index++)
+                    .Append(segmentExtension)
+                    .Append(queryString)
+                    .AppendLine();
             }
 
             builder.AppendLine("#EXT-X-ENDLIST");
