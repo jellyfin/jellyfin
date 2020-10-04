fc28ffea53861ea7e8c35d71dcf863e4900f4d8f
Joshua M. Boniface <joshua@boniface.me>
2020-10-04 19:03:39 -0400

    Improve handling of apiclient generator
    1. Replace the separate stable/unstable scripts with a single script and
    handle the difference with argument parsing.
    2. Unify the calls and pass the Azure arguments in on the CLI.

--- apiclient/templates/typescript/axios/unstable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-java -jar openapi-generator-cli.jar generate \
-    --input-spec $(System.ArtifactsDirectory)/openapispec/openapi.json \
-    --generator-name typescript-axios \
-    --output ./apiclient/generated/typescript/axios  \
-    --template-dir ./apiclient/templates/typescript/axios \
-    --ignore-file-override ./apiclient/.openapi-generator-ignore \
-    --additional-properties=useSingleRequestParameter="true",withSeparateModelsAndApi="true",npmName="axios",snapshotVersion="-SNAPSHOT.$(Build.BuildNumber)",npmRepository="https://pkgs.dev.azure.com/jellyfin-project/jellyfin/_packaging/unstable/npm/registry/"
