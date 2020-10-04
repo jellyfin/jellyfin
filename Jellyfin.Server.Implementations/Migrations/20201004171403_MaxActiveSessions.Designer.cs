b9d79d7635aa39052244f356e699bc125c88bc34
Joshua M. Boniface <joshua@boniface.me>
2020-10-04 13:14:32 -0400

    Add DB migration for new MaxActiveSessions field

--- Jellyfin.Server.Implementations/Migrations/20201004171403_MaxActiveSessions.Designer.cs
+++ Jellyfin.Server.Implementations/Migrations/20201004171403_MaxActiveSessions.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Jellyfin.Server.Implementations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Jellyfin.Server.Implementations.Migrations
 {
     [DbContext(typeof(JellyfinDb))]
-    partial class JellyfinDbModelSnapshot : ModelSnapshot
+    [Migration("20201004171403_MaxActiveSessions")]
+    partial class MaxActiveSessions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Jellyfin.Server.Implementations/Migrations/20201004171403_MaxActiveSessions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Jellyfin.Server.Implementations.Migrations
+{
+    public partial class MaxActiveSessions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxActiveSessions",
+                schema: "jellyfin",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxActiveSessions",
+                schema: "jellyfin",
+                table: "Users");
+        }
+    }
+}
