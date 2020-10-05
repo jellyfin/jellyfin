546c87d20b8710b7fc492d591c27404bfc2a155f
Jim Cartlidge <jimcartlidge@yahoo.co.uk>
2020-10-05 13:37:03 +0100

    Updated comment

--- Jellyfin.Networking/Manager/INetworkManager.cs
+++ Jellyfin.Networking/Manager/INetworkManager.cs
@@ -103,7 +103,7 @@ namespace Jellyfin.Networking.Manager
         /// <summary>
         /// Retrieves the bind address to use in system url's. (Server Discovery, PlayTo, LiveTV, SystemInfo)
         /// If no bind addresses are specified, an internal interface address is selected.
-        /// (See above).
+        /// (See <see cref="GetBindInterface(IPObject, out int?)"/>.
         /// </summary>
         /// <param name="source">IP address of the request.</param>
         /// <param name="port">Optional port returned, if it's part of an override.</param>
@@ -113,7 +113,7 @@ namespace Jellyfin.Networking.Manager
         /// <summary>
         /// Retrieves the bind address to use in system url's. (Server Discovery, PlayTo, LiveTV, SystemInfo)
         /// If no bind addresses are specified, an internal interface address is selected.
-        /// (See above).
+        /// (See <see cref="GetBindInterface(IPObject, out int?)"/>.
         /// </summary>
         /// <param name="source">Source of the request.</param>
         /// <param name="port">Optional port returned, if it's part of an override.</param>
