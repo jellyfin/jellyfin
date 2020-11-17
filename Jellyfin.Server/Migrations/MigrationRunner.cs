1831b6cbab0781a0d8f4bafc9e018179db0e19a5
crobibero <cody@robibe.ro>
2020-11-17 16:39:47 -0700

    Remove tvdb migration.

--- Jellyfin.Server/Migrations/MigrationRunner.cs
+++ Jellyfin.Server/Migrations/MigrationRunner.cs
@@ -24,8 +24,7 @@ namespace Jellyfin.Server.Migrations
             typeof(Routines.MigrateUserDb),
             typeof(Routines.ReaddDefaultPluginRepository),
             typeof(Routines.MigrateDisplayPreferencesDb),
-            typeof(Routines.RemoveDownloadImagesInAdvance),
-            typeof(Routines.DownloadTheTvdbPlugin)
+            typeof(Routines.RemoveDownloadImagesInAdvance)
         };
 
         /// <summary>
