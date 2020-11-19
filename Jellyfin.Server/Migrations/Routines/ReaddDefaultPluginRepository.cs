14fc7e6408aa35c7a87470f3cd2fc87971f7c442
Greenback <jimcartlidge@yahoo.co.uk>
2020-11-19 15:40:20 +0000

    revert changes.

--- Jellyfin.Server/Migrations/Routines/ReaddDefaultPluginRepository.cs
+++ Jellyfin.Server/Migrations/Routines/ReaddDefaultPluginRepository.cs
@@ -14,7 +14,7 @@ namespace Jellyfin.Server.Migrations.Routines
         private readonly RepositoryInfo _defaultRepositoryInfo = new RepositoryInfo
         {
             Name = "Jellyfin Stable",
-            Url = "https://repo.jellyfin.org/releases/plugin/manifest-stable.json",
+            Url = "https://repo.jellyfin.org/releases/plugin/manifest-stable.json"
         };
 
         /// <summary>
