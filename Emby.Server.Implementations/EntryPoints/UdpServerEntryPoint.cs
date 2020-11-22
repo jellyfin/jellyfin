8e7304b4972ca9d455a75715bf3677ba07a5b8d8
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-11-22 19:29:23 +0000

    Update UdpServerEntryPoint.cs

--- Emby.Server.Implementations/EntryPoints/UdpServerEntryPoint.cs
+++ Emby.Server.Implementations/EntryPoints/UdpServerEntryPoint.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using Emby.Server.Implementations.Udp;
-using Jellyfin.Networking.Configuration;
-using MediaBrowser.Common.Configuration;
 using MediaBrowser.Controller;
-using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -23,52 +19,45 @@ namespace Emby.Server.Implementations.EntryPoints
         /// </summary>
         public const int PortNumber = 7359;
 
+        /// <summary>
+        /// The logger.
+        /// </summary>
         private readonly ILogger<UdpServerEntryPoint> _logger;
         private readonly IServerApplicationHost _appHost;
         private readonly IConfiguration _config;
-        private readonly IServerConfigurationManager _serverConfiguration;
+
+        /// <summary>
+        /// The UDP server.
+        /// </summary>
         private UdpServer _udpServer;
         private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private bool _disposed = false;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="UdpServerEntryPoint"/> class.
+        /// Initializes a new instance of the <see cref="UdpServerEntryPoint" /> class.
         /// </summary>
-        /// <param name="logger">The <see cref="ILogger{UdpServerEntryPoint}"/>.</param>
-        /// <param name="appHost">The <see cref="IServerApplicationHost"/>.</param>
-        /// <param name="configuration">The <see cref="IConfiguration"/>.</param>
-        /// <param name="serverConfiguration">The <see cref="IServerConfigurationManager"/>.</param>
         public UdpServerEntryPoint(
             ILogger<UdpServerEntryPoint> logger,
             IServerApplicationHost appHost,
-            IConfiguration configuration,
-            IServerConfigurationManager serverConfiguration)
+            IConfiguration configuration)
         {
             _logger = logger;
             _appHost = appHost;
             _config = configuration;
-            _serverConfiguration = serverConfiguration;
-
-            _serverConfiguration.NamedConfigurationUpdated += OnNamedConfigurationUpdated;
-        }
-
-        /// <summary>
-        /// The OnNamedConfigurationUpdated.
-        /// </summary>
-        /// <param name="sender">The <see cref="object"/>.</param>
-        /// <param name="e">The e<see cref="ConfigurationUpdateEventArgs"/>.</param>
-        private void OnNamedConfigurationUpdated(object sender, ConfigurationUpdateEventArgs e)
-        {
-            if (string.Equals(e.Key, "dlna", StringComparison.OrdinalIgnoreCase))
-            {
-                ReloadComponent();
-            }
         }
 
         /// <inheritdoc />
         public Task RunAsync()
         {
-            ReloadComponent();
+            try
+            {
+                _udpServer = new UdpServer(_logger, _appHost, _config);
+                _udpServer.Start(PortNumber, _cancellationTokenSource.Token);
+            }
+            catch (SocketException ex)
+            {
+                _logger.LogWarning(ex, "Unable to start AutoDiscovery listener on UDP port {PortNumber}", PortNumber);
+            }
 
             return Task.CompletedTask;
         }
@@ -81,39 +70,13 @@ namespace Emby.Server.Implementations.EntryPoints
                 return;
             }
 
-            _serverConfiguration.NamedConfigurationUpdated -= OnNamedConfigurationUpdated;
             _cancellationTokenSource.Cancel();
-            if (_udpServer != null)
-            {
-                _udpServer.Dispose();
-            }
-
+            _udpServer.Dispose();
             _cancellationTokenSource.Dispose();
             _cancellationTokenSource = null;
             _udpServer = null;
 
             _disposed = true;
-        }
-
-        private void ReloadComponent()
-        {
-            if (_serverConfiguration.GetNetworkConfiguration().AutoDiscovery)
-            {
-                try
-                {
-                    _udpServer = new UdpServer(_logger, _appHost, _config);
-                    _udpServer.Start(PortNumber, _cancellationTokenSource.Token);
-                }
-                catch (SocketException ex)
-                {
-                    _logger.LogWarning(ex, "Unable to start AutoDiscovery listener on UDP port {PortNumber}", PortNumber);
-                }
-            }
-            else if (_udpServer != null)
-            {
-                _udpServer.Dispose();
-                _udpServer = null;
-            }
         }
     }
 }
