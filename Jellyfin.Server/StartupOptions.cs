ef36bbc318183420b496b6470090c3fb2ec9f23a
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-11-24 17:41:39 +0000

    Update StartupOptions.cs

--- Jellyfin.Server/StartupOptions.cs
+++ Jellyfin.Server/StartupOptions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using CommandLine;
 using Emby.Server.Implementations;
 using Emby.Server.Implementations.EntryPoints;
-using Emby.Server.Implementations.Udp;
 using Emby.Server.Implementations.Updates;
 using MediaBrowser.Controller.Extensions;
 
