a8fe9480009aa9c122340074429c9ab7e567c994
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-11-27 14:02:11 +0000

    Update SharedCodeManager.cs

--- DesignConcept/SharedCodeManager.cs
+++ DesignConcept/SharedCodeManager.cs
@@ -9,9 +9,8 @@ namespace Plugins
         /// </summary>
         /// <param name="serviceCollection">The <see cref="IServiceCollection"/>.</param>
         /// <param name="interfaceType">The <see cref="Type"/> to ensure is loaded.</param>
-        /// <param name="path">Optional path to the dll to load.</param>
-        /// <param name="callerAssembly">Assembly calling this method.</param>
-        
+        /// <param name="path">Optional path to where the dll is that may need loading.</param>
+        /// <param name="callerAssembly">The Assembly to be transmuted, normally the calling assembly.</param>
         public static void RegisterSharedCode(
             IServiceCollection serviceCollection, 
             Type interfaceType, 
@@ -23,7 +22,7 @@ namespace Plugins
             
             if (classRegistered.count == 0)
             {
-                // Load the assembly, as it hasn't been found.
+                // No it hasn't som load the assembly.
                 var ass = System.Runtime.Loader.AssemblyLoadContext.Default.LoadFromAssemblyPath(PluginPath + path);
 
                 // Extract all the objects from the registry.
@@ -37,27 +36,32 @@ namespace Plugins
                     throw new Exception(ex, "Error loading types from {Assembly}.", ass.FullName);
                 }
 
-                /// Register each class implimenting SharedCode attribute (DI in shared code).                
-                var sharedCode = GetSharedCodeAssets(ass);
+                /// Register each class with the attribute SharedCodeAttribute into the DI. Basically, extending the DI into the shared code.
+                var sharedCode = GetSharedCodeAssets(ass, false);
                 foreach (var (interfaceToRegisterAgainst, type) in sharedCode)
                 {   
+                    if (interfaceToRegisterAgainst == null)
+                    {
+                        throw new Exception("Cannot locate {interfaceName} in {Assembly}", (string)customAttrib[0], ass.FullName);
+                    }
+
                     // Add loaded assembly's attributed interfaces to the assembly.
                     serviceCollection.AddSingleton(interfaceToRegisterAgainst, type);
                 }
             }
 
-            // Transmute all sharedCode to loaded assembly.
-            var codeBase = GetSharedCode(callerAssembly);
+            // Get a list of all interfaces with the attribute SharedCodeAttribute in the calling assembly.
+            var codeBase = GetSharedCodeAssets(callerAssembly, true);
             foreach (var (interfaceToRegisterAgainst, _) in sharedCode)
             {
-                // how do we know which interfaces to repoint?
+                // Lookup each one in the serviceCollection to see where we need to transmute them to.
                 foreach (var service in serviceCollection)
                 {
                     // Possible issue: requires unique interfaces across namespaces. 
                     // TODO: look at interface assembly path to see if it ends with <path>
                     if (string.Equals(service.ServiceType.Type.FullName, interfaceToRegisterAgainst))
                     {
-                        // Point out interfaces to the loaded assembly interface.
+                        // Transmute the interfaces, so that they now point to the ones into the loaded assembly.
                         interfaceToRegisterAgainst.Transmute(service.ServiceType.Type);
                     }
                 }
@@ -65,20 +69,24 @@ namespace Plugins
 
         }
 
-        private IEnumerable<(Type t, Type c)>GetSharedCodeAssets(Assembly ass)
+        /// <summary>
+        /// Returns the with the attribute SharedCodeAttribute in an assembly.
+        /// </summary>
+        private IEnumerable<(Type t, Type c)>GetSharedCodeAssets(Assembly ass, bool interfaceOnly)
         {
-            foreach (Type type in ass.GetTypes()) 
+            var types = ass.GetTypes();
+            if (interfaceOnly)
+            {
+                types = types.Where(p => p.IsInterface);
+            }
+                                     
+            foreach (Type type in types) 
             {
                 var customAttrib = type.GetCustomAttributes(typeof(SharedCodeAttribute), true);
                 if (customAttrib.Length > 0) 
                 {
                     // Extract the interfaceName property from the attribute, to decide which type to register it against.
                     var interfaceToRegisterAgainst = ass.GetTypes().Where(i = string.Equals(i.FullName, (string)customAttrib[0]).FirstOrDefault();
-                    if (interfaceToRegisterAgainst == null)
-                    {
-                        throw new Exception("Cannot locate {interfaceName} in {Assembly}", (string)customAttrib[0], ass.FullName);
-                    }
-                    
                     yield (interfaceToRegisterAgainst, type);
                 }
             }
