a935869abd6631fa39de03179bb9880523deb4d7
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-11-27 13:51:01 +0000

    Update SampleSharedCodeExample.cs

--- DesignConcept/SampleSharedCodeExample.cs
+++ DesignConcept/SampleSharedCodeExample.cs
@@ -1,10 +1,15 @@
 namespace Test
 {
+    /// This code would be part of the dll that is loaded as required.
+    
+    /// This is the actual interface that will be used by the plugins. As such, it doesn't need any attribute.
+    /// It's the interface that is matched with the one provided in the SharedCodeAttribute.
     public interface IRequiredInTwoPlugins
     {
         string value { get; set; }
     }
 
+    /// This is a sample shared code class.    
     [ SharedCodeAttribute("IRequiredInTwoPlugins"]
     public class RequiredInTwoPlugins : IRequiredInTwoPlugins, IOtherInterfaceNotToUse
     {
