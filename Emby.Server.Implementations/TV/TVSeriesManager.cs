c196ad3351e07d21ceef8f932129ab0e8d43a9b4
MrTimscampi <julien.machiels@protonmail.com>
2020-11-27 10:25:43 +0100

    Don't return first episodes in next up

--- Emby.Server.Implementations/TV/TVSeriesManager.cs
+++ Emby.Server.Implementations/TV/TVSeriesManager.cs
@@ -146,23 +146,10 @@ namespace Emby.Server.Implementations.TV
             var allNextUp = seriesKeys
                 .Select(i => GetNextUp(i, currentUser, dtoOptions));
 
-            // allNextUp = allNextUp.OrderByDescending(i => i.Item1);
-
-            // If viewing all next up for all series, remove first episodes
-            // But if that returns empty, keep those first episodes (avoid completely empty view)
-            var alwaysEnableFirstEpisode = !string.IsNullOrEmpty(request.SeriesId);
-            var anyFound = false;
-
             return allNextUp
                 .Where(i =>
                 {
-                    if (alwaysEnableFirstEpisode || i.Item1 != DateTime.MinValue)
-                    {
-                        anyFound = true;
-                        return true;
-                    }
-
-                    if (!anyFound && i.Item1 == DateTime.MinValue)
+                    if (i.Item1 != DateTime.MinValue)
                     {
                         return true;
                     }
