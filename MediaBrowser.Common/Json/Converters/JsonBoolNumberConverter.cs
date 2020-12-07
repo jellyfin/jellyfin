66a1880a580b25db7add41ed3947166b10415f37
crobibero <cody@robibe.ro>
2020-12-06 20:26:21 -0700

    Add number to bool json converter

---
+++ MediaBrowser.Common/Json/Converters/JsonBoolNumberConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MediaBrowser.Common.Json.Converters
+{
+    /// <summary>
+    /// Converts a number to a boolean.
+    /// This is needed for HDHomerun.
+    /// </summary>
+    /// <remarks>
+    /// Adding this to the JsonConverter list causes recursion.
+    /// </remarks>
+    public class JsonBoolNumberConverter : JsonConverter<bool>
+    {
+        /// <inheritdoc />
+        public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Number)
+            {
+                return Convert.ToBoolean(reader.GetInt32());
+            }
+
+            return JsonSerializer.Deserialize<bool>(ref reader, options);
+        }
+
+        /// <inheritdoc />
+        public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, value, options);
+        }
+    }
+}
---
+++ tests/Jellyfin.Common.Tests/Json/JsonBoolNumberTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using Jellyfin.Common.Tests.Models;
+using Xunit;
+
+namespace Jellyfin.Common.Tests.Json
+{
+    public static class JsonBoolNumberTests
+    {
+        [Theory]
+        [InlineData("1", true)]
+        [InlineData("0", false)]
+        [InlineData("2", true)]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        public static void Deserialize_Number_Valid_Success(string input, bool? output)
+        {
+            var inputJson = $"{{ \"Value\": {input} }}";
+            var options = new JsonSerializerOptions();
+            var value = JsonSerializer.Deserialize<BoolTypeModel>(inputJson, options);
+            Assert.Equal(value?.Value, output);
+        }
+    }
+}
