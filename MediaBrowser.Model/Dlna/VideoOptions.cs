42211693daa4535d4e03a577a2af5dd626191e7e
BaronGreenback <jimcartlidge@yahoo.co.uk>
2020-12-30 21:20:59 +0000

    commented

--- MediaBrowser.Model/Dlna/VideoOptions.cs
+++ MediaBrowser.Model/Dlna/VideoOptions.cs
@@ -1,3 +1,6 @@
+using System;
+using MediaBrowser.Model.Dto;
+
 namespace MediaBrowser.Model.Dlna
 {
     /// <summary>
@@ -5,6 +8,19 @@ namespace MediaBrowser.Model.Dlna
     /// </summary>
     public class VideoOptions : AudioOptions
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VideoOptions"/> class.
+        /// </summary>
+        /// <param name="itemId">The <see cref="Guid"/>.</param>
+        /// <param name="source">An array of <see cref="MediaSourceInfo"/>.</param>
+        /// <param name="profile">A <seealso cref="DeviceProfile"/>.</param>
+        /// <param name="deviceId">The device id.</param>
+        /// <param name="maxBitRate">The optional maximum bit rate.</param>
+        public VideoOptions(Guid itemId, MediaSourceInfo[] source, DeviceProfile profile, string deviceId, int? maxBitRate)
+        : base(itemId, source, profile, deviceId, maxBitRate)
+        {
+        }
+
         /// <summary>
         /// Gets or sets the audio stream index.
         /// </summary>
