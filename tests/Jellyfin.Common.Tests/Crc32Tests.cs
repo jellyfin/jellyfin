dc9e2ad1a4a21677cb22d3a2c76dee40859462bb
Bond_009 <bond.009@outlook.com>
2021-02-13 11:46:38 +0100

    Add some more Crc32 tests

--- tests/Jellyfin.Common.Tests/Crc32Tests.cs
+++ tests/Jellyfin.Common.Tests/Crc32Tests.cs
@@ -19,5 +19,15 @@ namespace Jellyfin.Common.Tests
         {
             Assert.Equal(expected, Crc32.Compute(Encoding.UTF8.GetBytes(data)));
         }
+
+        [Theory]
+        [InlineData(0x414fa339, "54686520717569636B2062726F776E20666F78206A756D7073206F76657220746865206C617A7920646F67")]
+        [InlineData(0x190a55ad, "0000000000000000000000000000000000000000000000000000000000000000")]
+        [InlineData(0xff6cab0b, "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")]
+        [InlineData(0x91267e8a, "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F")]
+        public static void Compute_ValidHex_Success(uint expected, string data)
+        {
+            Assert.Equal(expected, Crc32.Compute(Convert.FromHexString(data)));
+        }
     }
 }
