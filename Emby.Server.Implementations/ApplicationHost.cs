4b6ff7ffa5f88992420976c2c28e898dfdcbe4dc
Claus Vium <cvium@users.noreply.github.com>
2021-02-27 20:01:51 +0100

    Merge pull request #5312 from BaronGreenback/FixFor5280
    (cherry picked from commit 9e77fdc70df59cd8f6a60425c5e79c5aeb9b13c8)
    Signed-off-by: Joshua M. Boniface <joshua@boniface.me>

--- Emby.Server.Implementations/ApplicationHost.cs
+++ Emby.Server.Implementations/ApplicationHost.cs
@@ -134,6 +134,9 @@ namespace Emby.Server.Implementations
 
         public bool CoreStartupHasCompleted { get; private set; }
 
+        /// <inheritdoc />
+        public Uri PublishedServerUrl => _startupOptions.PublishedServerUrl;
+
         public virtual bool CanLaunchWebBrowser
         {
             get
