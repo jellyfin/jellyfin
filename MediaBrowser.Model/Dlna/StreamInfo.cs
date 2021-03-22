74e14b4ca591b9043435ee1065827101379b1318
cvium <clausvium@gmail.com>
2021-03-22 22:34:47 +0100

    fix isdirectstream default

--- MediaBrowser.Model/Dlna/StreamInfo.cs
+++ MediaBrowser.Model/Dlna/StreamInfo.cs
@@ -630,10 +630,8 @@ namespace MediaBrowser.Model.Dlna
                     continue;
                 }
 
-                // Be careful, IsDirectStream==true by default (Static != false or not in query).
-                // See initialization of StreamingRequestDto in AudioController.GetAudioStream() method : Static = @static ?? false.
                 if (string.Equals(pair.Name, "Static", StringComparison.OrdinalIgnoreCase) &&
-                    string.Equals(pair.Value, "true", StringComparison.OrdinalIgnoreCase))
+                    string.Equals(pair.Value, "false", StringComparison.OrdinalIgnoreCase))
                 {
                     continue;
                 }
