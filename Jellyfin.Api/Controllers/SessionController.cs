f114ba57dd24ed38786a478df7a641f81dd9d5c7
Brian Arnold <connect@brian-arnold.dev>
2021-03-26 10:37:59 -0400

    Fixed comment and code block runes to match coding standards required by Jellyfin team.

--- Jellyfin.Api/Controllers/SessionController.cs
+++ Jellyfin.Api/Controllers/SessionController.cs
@@ -327,11 +327,13 @@ namespace Jellyfin.Api.Controllers
             {
                 throw new ArgumentException("Request body may not be null");
             }
-            //Need to check if message.Text is null, since [Required] can't be applied to properties of a deserialized object.
+
+            // Need to check if message.Text is null, since [Required] can't be applied to properties of a deserialized object.
             if (string.IsNullOrWhiteSpace(command.Text))
             {
                 throw new ArgumentNullException("Message Text may not be empty.");
             }
+
             var nullCorrectedCommand = new MessageCommand
             {
                 Header = string.IsNullOrWhiteSpace(command.Header) ? "Message from Server" : command.Header,
