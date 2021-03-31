f3ef927c20ee5f9cfa53b6cb71b99dad5338076d
Maxr1998 <max.rumpf1998@gmail.com>
2021-03-31 19:06:42 +0200

    Fix typo and reword comment

--- MediaBrowser.Model/Dlna/StreamBuilder.cs
+++ MediaBrowser.Model/Dlna/StreamBuilder.cs
@@ -674,7 +674,7 @@ namespace MediaBrowser.Model.Dlna
 
             var videoStream = item.VideoStream;
 
-            // TODO: This doesn't accout for situation of device being able to handle media bitrate, but wifi connection not fast enough
+            // TODO: This doesn't account for situations where the device is able to handle the media's bitrate, but the connection isn't fast enough
             var directPlayEligibilityResult = IsEligibleForDirectPlay(item, GetBitrateForDirectPlayCheck(item, options, true) ?? 0, subtitleStream, options, PlayMethod.DirectPlay);
             var directStreamEligibilityResult = IsEligibleForDirectPlay(item, options.GetMaxBitrate(false) ?? 0, subtitleStream, options, PlayMethod.DirectStream);
             bool isEligibleForDirectPlay = options.EnableDirectPlay && (options.ForceDirectPlay || directPlayEligibilityResult.Item1);
