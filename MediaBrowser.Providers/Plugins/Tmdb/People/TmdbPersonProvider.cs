06b8cf42d1d1edc424eaccb487590bc6a695ccba
cvium <clausvium@gmail.com>
2021-04-20 08:59:15 +0200

    Fix TMDb Person Provider

--- MediaBrowser.Providers/Plugins/Tmdb/People/TmdbPersonProvider.cs
+++ MediaBrowser.Providers/Plugins/Tmdb/People/TmdbPersonProvider.cs
@@ -30,11 +30,9 @@ namespace MediaBrowser.Providers.Plugins.Tmdb.People
 
         public async Task<IEnumerable<RemoteSearchResult>> GetSearchResults(PersonLookupInfo searchInfo, CancellationToken cancellationToken)
         {
-            var personTmdbId = Convert.ToInt32(searchInfo.GetProviderId(MetadataProvider.Tmdb), CultureInfo.InvariantCulture);
-
-            if (personTmdbId <= 0)
+            if (searchInfo.TryGetProviderId(MetadataProvider.Tmdb, out var personTmdbId))
             {
-                var personResult = await _tmdbClientManager.GetPersonAsync(personTmdbId, cancellationToken).ConfigureAwait(false);
+                var personResult = await _tmdbClientManager.GetPersonAsync(Convert.ToInt32(personTmdbId), cancellationToken).ConfigureAwait(false);
 
                 if (personResult != null)
                 {
@@ -51,17 +49,13 @@ namespace MediaBrowser.Providers.Plugins.Tmdb.People
                     }
 
                     result.SetProviderId(MetadataProvider.Tmdb, personResult.Id.ToString(CultureInfo.InvariantCulture));
-                    result.SetProviderId(MetadataProvider.Imdb, personResult.ExternalIds.ImdbId);
+                    if (!string.IsNullOrEmpty(personResult.ExternalIds.ImdbId))
+                    {
+                        result.SetProviderId(MetadataProvider.Imdb, personResult.ExternalIds.ImdbId);
+                    }
 
                     return new[] { result };
                 }
-            }
-
-            // TODO why? Because of the old rate limit?
-            if (searchInfo.IsAutomated)
-            {
-                // Don't hammer moviedb searching by name
-                return Enumerable.Empty<RemoteSearchResult>();
             }
 
             var personSearchResult = await _tmdbClientManager.SearchPersonAsync(searchInfo.Name, cancellationToken).ConfigureAwait(false);
