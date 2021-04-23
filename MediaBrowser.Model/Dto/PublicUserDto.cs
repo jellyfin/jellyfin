523465d909a353dfdbdded2b22d65e0429e0ba60
Brian C. Arnold <connect@brian-arnold.dev>
2021-04-23 15:20:49 -0400

    Created PublicUserDto that doesn't include information about users such as whether they are admins. This will decrease the likelihood of an attacker targetting an admin account specifically.

---
+++ MediaBrowser.Model/Dto/PublicUserDto.cs
@@ -0,0 +1,83 @@
+#nullable disable
+using System;
+using MediaBrowser.Model.Configuration;
+using MediaBrowser.Model.Users;
+
+namespace MediaBrowser.Model.Dto
+{
+    /// <summary>
+    /// Class PublicUserDto.
+    /// </summary>
+    public class PublicUserDto : IItemDto, IHasServerId
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PublicUserDto"/> class.
+        /// </summary>
+        public PublicUserDto()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PublicUserDto"/> class from a <see cref="UserDto"/> object.
+        /// </summary>
+        public PublicUserDto(UserDto sourceUser)
+        {
+            this.Name = sourceUser.Name;
+            this.ServerId = sourceUser.ServerId;
+            this.ServerName = sourceUser.ServerName;
+            this.Id = sourceUser.Id;
+            this.PrimaryImageTag = sourceUser.PrimaryImageTag;
+            this.HasPassword = sourceUser.HasPassword;
+            this.PrimaryImageAspectRatio = sourceUser.PrimaryImageAspectRatio;
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the server identifier.
+        /// </summary>
+        /// <value>The server identifier.</value>
+        public string ServerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the server.
+        /// This is not used by the server and is for client-side usage only.
+        /// </summary>
+        /// <value>The name of the server.</value>
+        public string ServerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>The id.</value>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the primary image tag.
+        /// </summary>
+        /// <value>The primary image tag.</value>
+        public string PrimaryImageTag { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance has password.
+        /// </summary>
+        /// <value><c>true</c> if this instance has password; otherwise, <c>false</c>.</value>
+        public bool HasPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the primary image aspect ratio.
+        /// </summary>
+        /// <value>The primary image aspect ratio.</value>
+        public double? PrimaryImageAspectRatio { get; set; }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return Name ?? base.ToString();
+        }
+    }
+}
