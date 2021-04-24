c2072183c6f0ab2681ca620c215ce3f23cbcec0b
MrTimscampi <julien.machiels@protonmail.com>
2021-04-24 15:24:09 +0200

    Enable Person support for Photo items

--- MediaBrowser.Controller/Entities/Photo.cs
+++ MediaBrowser.Controller/Entities/Photo.cs
@@ -17,6 +17,9 @@ namespace MediaBrowser.Controller.Entities
         public override Folder LatestItemsIndexContainer => AlbumEntity;
 
         [JsonIgnore]
+        public override bool SupportsPeople => true;
+
+        [JsonIgnore]
         public PhotoAlbum AlbumEntity
         {
             get
