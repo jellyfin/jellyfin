043e69d252d5862ec5466ad037fea05f5aaef40f
BaronGreenback <jimcartlidge@yahoo.co.uk>
2021-05-07 18:49:57 +0100

    Update tests/Jellyfin.Api.Tests/Controllers/EncodedQueryStringTest.cs
    Co-authored-by: Cody Robibero <cody@robibe.ro>

--- tests/Jellyfin.Api.Tests/Controllers/EncodedQueryStringTest.cs
+++ tests/Jellyfin.Api.Tests/Controllers/EncodedQueryStringTest.cs
@@ -34,7 +34,6 @@ namespace Jellyfin.Api.Tests.Controllers
         [InlineData("a=1", "a=1")] // won't be processed as it has a value
         [InlineData("%3D", "==")] // will decode with an empty string value '=' = ''.
         [InlineData("a%3D1%26b%3D2%26c%3D3", "a=1&b=2&c=3")] // will be processed.
-
         public async Task Ensure_Decoding_Of_Urls_Is_Working(string sourceUrl, string unencodedUrl)
         {
             var client = _factory.CreateClient();
