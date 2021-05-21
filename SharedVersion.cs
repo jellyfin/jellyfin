49873c3d7f79d62be06247878f4b892d282bf1be
Joshua M. Boniface <joshua@boniface.me>
2021-05-20 22:06:25 -0400

    Bump version to 10.7.6

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.7.5")]
-[assembly: AssemblyFileVersion("10.7.5")]
+[assembly: AssemblyVersion("10.7.6")]
+[assembly: AssemblyFileVersion("10.7.6")]
