06401ffa0d8ae98fa42c750847f97c3c291b06ae
Bond_009 <bond.009@outlook.com>
2021-06-08 22:26:59 +0200

    Fix duplicate keys causing an exception

--- tests/Jellyfin.Server.Integration.Tests/Controllers/EncoderController.cs
+++ tests/Jellyfin.Server.Integration.Tests/Controllers/EncoderController.cs
@@ -29,5 +29,25 @@ namespace Jellyfin.Api.Controllers
                 StatusCode = 200
             };
         }
+
+        /// <summary>
+        /// Tests the url decoding.
+        /// </summary>
+        /// <param name="params">Parameters to echo back in the response.</param>
+        /// <returns>An <see cref="OkResult"/>.</returns>
+        /// <response code="200">Information retrieved.</response>
+        [HttpGet("UrlArrayDecode")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ContentResult TestUrlArrayDecoding([FromQuery] Dictionary<string, string[]>? @params = null)
+        {
+            return new ContentResult()
+            {
+                Content = (@params != null && @params.Count > 0)
+                    ? string.Join("&", @params.Select(x => x.Key + "=" + string.Join(',', x.Value)))
+                    : string.Empty,
+                ContentType = "text/plain; charset=utf-8",
+                StatusCode = 200
+            };
+        }
     }
 }
--- tests/Jellyfin.Server.Integration.Tests/EncodedQueryStringTest.cs
+++ tests/Jellyfin.Server.Integration.Tests/EncodedQueryStringTest.cs
@@ -20,11 +20,26 @@ namespace Jellyfin.Server.Integration.Tests
         [InlineData("a=1&b=2&c=3", "a=1&b=2&c=3")] // won't be processed as there is more than 1.
         [InlineData("a=1", "a=1")] // won't be processed as it has a value
         [InlineData("a%3D1%26b%3D2%26c%3D3", "a=1&b=2&c=3")] // will be processed.
+        [InlineData("a=b&a=c", "a=b")]
+        [InlineData("a%3Db%26a%3Dc", "a=b")]
         public async Task Ensure_Decoding_Of_Urls_Is_Working(string sourceUrl, string unencodedUrl)
         {
             var client = _factory.CreateClient();
 
             var response = await client.GetAsync("Encoder/UrlDecode?" + sourceUrl).ConfigureAwait(false);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            string reply = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            Assert.Equal(unencodedUrl, reply);
+        }
+
+        [Theory]
+        [InlineData("a=b&a=c", "a=b,c")]
+        [InlineData("a%3Db%26a%3Dc", "a=b,c")]
+        public async Task Ensure_Array_Decoding_Of_Urls_Is_Working(string sourceUrl, string unencodedUrl)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("Encoder/UrlArrayDecode?" + sourceUrl).ConfigureAwait(false);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             string reply = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             Assert.Equal(unencodedUrl, reply);
