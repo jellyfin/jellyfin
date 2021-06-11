fdb28499ff52aa0fb1d31963a37036be211a4daf
BaronGreenback <jimcartlidge@yahoo.co.uk>
2021-06-11 13:57:38 +0100

    removed nullable reference.

--- MediaBrowser.Model/Dlna/StreamInfo.cs
+++ MediaBrowser.Model/Dlna/StreamInfo.cs
@@ -601,11 +601,11 @@ namespace MediaBrowser.Model.Dlna
         /// <summary>
         /// Returns this class as a url.
         /// </summary>
-        /// <param name="baseUrl">The baseUrl.</param>
-        /// <param name="accessToken">The accessToken.</param>
+        /// <param name="baseUrl">Nullable. The baseUrl.</param>
+        /// <param name="accessToken">Nullable. The accessToken.</param>
         /// <param name="query">Optional extra query.</param>
         /// <returns>A querystring representation of this object.</returns>
-        public string ToUrl(string? baseUrl, string? accessToken, string? query = null)
+        public string ToUrl(string baseUrl, string accessToken, string query = null)
         {
             if (PlayMethod == PlayMethod.DirectPlay)
             {
