f4af78817d5832c11ff96f365714917fad0db4f9
Fernando Fernández <ferferga@hotmail.com>
2021-09-06 21:05:21 +0200

    Move model configuration to its own classes

---
+++ Jellyfin.Server.Implementations/ModelConfiguration/ApiKeyConfiguration.cs
@@ -0,0 +1,22 @@
+using Jellyfin.Data.Entities.Security;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Jellyfin.Server.Implementations.ModelConfiguration
+{
+    /// <summary>
+    /// FluentAPI configuration for the ApiKey entity.
+    /// </summary>
+    public class ApiKeyConfiguration : IEntityTypeConfiguration<ApiKey>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<ApiKey> builder)
+        {
+            // Indexes
+
+            builder
+                .HasIndex(entity => entity.AccessToken)
+                .IsUnique();
+        }
+    }
+}
---
+++ Jellyfin.Server.Implementations/ModelConfiguration/DisplayPreferencesConfiguration.cs
@@ -0,0 +1,29 @@
+using Jellyfin.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Jellyfin.Server.Implementations.ModelConfiguration
+{
+    /// <summary>
+    /// FluentAPI configuration for the DisplayPreferencesConfiguration entity.
+    /// </summary>
+    public class DisplayPreferencesConfiguration : IEntityTypeConfiguration<DisplayPreferences>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<DisplayPreferences> builder)
+        {
+            // Delete behaviour
+
+            builder
+                .HasMany(d => d.HomeSections)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Indexes
+
+            builder
+                .HasIndex(entity => new { entity.UserId, entity.ItemId, entity.Client })
+                .IsUnique();
+        }
+    }
+}
