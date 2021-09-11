3313efce195a66b0363ea7224d70056fdda14ab6
ianjazz246 <ianjazz246@gmail.com>
2021-09-11 13:29:58 -0700

    Revert "Use TheoryData in UrlDecodeQueryFeatureTest"
    This reverts commit c5393b25798098aa079c185971b7ac28e5f5b994.

--- tests/Jellyfin.Server.Tests/UrlDecodeQueryFeatureTests.cs
+++ tests/Jellyfin.Server.Tests/UrlDecodeQueryFeatureTests.cs
@@ -10,18 +10,11 @@ namespace Jellyfin.Server.Tests
 {
     public static class UrlDecodeQueryFeatureTests
     {
-        public static TheoryData<string, string> EmptyValueTest_TestData()
-        {
-            var data = new TheoryData<string, string>();
-            data.Add("e0a72cb2a2c7", "e0a72cb2a2c7"); // isn't encoded
-            data.Add("random+test", "random test"); // encoded
-            data.Add("random%20test", "random test"); // encoded
-            data.Add("++", "  "); // encoded
-            return data;
-        }
-
         [Theory]
-        [MemberData(nameof(EmptyValueTest_TestData))]
+        [InlineData("e0a72cb2a2c7", "e0a72cb2a2c7")] // isn't encoded
+        [InlineData("random+test", "random test")] // encoded
+        [InlineData("random%20test", "random test")] // encoded
+        [InlineData("++", "  ")] // encoded
         public static void EmptyValueTest(string query, string key)
         {
             var dict = new Dictionary<string, StringValues>
