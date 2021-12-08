03b3f08354f496d18444779977bfc1602bbb2c73
Jonas Resch <32968142+jonas-resch@users.noreply.github.com>
2021-12-08 18:55:28 +0100

    Format code in MediaBrowser.Providers/MediaInfo/AudioResolver.cs
    Co-authored-by: Claus Vium <cvium@users.noreply.github.com>

--- MediaBrowser.Providers/MediaInfo/AudioResolver.cs
+++ MediaBrowser.Providers/MediaInfo/AudioResolver.cs
@@ -131,7 +131,9 @@ namespace MediaBrowser.Providers.MediaInfo
             for (int i = 0; i < files.Count; i++)
             {
                 string file = files[i];
-                if (string.Equals(video.Path, file, StringComparison.OrdinalIgnoreCase) || !AudioFileParser.IsAudioFile(file, _namingOptions) || Path.GetExtension(file).Equals(".strm", StringComparison.OrdinalIgnoreCase))
+                if (string.Equals(video.Path, file, StringComparison.OrdinalIgnoreCase)
+                    || !AudioFileParser.IsAudioFile(file, _namingOptions)
+                    || Path.GetExtension(file.AsSpan()).Equals(".strm", StringComparison.OrdinalIgnoreCase))
                 {
                     continue;
                 }
