58b9e5af79be32fdc5c7ca1c5542206fdea85257
Claus Vium <cvium@users.noreply.github.com>
2022-01-01 14:52:54 +0100

    Update Emby.Server.Implementations/Library/Resolvers/GenericVideoResolver.cs
    Co-authored-by: Joe Rogers <1337joe@users.noreply.github.com>

--- Emby.Server.Implementations/Library/Resolvers/GenericVideoResolver.cs
+++ Emby.Server.Implementations/Library/Resolvers/GenericVideoResolver.cs
@@ -1,15 +1,21 @@
 ﻿#nullable disable
 
-#pragma warning disable CS1591
-
 using Emby.Naming.Common;
 using MediaBrowser.Controller.Entities;
 
 namespace Emby.Server.Implementations.Library.Resolvers
 {
+    /// <summary>
+    /// Resolves a Path into a Video or Video subclass.
+    /// </summary>
+    /// <typeparam name="T">The type of item to resolve.</typeparam>
     public class GenericVideoResolver<T> : BaseVideoResolver<T>
         where T : Video, new()
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericVideoResolver"/> class.
+        /// </summary>
+        /// <param name="namingOptions">The naming options.</param>
         public GenericVideoResolver(NamingOptions namingOptions)
             : base(namingOptions)
         {
