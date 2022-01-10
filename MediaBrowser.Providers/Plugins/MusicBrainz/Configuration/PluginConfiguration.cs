1dcd04babcb3f18318a241277fba79a6cbd870f7
Cody Robibero <cody@robibe.ro>
2022-01-09 18:53:31 -0700

    Clean up

--- MediaBrowser.Providers/Plugins/MusicBrainz/Configuration/PluginConfiguration.cs
+++ MediaBrowser.Providers/Plugins/MusicBrainz/Configuration/PluginConfiguration.cs
@@ -1,48 +1,58 @@
-﻿#pragma warning disable CS1591
-
-using MediaBrowser.Model.Plugins;
+﻿using MediaBrowser.Model.Plugins;
 using MetaBrainz.MusicBrainz;
 
-namespace MediaBrowser.Providers.Plugins.MusicBrainz
+namespace MediaBrowser.Providers.Plugins.MusicBrainz.Configuration;
+
+/// <summary>
+/// MusicBrainz plugin configuration.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
 {
-    public class PluginConfiguration : BasePluginConfiguration
+    private const string DefaultServer = "musicbrainz.org";
+
+    private const double DefaultRateLimit = 1.0;
+
+    private string _server = DefaultServer;
+
+    private double _rateLimit = DefaultRateLimit;
+
+    /// <summary>
+    /// Gets or sets the server url.
+    /// </summary>
+    public string Server
     {
-        private string _server = Plugin.DefaultServer;
+        get => _server;
 
-        private double _rateLimit = Plugin.DefaultRateLimit;
-
-        public string Server
+        set
         {
-            get
-            {
-                return _server;
-            }
-
-            set
-            {
-                _server = value.TrimEnd('/');
-                Query.DefaultWebSite = _server;
-            }
+            _server = value.TrimEnd('/');
+            Query.DefaultServer = _server;
         }
-
-        public double RateLimit
-        {
-            get => _rateLimit;
-            set
-            {
-                if (value < Plugin.DefaultRateLimit && _server == Plugin.DefaultServer)
-                {
-                    _rateLimit = Plugin.DefaultRateLimit;
-                }
-                else
-                {
-                    _rateLimit = value;
-                }
-
-                Query.DelayBetweenRequests = _rateLimit;
-            }
-        }
-
-        public bool ReplaceArtistName { get; set; }
     }
+
+    /// <summary>
+    /// Gets or sets the rate limit.
+    /// </summary>
+    public double RateLimit
+    {
+        get => _rateLimit;
+        set
+        {
+            if (value < DefaultRateLimit && _server == DefaultServer)
+            {
+                _rateLimit = DefaultRateLimit;
+            }
+            else
+            {
+                _rateLimit = value;
+            }
+
+            Query.DelayBetweenRequests = _rateLimit;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to replace the artist name.
+    /// </summary>
+    public bool ReplaceArtistName { get; set; }
 }
