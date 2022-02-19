36cdeaa53c0bee8ede6d5ecf66ca629ab55ab3d7
cameron <cameronlambert98@gmail.com>
2022-02-18 23:18:28 -0800

    dont use noaccurate_seek for hls

--- MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
+++ MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
@@ -2167,6 +2167,7 @@ namespace MediaBrowser.Controller.MediaEncoding
                     // Important: If this is ever re-enabled, make sure not to use it with wtv because it breaks seeking
                     if (!string.Equals(state.InputContainer, "wtv", StringComparison.OrdinalIgnoreCase)
                         && state.TranscodingType != TranscodingJobType.Progressive
+                        && state.TranscodingType != TranscodingJobType.Hls
                         && !state.EnableBreakOnNonKeyFrames(outputVideoCodec)
                         && (state.BaseRequest.StartTimeTicks ?? 0) > 0)
                     {
