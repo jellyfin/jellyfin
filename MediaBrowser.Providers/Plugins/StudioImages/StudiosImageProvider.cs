c5ca29d2e2bb2db43a3dbf5ae42c1d2a64c857d6
Bill Thornton <thornbill@users.noreply.github.com>
2022-03-08 10:36:05 -0500

    Remove unused poster support from StudiosImageProvider (#7423)

--- MediaBrowser.Providers/Plugins/StudioImages/StudiosImageProvider.cs
+++ MediaBrowser.Providers/Plugins/StudioImages/StudiosImageProvider.cs
@@ -50,41 +50,29 @@ namespace MediaBrowser.Providers.Studios
         {
             return new List<ImageType>
             {
-                ImageType.Primary,
                 ImageType.Thumb
             };
         }
 
-        public Task<IEnumerable<RemoteImageInfo>> GetImages(BaseItem item, CancellationToken cancellationToken)
+        public async Task<IEnumerable<RemoteImageInfo>> GetImages(BaseItem item, CancellationToken cancellationToken)
         {
-            return GetImages(item, true, true, cancellationToken);
-        }
+            var thumbsPath = Path.Combine(_config.ApplicationPaths.CachePath, "imagesbyname", "remotestudiothumbs.txt");
 
-        private async Task<IEnumerable<RemoteImageInfo>> GetImages(BaseItem item, bool posters, bool thumbs, CancellationToken cancellationToken)
-        {
-            var list = new List<RemoteImageInfo>();
-
-            if (posters)
-            {
-                var posterPath = Path.Combine(_config.ApplicationPaths.CachePath, "imagesbyname", "remotestudioposters.txt");
-
-                posterPath = await EnsurePosterList(posterPath, cancellationToken).ConfigureAwait(false);
-
-                list.Add(GetImage(item, posterPath, ImageType.Primary, "folder"));
-            }
+            thumbsPath = await EnsureThumbsList(thumbsPath, cancellationToken).ConfigureAwait(false);
 
             cancellationToken.ThrowIfCancellationRequested();
 
-            if (thumbs)
+            var imageInfo = GetImage(item, thumbsPath, ImageType.Thumb, "thumb");
+
+            if (imageInfo == null)
             {
-                var thumbsPath = Path.Combine(_config.ApplicationPaths.CachePath, "imagesbyname", "remotestudiothumbs.txt");
-
-                thumbsPath = await EnsureThumbsList(thumbsPath, cancellationToken).ConfigureAwait(false);
-
-                list.Add(GetImage(item, thumbsPath, ImageType.Thumb, "thumb"));
+                return Enumerable.Empty<RemoteImageInfo>();
             }
 
-            return list.Where(i => i != null);
+            return new RemoteImageInfo[]
+            {
+                imageInfo
+            };
         }
 
         private RemoteImageInfo GetImage(BaseItem item, string filename, ImageType type, string remoteFilename)
@@ -116,13 +104,6 @@ namespace MediaBrowser.Providers.Studios
         private Task<string> EnsureThumbsList(string file, CancellationToken cancellationToken)
         {
             string url = string.Format(CultureInfo.InvariantCulture, "{0}/thumbs.txt", repositoryUrl);
-
-            return EnsureList(url, file, _fileSystem, cancellationToken);
-        }
-
-        private Task<string> EnsurePosterList(string file, CancellationToken cancellationToken)
-        {
-            string url = string.Format(CultureInfo.InvariantCulture, "{0}/posters.txt", repositoryUrl);
 
             return EnsureList(url, file, _fileSystem, cancellationToken);
         }
