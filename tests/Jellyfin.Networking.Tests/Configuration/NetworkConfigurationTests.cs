edeb1983139c2668450d819273f229026a48f0b5
Bond_009 <bond.009@outlook.com>
2022-03-11 15:44:16 +0100

    Add tests for BaseUrl normalization

---
+++ tests/Jellyfin.Networking.Tests/Configuration/NetworkConfigurationTests.cs
@@ -0,0 +1,28 @@
+using Jellyfin.Networking.Configuration;
+using Xunit;
+
+namespace Jellyfin.Networking.Tests.Configuration;
+
+public static class NetworkConfigurationTests
+{
+    [Theory]
+    [InlineData("", null)]
+    [InlineData("", "")]
+    [InlineData("/Test", "/Test")]
+    [InlineData("/Test", "Test")]
+    [InlineData("/Test", "Test/")]
+    [InlineData("/Test", "/Test/")]
+    [InlineData("/Test/2", "/Test/2")]
+    [InlineData("/Test/2", "Test/2")]
+    [InlineData("/Test/2", "Test/2/")]
+    [InlineData("/Test/2", "/Test/2/")]
+    public static void BaseUrl_ReturnsNormalized(string expected, string input)
+    {
+        var config = new NetworkConfiguration()
+        {
+            BaseUrl = input
+        };
+
+        Assert.Equal(expected, config.BaseUrl);
+    }
+}
