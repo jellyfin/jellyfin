c5cf74eab5824b8d54836377249834f27ff29597
Jellifi007 <jellyfi007@outlook.com>
2022-04-14 17:16:16 +0200

    Fixes search regression for string with diacritics #7484 #7450 #7560

--- MediaBrowser.Providers/Manager/MetadataService.cs
+++ MediaBrowser.Providers/Manager/MetadataService.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Diacritics.Extensions;
+using Jellyfin.Extensions;
 using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Audio;
