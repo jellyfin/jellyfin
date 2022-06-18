d2caed25fbbfbd047f6b52c50a501a6dcba415df
nyanmisaka <nst799610810@gmail.com>
2022-06-18 19:41:17 +0800

    Register DOVI side_data in db

--- Emby.Server.Implementations/Data/SqliteItemRepository.cs
+++ Emby.Server.Implementations/Data/SqliteItemRepository.cs
@@ -170,7 +170,15 @@ namespace Emby.Server.Implementations.Data
             "CodecTimeBase",
             "ColorPrimaries",
             "ColorSpace",
-            "ColorTransfer"
+            "ColorTransfer",
+            "DvVersionMajor",
+            "DvVersionMinor",
+            "DvProfile",
+            "DvLevel",
+            "RpuPresentFlag",
+            "ElPresentFlag",
+            "BlPresentFlag",
+            "DvBlSignalCompatibilityId"
         };
 
         private static readonly string _mediaStreamSaveColumnsInsertQuery =
@@ -341,7 +349,7 @@ namespace Emby.Server.Implementations.Data
         public void Initialize(SqliteUserDataRepository userDataRepo, IUserManager userManager)
         {
             const string CreateMediaStreamsTableCommand
-                    = "create table if not exists mediastreams (ItemId GUID, StreamIndex INT, StreamType TEXT, Codec TEXT, Language TEXT, ChannelLayout TEXT, Profile TEXT, AspectRatio TEXT, Path TEXT, IsInterlaced BIT, BitRate INT NULL, Channels INT NULL, SampleRate INT NULL, IsDefault BIT, IsForced BIT, IsExternal BIT, Height INT NULL, Width INT NULL, AverageFrameRate FLOAT NULL, RealFrameRate FLOAT NULL, Level FLOAT NULL, PixelFormat TEXT, BitDepth INT NULL, IsAnamorphic BIT NULL, RefFrames INT NULL, CodecTag TEXT NULL, Comment TEXT NULL, NalLengthSize TEXT NULL, IsAvc BIT NULL, Title TEXT NULL, TimeBase TEXT NULL, CodecTimeBase TEXT NULL, ColorPrimaries TEXT NULL, ColorSpace TEXT NULL, ColorTransfer TEXT NULL, PRIMARY KEY (ItemId, StreamIndex))";
+                    = "create table if not exists mediastreams (ItemId GUID, StreamIndex INT, StreamType TEXT, Codec TEXT, Language TEXT, ChannelLayout TEXT, Profile TEXT, AspectRatio TEXT, Path TEXT, IsInterlaced BIT, BitRate INT NULL, Channels INT NULL, SampleRate INT NULL, IsDefault BIT, IsForced BIT, IsExternal BIT, Height INT NULL, Width INT NULL, AverageFrameRate FLOAT NULL, RealFrameRate FLOAT NULL, Level FLOAT NULL, PixelFormat TEXT, BitDepth INT NULL, IsAnamorphic BIT NULL, RefFrames INT NULL, CodecTag TEXT NULL, Comment TEXT NULL, NalLengthSize TEXT NULL, IsAvc BIT NULL, Title TEXT NULL, TimeBase TEXT NULL, CodecTimeBase TEXT NULL, ColorPrimaries TEXT NULL, ColorSpace TEXT NULL, ColorTransfer TEXT NULL, DvVersionMajor INT NULL, DvVersionMinor INT NULL, DvProfile INT NULL, DvLevel INT NULL, RpuPresentFlag INT NULL, ElPresentFlag INT NULL, BlPresentFlag INT NULL, DvBlSignalCompatibilityId INT NULL, PRIMARY KEY (ItemId, StreamIndex))";
             const string CreateMediaAttachmentsTableCommand
                     = "create table if not exists mediaattachments (ItemId GUID, AttachmentIndex INT, Codec TEXT, CodecTag TEXT NULL, Comment TEXT NULL, Filename TEXT NULL, MIMEType TEXT NULL, PRIMARY KEY (ItemId, AttachmentIndex))";
 
@@ -555,6 +563,15 @@ namespace Emby.Server.Implementations.Data
                         AddColumn(db, "MediaStreams", "ColorPrimaries", "TEXT", existingColumnNames);
                         AddColumn(db, "MediaStreams", "ColorSpace", "TEXT", existingColumnNames);
                         AddColumn(db, "MediaStreams", "ColorTransfer", "TEXT", existingColumnNames);
+
+                        AddColumn(db, "MediaStreams", "DvVersionMajor", "INT", existingColumnNames);
+                        AddColumn(db, "MediaStreams", "DvVersionMinor", "INT", existingColumnNames);
+                        AddColumn(db, "MediaStreams", "DvProfile", "INT", existingColumnNames);
+                        AddColumn(db, "MediaStreams", "DvLevel", "INT", existingColumnNames);
+                        AddColumn(db, "MediaStreams", "RpuPresentFlag", "INT", existingColumnNames);
+                        AddColumn(db, "MediaStreams", "ElPresentFlag", "INT", existingColumnNames);
+                        AddColumn(db, "MediaStreams", "BlPresentFlag", "INT", existingColumnNames);
+                        AddColumn(db, "MediaStreams", "DvBlSignalCompatibilityId", "INT", existingColumnNames);
                     },
                     TransactionMode);
 
@@ -5859,6 +5876,15 @@ AND Type = @InternalPersonType)");
                         statement.TryBind("@ColorPrimaries" + index, stream.ColorPrimaries);
                         statement.TryBind("@ColorSpace" + index, stream.ColorSpace);
                         statement.TryBind("@ColorTransfer" + index, stream.ColorTransfer);
+
+                        statement.TryBind("@DvVersionMajor" + index, stream.DvVersionMajor);
+                        statement.TryBind("@DvVersionMinor" + index, stream.DvVersionMinor);
+                        statement.TryBind("@DvProfile" + index, stream.DvProfile);
+                        statement.TryBind("@DvLevel" + index, stream.DvLevel);
+                        statement.TryBind("@RpuPresentFlag" + index, stream.RpuPresentFlag);
+                        statement.TryBind("@ElPresentFlag" + index, stream.ElPresentFlag);
+                        statement.TryBind("@BlPresentFlag" + index, stream.BlPresentFlag);
+                        statement.TryBind("@DvBlSignalCompatibilityId" + index, stream.DvBlSignalCompatibilityId);
                     }
 
                     statement.Reset();
@@ -6028,6 +6054,46 @@ AND Type = @InternalPersonType)");
             if (reader.TryGetString(34, out var colorTransfer))
             {
                 item.ColorTransfer = colorTransfer;
+            }
+
+            if (reader.TryGetInt32(35, out var dvVersionMajor))
+            {
+                item.DvVersionMajor = dvVersionMajor;
+            }
+
+            if (reader.TryGetInt32(36, out var dvVersionMinor))
+            {
+                item.DvVersionMinor = dvVersionMinor;
+            }
+
+            if (reader.TryGetInt32(37, out var dvProfile))
+            {
+                item.DvProfile = dvProfile;
+            }
+
+            if (reader.TryGetInt32(38, out var dvLevel))
+            {
+                item.DvLevel = dvLevel;
+            }
+
+            if (reader.TryGetInt32(39, out var rpuPresentFlag))
+            {
+                item.RpuPresentFlag = rpuPresentFlag;
+            }
+
+            if (reader.TryGetInt32(40, out var elPresentFlag))
+            {
+                item.ElPresentFlag = elPresentFlag;
+            }
+
+            if (reader.TryGetInt32(41, out var blPresentFlag))
+            {
+                item.BlPresentFlag = blPresentFlag;
+            }
+
+            if (reader.TryGetInt32(42, out var dvBlSignalCompatibilityId))
+            {
+                item.DvBlSignalCompatibilityId = dvBlSignalCompatibilityId;
             }
 
             if (item.Type == MediaStreamType.Subtitle)
