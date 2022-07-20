1c6b6f5d36cb09c0352bebc1ab26b88e4f511921
Shadowghost <Ghost_of_Stone@web.de>
2022-07-20 07:42:52 +0200

    Remove socket wrkaround

--- Jellyfin.Server/Program.cs
+++ Jellyfin.Server/Program.cs
@@ -335,13 +335,6 @@ namespace Jellyfin.Server
                     if (startupConfig.UseUnixSocket() && Environment.OSVersion.Platform == PlatformID.Unix)
                     {
                         var socketPath = GetUnixSocketPath(startupConfig, appPaths);
-
-                        // Workaround for https://github.com/aspnet/AspNetCore/issues/14134
-                        if (File.Exists(socketPath))
-                        {
-                            File.Delete(socketPath);
-                        }
-
                         options.ListenUnixSocket(socketPath);
                         _logger.LogInformation("Kestrel listening to unix socket {SocketPath}", socketPath);
                     }
