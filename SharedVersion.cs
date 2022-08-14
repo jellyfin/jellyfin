b344771f8ad3a34f7d54962a1efef559f4c1f642
Joshua Boniface <joshua@boniface.me>
2022-08-13 21:51:50 -0400

    Bump version to 10.8.4

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.8.3")]
-[assembly: AssemblyFileVersion("10.8.3")]
+[assembly: AssemblyVersion("10.8.4")]
+[assembly: AssemblyFileVersion("10.8.4")]
