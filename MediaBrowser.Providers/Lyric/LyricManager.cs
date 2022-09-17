9350fa40bda4464a1a5f01fee7f2f72415b7f5d4
1hitsong <3330318+1hitsong@users.noreply.github.com>
2022-09-17 08:46:09 -0400

    Convert _lyricProviders to an array.

--- MediaBrowser.Providers/Lyric/LyricManager.cs
+++ MediaBrowser.Providers/Lyric/LyricManager.cs
@@ -2,56 +2,20 @@
 
 #pragma warning disable CS1591
 
-using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Jellyfin.Extensions;
-using MediaBrowser.Common.Extensions;
 using MediaBrowser.Controller.Entities;
-using MediaBrowser.Controller.Entities.Movies;
-using MediaBrowser.Controller.Entities.TV;
-using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Lyrics;
-using MediaBrowser.Controller.Persistence;
-using MediaBrowser.Controller.Providers;
-using MediaBrowser.Controller.Subtitles;
-using MediaBrowser.Model.Configuration;
-using MediaBrowser.Model.Entities;
-using MediaBrowser.Model.Globalization;
-using MediaBrowser.Model.IO;
-using MediaBrowser.Model.Providers;
-using Microsoft.Extensions.Logging;
 
 namespace MediaBrowser.Providers.Lyric
 {
     public class LyricManager : ILyricManager
     {
-        private readonly ILogger<LyricManager> _logger;
-        private readonly IFileSystem _fileSystem;
-        private readonly ILibraryMonitor _monitor;
-        private readonly IMediaSourceManager _mediaSourceManager;
-        private readonly ILocalizationManager _localization;
+        private readonly ILyricProvider[] _lyricProviders;
 
-        private IEnumerable<ILyricProvider> _lyricProviders;
-
-        public LyricManager(
-            ILogger<LyricManager> logger,
-            IFileSystem fileSystem,
-            ILibraryMonitor monitor,
-            IMediaSourceManager mediaSourceManager,
-            ILocalizationManager localizationManager,
-            IEnumerable<ILyricProvider> lyricProviders)
+        public LyricManager(IEnumerable<ILyricProvider> lyricProviders)
         {
-            _logger = logger;
-            _fileSystem = fileSystem;
-            _monitor = monitor;
-            _mediaSourceManager = mediaSourceManager;
-            _localization = localizationManager;
-            _lyricProviders = lyricProviders;
+            _lyricProviders = lyricProviders.ToArray();
         }
 
         /// <inheritdoc />
