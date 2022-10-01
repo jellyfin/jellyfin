12cf98b4e3625a69000a24a27e7117d2d31ad4bc
Shadowghost <Ghost_of_Stone@web.de>
2022-10-01 20:18:14 +0200

    Update taglibsharp, add audible tag parsing

--- MediaBrowser.Providers/MediaInfo/AudioFileProber.cs
+++ MediaBrowser.Providers/MediaInfo/AudioFileProber.cs
@@ -134,6 +134,10 @@ namespace MediaBrowser.Providers.MediaInfo
             {
                 tags = file.GetTag(TagTypes.FlacMetadata);
             }
+            else if (tagTypes.HasFlag(TagTypes.AudibleMetadata))
+            {
+                tags = file.GetTag(TagTypes.AudibleMetadata);
+            }
             else if (tagTypes.HasFlag(TagTypes.Id3v1))
             {
                 tags = file.GetTag(TagTypes.Id3v1);
