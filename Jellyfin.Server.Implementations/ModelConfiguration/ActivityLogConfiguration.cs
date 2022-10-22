7c75dcfb9ce026d85f64e2cf60fbd2a74f7275d3
cvium <clausvium@gmail.com>
2022-10-22 10:04:50 +0200

    use filescoped namespace

--- Jellyfin.Server.Implementations/ModelConfiguration/ActivityLogConfiguration.cs
+++ Jellyfin.Server.Implementations/ModelConfiguration/ActivityLogConfiguration.cs
@@ -2,17 +2,16 @@ using Jellyfin.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Jellyfin.Server.Implementations.ModelConfiguration
+namespace Jellyfin.Server.Implementations.ModelConfiguration;
+
+/// <summary>
+/// FluentAPI configuration for the ActivityLog entity.
+/// </summary>
+public class ActivityLogConfiguration : IEntityTypeConfiguration<ActivityLog>
 {
-    /// <summary>
-    /// FluentAPI configuration for the ActivityLog entity.
-    /// </summary>
-    public class ActivityLogConfiguration : IEntityTypeConfiguration<ActivityLog>
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<ActivityLog> builder)
     {
-        /// <inheritdoc/>
-        public void Configure(EntityTypeBuilder<ActivityLog> builder)
-        {
-            builder.HasIndex(entity => entity.DateCreated);
-        }
+        builder.HasIndex(entity => entity.DateCreated);
     }
 }
