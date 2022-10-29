6621121c1b958f0e3726f3b4f1d9c2604e398504
Stefan Hendricks <38368299+Neuheit@users.noreply.github.com>
2022-10-29 18:54:11 -0400

    Add .NET MIT License

--- Jellyfin.Networking/HappyEyeballs/HttpClientExtension.cs
+++ Jellyfin.Networking/HappyEyeballs/HttpClientExtension.cs
@@ -1,3 +1,29 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) .NET Foundation and Contributors
+
+All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 using System.IO;
 using System.Net.Http;
 using System.Net.Sockets;
@@ -17,8 +43,6 @@ namespace Jellyfin.Networking.HappyEyeballs
         /// Gets or sets a value indicating whether the client should use IPv6.
         /// </summary>
         public static bool UseIPv6 { get; set; } = true;
-
-        // Implementation taken from https://github.com/rmkerr/corefx/blob/SocketsHttpHandler_Connect_HappyEyeballs/src/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/ConnectHelper.cs
 
         /// <summary>
         /// Implements the httpclient callback method.
