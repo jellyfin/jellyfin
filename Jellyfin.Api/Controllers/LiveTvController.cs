44077b4f5c34ed7f23888a9cb438e0189134ff65
Shadowghost <Ghost_of_Stone@web.de>
2023-01-20 01:27:39 +0100

    Apply review suggestions

--- Jellyfin.Api/Controllers/LiveTvController.cs
+++ Jellyfin.Api/Controllers/LiveTvController.cs
@@ -1156,7 +1156,6 @@ namespace Jellyfin.Api.Controllers
         /// or a <see cref="NotFoundResult"/> if recording not found.
         /// </returns>
         [HttpGet("LiveRecordings/{recordingId}/stream")]
-        [Authorize(Policy = Policies.LiveTvAccess)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesVideoFile]
@@ -1185,7 +1184,6 @@ namespace Jellyfin.Api.Controllers
         /// or a <see cref="NotFoundResult"/> if stream not found.
         /// </returns>
         [HttpGet("LiveStreamFiles/{streamId}/stream.{container}")]
-        [Authorize(Policy = Policies.LiveTvAccess)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesVideoFile]
