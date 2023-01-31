a2ac791bb779297bedb608825602912228d415c1
Ronan Charles-Lorel <ronan.charleslorel@gmail.com>
2023-01-31 15:20:57 +0100

    Add a way to add more invalid characters when sanitizing a filename

--- Emby.Server.Implementations/IO/ManagedFileSystem.cs
+++ Emby.Server.Implementations/IO/ManagedFileSystem.cs
@@ -294,7 +294,9 @@ namespace Emby.Server.Implementations.IO
         /// <exception cref="ArgumentNullException">The filename is null.</exception>
         public string GetValidFilename(string filename)
         {
-            var invalid = Path.GetInvalidFileNameChars();
+            //necessary because (as per the doc) GetInvalidFileNameChars is not exhaustive and may not return all invalid chars, which creates issues
+            char[] genericInvalidChars = {':'};
+            var invalid = Path.GetInvalidFileNameChars().Concat(genericInvalidChars).ToArray();
             var first = filename.IndexOfAny(invalid);
             if (first == -1)
             {
