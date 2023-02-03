b9c5d0b91a31c3d09435410a819770bc360bf238
holow29 <67209066+holow29@users.noreply.github.com>
2023-02-03 15:20:02 -0500

    Update EncodingHelper.cs
    Remove whitespace
    Replace GetMinValue function with Math.Min() and logic
    Remove commented code

--- MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
+++ MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
@@ -2221,11 +2221,6 @@ namespace MediaBrowser.Controller.MediaEncoding
                 // libmp3lame currently only supports two channel output
                 transcoderChannelLimit = 2;
             }
-//             else if (codec.IndexOf("aac", StringComparison.OrdinalIgnoreCase) != -1)
-//             {
-//                 // aac is able to handle 8ch(7.1 layout)
-//                 transcoderChannelLimit = 8;
-//             }
             else
             {
                 // If we don't have any media info then limit it to 8 to prevent encoding errors due to asking for too many channels
@@ -2235,20 +2230,26 @@ namespace MediaBrowser.Controller.MediaEncoding
             var isTranscodingAudio = !IsCopyCodec(codec);
 
             int? resultChannels = state.GetRequestedAudioChannels(codec);
-            
-             if (inputChannels.HasValue)
+
+            if (inputChannels.HasValue)
             {
                 resultChannels = resultChannels.HasValue
                     ? Math.Min(resultChannels.Value, inputChannels.Value)
                     : inputChannels.Value;
             }
-            
+
             if (isTranscodingAudio)
             {
                 // Set resultChannels to minimum between TranscodingMaxAudioChannels,resultChannels,transcoderChannelLimit
-                resultChannels = GetMinValue(
-                    GetMinValue(request.TranscodingMaxAudioChannels, resultChannels),
-                    GetMinValue(transcoderChannelLimit, resultChannels)); 
+                resultChannels = resultChannels.HasValue
+                    ? Math.Min(
+                        request.TranscodingMaxAudioChannels.HasValue
+                            ? Math.Min(resultChannels.Value, request.TranscodingMaxAudioChannels.Value)
+                            : resultChannels.Value,
+                        transcoderChannelLimit.Value)
+                    : request.TranscodingMaxAudioChannels.HasValue
+                        ? Math.Min(transcoderChannelLimit.Value, request.TranscodingMaxAudioChannels.Value)
+                        : transcoderChannelLimit.Value;
             }
 
             // Avoid transcoding to audio channels other than 1ch, 2ch, 6ch (5.1 layout) and 8ch (7.1 layout).
@@ -2262,21 +2263,6 @@ namespace MediaBrowser.Controller.MediaEncoding
             }
 
             return resultChannels;
-        }
-
-        private int? GetMinValue(int? val1, int? val2)
-        {
-            if (!val1.HasValue)
-            {
-                return val2;
-            }
-
-            if (!val2.HasValue)
-            {
-                return val1;
-            }
-
-            return Math.Min(val1.Value, val2.Value);
         }
 
         /// <summary>
