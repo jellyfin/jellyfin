83ae6fcdb466b2bee17af07ac415f6e5892ff3c7
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-02-04 02:45:24 +0000

    Fix Braces for multi-line statements should not share line

--- MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
+++ MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
@@ -5788,7 +5788,8 @@ namespace MediaBrowser.Controller.MediaEncoding
 
             // Copy the movflags from GetProgressiveVideoFullCommandLine
             // See #9248 and the associated PR for why this is needed
-            var mp4ContainerNames = new HashSet<String> {
+            var mp4ContainerNames = new HashSet<String>
+            {
                 "mp4",
                 "m4a",
                 "m4p",
