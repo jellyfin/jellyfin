ebbd1b9db16cba0f7a970e783eec3066448bf289
Bond-009 <bond.009@outlook.com>
2023-02-07 00:16:03 +0100

    Update MediaBrowser.Providers/Manager/ItemImageProvider.cs

--- MediaBrowser.Providers/Manager/ItemImageProvider.cs
+++ MediaBrowser.Providers/Manager/ItemImageProvider.cs
@@ -313,7 +313,7 @@ namespace MediaBrowser.Providers.Manager
                 }
 
                 minWidth = savedOptions.GetMinWidth(ImageType.Backdrop);
-                var listWithNoLangFirst = list.Where(i => string.IsNullOrEmpty(i.Language)).Concat(list.Where(i => !string.IsNullOrEmpty(i.Language)));
+                var listWithNoLangFirst = list.OrderByDescending(i => string.IsNullOrEmpty(i.Language));
                 await DownloadMultiImages(item, ImageType.Backdrop, refreshOptions, backdropLimit, provider, result, listWithNoLangFirst, minWidth, cancellationToken).ConfigureAwait(false);
             }
             catch (OperationCanceledException)
