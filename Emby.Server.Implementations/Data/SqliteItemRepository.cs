32eccc139c4b35aef04a29cefb11c6130726f617
Cody Robibero <cody@robibe.ro>
2023-02-11 07:46:52 -0700

    LiveTV fixes

--- Emby.Server.Implementations/Data/SqliteItemRepository.cs
+++ Emby.Server.Implementations/Data/SqliteItemRepository.cs
@@ -5440,6 +5440,9 @@ AND Type = @InternalPersonType)");
 
             list.AddRange(inheritedTags.Select(i => (6, i)));
 
+            // Remove all invalid values.
+            list.RemoveAll(i => string.IsNullOrEmpty(i.Item2));
+
             return list;
         }
 
