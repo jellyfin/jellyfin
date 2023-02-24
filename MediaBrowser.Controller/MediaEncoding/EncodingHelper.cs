eafd785eb614bc11982a42e6fa44317936c41665
Shadowghost <Ghost_of_Stone@web.de>
2023-02-24 15:04:52 +0100

    Fix encoder checks for DTS and TrueHD

--- MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
+++ MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
@@ -548,6 +548,11 @@ namespace MediaBrowser.Controller.MediaEncoding
                 return "flac";
             }
 
+            if (string.Equals(codec, "dts", StringComparison.OrdinalIgnoreCase))
+            {
+                return "dca";
+            }
+
             return codec.ToLowerInvariant();
         }
 
