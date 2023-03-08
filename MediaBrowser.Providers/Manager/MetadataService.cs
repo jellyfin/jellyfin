5fdd7386437bd15c7456e2841b0ebc52744ca72b
SenorSmartyPants <senorsmartypants@gmail.com>
2023-03-08 16:28:28 -0600

    Fix linting error

--- MediaBrowser.Providers/Manager/MetadataService.cs
+++ MediaBrowser.Providers/Manager/MetadataService.cs
@@ -704,7 +704,6 @@ namespace MediaBrowser.Providers.Manager
                                 // remove imagetype that has just been downloaded
                                 replaceImages.Remove(remoteImage.Type);
                                 localImagesFound = true;
-
                             }
                             catch (HttpRequestException ex)
                             {
