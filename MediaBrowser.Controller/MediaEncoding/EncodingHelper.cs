3a5503be5ff834ec399189c0ca658385eb17037a
Shadowghost <Ghost_of_Stone@web.de>
2023-03-17 11:49:07 +0100

    Fix condition in CanStreamCopyAudio

--- MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
+++ MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
@@ -1970,9 +1970,9 @@ namespace MediaBrowser.Controller.MediaEncoding
                 }
             }
 
-            // Video bitrate must fall within requested value
+            // Audio bitrate must fall within requested value
             if (request.AudioBitRate.HasValue
-                && audioStream.BitDepth.HasValue
+                && audioStream.BitRate.HasValue
                 && audioStream.BitRate.Value > request.AudioBitRate.Value)
             {
                 return false;
