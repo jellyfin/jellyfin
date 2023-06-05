29368a1566afcbfa861e3cf431156ee0f73f1f2b
Mark Lopez <m@silvenga.com>
2023-06-05 11:46:13 -0500

    Source SQLite cache_size from an Environment Variable (#9666)

--- Emby.Server.Implementations/ConfigurationOptions.cs
+++ Emby.Server.Implementations/ConfigurationOptions.cs
@@ -11,14 +11,15 @@ namespace Emby.Server.Implementations
         /// <summary>
         /// Gets a new copy of the default configuration options.
         /// </summary>
-        public static Dictionary<string, string?> DefaultConfiguration => new Dictionary<string, string?>
+        public static Dictionary<string, string?> DefaultConfiguration => new()
         {
             { HostWebClientKey, bool.TrueString },
             { DefaultRedirectKey, "web/" },
             { FfmpegProbeSizeKey, "1G" },
             { FfmpegAnalyzeDurationKey, "200M" },
             { PlaylistsAllowDuplicatesKey, bool.FalseString },
-            { BindToUnixSocketKey, bool.FalseString }
+            { BindToUnixSocketKey, bool.FalseString },
+            { SqliteCacheSizeKey, "20000" }
         };
     }
 }
