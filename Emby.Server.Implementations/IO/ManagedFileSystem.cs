c21140eeb54fe628de957d07a06bd800ad518f42
Ronan Charles-Lorel <ronan.charleslorel@gmail.com>
2023-07-01 03:24:19 +0200

    Formatting
    Fixes debug build?
    
    Co-authored-by: Bond-009 <bond.009@outlook.com>

--- Emby.Server.Implementations/IO/ManagedFileSystem.cs
+++ Emby.Server.Implementations/IO/ManagedFileSystem.cs
@@ -20,13 +20,15 @@ namespace Emby.Server.Implementations.IO
         private readonly List<IShortcutHandler> _shortcutHandlers = new List<IShortcutHandler>();
         private readonly string _tempPath;
         private static readonly bool _isEnvironmentCaseInsensitive = OperatingSystem.IsWindows();
-        private static readonly char[] _invalidPathCharacters = {
-                '\"', '<', '>', '|', '\0',
-                (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
-                (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
-                (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
-                (char)31, ':', '*', '?', '\\', '/'
-            };
+        private static readonly char[] _invalidPathCharacters =
+        {
+            '\"', '<', '>', '|', '\0',
+            (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
+            (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
+            (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
+            (char)31, ':', '*', '?', '\\', '/'
+         };
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ManagedFileSystem"/> class.
         /// </summary>
