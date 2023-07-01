4dc87a6f93249b15d6660781c0026b77f1066c37
Ronan Charles-Lorel <ronan.charleslorel@gmail.com>
2023-07-01 03:37:18 +0200

    Align indentation on bottom brace of new list
    Should stop error SA1137 in debug build

--- Emby.Server.Implementations/IO/ManagedFileSystem.cs
+++ Emby.Server.Implementations/IO/ManagedFileSystem.cs
@@ -27,7 +27,7 @@ namespace Emby.Server.Implementations.IO
             (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
             (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
             (char)31, ':', '*', '?', '\\', '/'
-         };
+        };
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ManagedFileSystem"/> class.
