61a49e94c4753eb8dbc421530656962f4a7a33c8
Tim Eisele <Shadowghost@users.noreply.github.com>
2023-09-17 14:13:25 +0200

    Update Emby.Server.Implementations/ScheduledTasks/Tasks/CleanupCollectionAndPlaylistPathsTask.cs
    Co-authored-by: Bond-009 <bond.009@outlook.com>

--- Emby.Server.Implementations/ScheduledTasks/Tasks/CleanupCollectionAndPlaylistPathsTask.cs
+++ Emby.Server.Implementations/ScheduledTasks/Tasks/CleanupCollectionAndPlaylistPathsTask.cs
@@ -136,7 +136,6 @@ public class CleanupCollectionAndPlaylistPathsTask : IScheduledTask
                 },
                 RefreshPriority.High);
 
-            itemsToRemove.Clear();
         }
     }
 
