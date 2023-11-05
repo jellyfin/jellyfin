a9ef103c95a7460031879726f4afda3013ca6619
Bond_009 <bond.009@outlook.com>
2023-11-05 02:01:14 +0100

    Add IDisposableAnalyzers to more projects

--- MediaBrowser.MediaEncoding/Subtitles/SubtitleEncoder.cs
+++ MediaBrowser.MediaEncoding/Subtitles/SubtitleEncoder.cs
@@ -135,19 +135,17 @@ namespace MediaBrowser.MediaEncoding.Subtitles
             var subtitleStream = mediaSource.MediaStreams
                .First(i => i.Type == MediaStreamType.Subtitle && i.Index == subtitleStreamIndex);
 
-            var subtitle = await GetSubtitleStream(mediaSource, subtitleStream, cancellationToken)
+            var (stream, inputFormat) = await GetSubtitleStream(mediaSource, subtitleStream, cancellationToken)
                         .ConfigureAwait(false);
-
-            var inputFormat = subtitle.Format;
 
             // Return the original if the same format is being requested
             // Character encoding was already handled in GetSubtitleStream
             if (string.Equals(inputFormat, outputFormat, StringComparison.OrdinalIgnoreCase))
             {
-                return subtitle.Stream;
+                return stream;
             }
 
-            using (var stream = subtitle.Stream)
+            using (stream)
             {
                 return ConvertSubtitles(stream, inputFormat, outputFormat, startTimeTicks, endTimeTicks, preserveOriginalTimestamps, cancellationToken);
             }
