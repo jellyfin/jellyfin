7670fdb1b7a58ab323c717ae7d8735961e319127
Cody Robibero <cody@robibe.ro>
2023-11-09 10:33:05 -0700

    Update Jellyfin.Data/Attributes/OpenApiIgnoreEnumAttribute.cs
    Co-authored-by: Patrick Barron <barronpm@gmail.com>

--- Jellyfin.Data/Attributes/OpenApiIgnoreEnumAttribute.cs
+++ Jellyfin.Data/Attributes/OpenApiIgnoreEnumAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace Jellyfin.Data.Attributes;
 
 /// <summary>
-/// Attribute to specify that the enum value is to be ignored when generating the openapi spec..
+/// Attribute to specify that the enum value is to be ignored when generating the openapi spec.
 /// </summary>
 [AttributeUsage(AttributeTargets.Field)]
 public sealed class OpenApiIgnoreEnumAttribute : Attribute
