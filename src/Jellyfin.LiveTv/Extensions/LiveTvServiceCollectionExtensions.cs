449365182cb710e5d0d18b1d599a53f76b814dd8
Patrick Barron <barronpm@gmail.com>
2024-01-12 19:07:44 -0500

    Move LiveTV service registration to extension method

---
+++ src/Jellyfin.LiveTv/Extensions/LiveTvServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Jellyfin.LiveTv.Channels;
+using MediaBrowser.Controller.Channels;
+using MediaBrowser.Controller.LiveTv;
+using MediaBrowser.Model.IO;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.LiveTv.Extensions;
+
+/// <summary>
+/// Live TV extensions for <see cref="IServiceCollection"/>.
+/// </summary>
+public static class LiveTvServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds Live TV services to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    public static void AddLiveTvServices(this IServiceCollection services)
+    {
+        services.AddSingleton<LiveTvDtoService>();
+        services.AddSingleton<ILiveTvManager, LiveTvManager>();
+        services.AddSingleton<IChannelManager, ChannelManager>();
+        services.AddSingleton<IStreamHelper, StreamHelper>();
+    }
+}
