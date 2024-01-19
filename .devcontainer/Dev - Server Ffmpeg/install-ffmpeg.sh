23c77706838aa2e5cf83ee036f3853a90acc4a68
JPVenson <JPVenson@users.noreply.github.com>
2024-01-19 00:48:03 +0000

    Fixed ffmpeg version updated lauch with ffmpeg

--- .devcontainer/Dev - Server Ffmpeg/install-ffmpeg.sh
+++ .devcontainer/Dev - Server Ffmpeg/install-ffmpeg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo wget https://repo.jellyfin.org/releases/server/ubuntu/versions/jellyfin-ffmpeg/6.0-8/jellyfin-ffmpeg6_6.0-8-focal_amd64.deb -O ffmpeg.deb
+wget https://repo.jellyfin.org/releases/server/ubuntu/versions/jellyfin-ffmpeg/6.0.1-1/jellyfin-ffmpeg6_6.0.1-1-jammy_amd64.deb -O ffmpeg.deb
+
+sudo apt update
 sudo apt install -f ./ffmpeg.deb -y
 rm ffmpeg.deb
