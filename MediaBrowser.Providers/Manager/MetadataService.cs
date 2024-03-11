eb7a89dc110a50f150a007a67d410c7afd9af3d3
LJQ <leejunquan7@gmail.com>
2024-03-11 18:26:07 +0800

    Patch for when Scan for new and updated files are used, title is not replaced

--- MediaBrowser.Providers/Manager/MetadataService.cs
+++ MediaBrowser.Providers/Manager/MetadataService.cs
@@ -773,7 +773,15 @@ namespace MediaBrowser.Providers.Manager
                             MergeData(metadata, temp, Array.Empty<MetadataField>(), false, false);
                         }
 
-                        MergeData(temp, metadata, item.LockedFields, options.ReplaceAllMetadata, false);
+                        // For when new and updated files are scanned, metadata is always replaced since the data at this point is new and there is no prior data.
+                        if (options.MetadataRefreshMode == MetadataRefreshMode.Default)
+                        {
+                            MergeData(temp, metadata, item.LockedFields, true, false);
+                        }
+                        else
+                        {
+                            MergeData(temp, metadata, item.LockedFields, options.ReplaceAllMetadata, false);
+                        }
                     }
                 }
             }
