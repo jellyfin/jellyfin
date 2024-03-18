6726650b3a8c9596feb819f4037958c3f81aa8a2
Cody Robibero <cody@robibe.ro>
2024-03-18 17:47:41 -0600

    Add default auth policy to generated openapi spec

--- Jellyfin.Server/Filters/SecurityRequirementsOperationFilter.cs
+++ Jellyfin.Server/Filters/SecurityRequirementsOperationFilter.cs
@@ -6,86 +6,72 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace Jellyfin.Server.Filters
+namespace Jellyfin.Server.Filters;
+
+/// <summary>
+/// Security requirement operation filter.
+/// </summary>
+public class SecurityRequirementsOperationFilter : IOperationFilter
 {
-    /// <summary>
-    /// Security requirement operation filter.
-    /// </summary>
-    public class SecurityRequirementsOperationFilter : IOperationFilter
+    private const string DefaultAuthPolicy = "Default";
+    private static readonly Type _attributeType = typeof(AuthorizeAttribute);
+
+    /// <inheritdoc />
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        /// <inheritdoc />
-        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        var requiredScopes = new List<string>();
+
+        var requiresAuth = false;
+        // Add all method scopes.
+        foreach (var authorizeAttribute in context.MethodInfo.GetCustomAttributes(_attributeType, true).Cast<AuthorizeAttribute>())
         {
-            var requiredScopes = new List<string>();
-
-            var requiresAuth = false;
-            // Add all method scopes.
-            foreach (var attribute in context.MethodInfo.GetCustomAttributes(true))
+            requiresAuth = true;
+            var policy = authorizeAttribute.Policy ?? DefaultAuthPolicy;
+            if (!requiredScopes.Contains(policy, StringComparer.Ordinal))
             {
-                if (attribute is not AuthorizeAttribute authorizeAttribute)
-                {
-                    continue;
-                }
-
-                requiresAuth = true;
-                if (authorizeAttribute.Policy is not null
-                    && !requiredScopes.Contains(authorizeAttribute.Policy, StringComparer.Ordinal))
-                {
-                    requiredScopes.Add(authorizeAttribute.Policy);
-                }
+                requiredScopes.Add(policy);
             }
-
-            // Add controller scopes if any.
-            var controllerAttributes = context.MethodInfo.DeclaringType?.GetCustomAttributes(true);
-            if (controllerAttributes is not null)
-            {
-                foreach (var attribute in controllerAttributes)
-                {
-                    if (attribute is not AuthorizeAttribute authorizeAttribute)
-                    {
-                        continue;
-                    }
-
-                    requiresAuth = true;
-                    if (authorizeAttribute.Policy is not null
-                        && !requiredScopes.Contains(authorizeAttribute.Policy, StringComparer.Ordinal))
-                    {
-                        requiredScopes.Add(authorizeAttribute.Policy);
-                    }
-                }
-            }
-
-            if (!requiresAuth)
-            {
-                return;
-            }
-
-            if (!operation.Responses.ContainsKey("401"))
-            {
-                operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
-            }
-
-            if (!operation.Responses.ContainsKey("403"))
-            {
-                operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
-            }
-
-            var scheme = new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = AuthenticationSchemes.CustomAuthentication
-                }
-            };
-
-            operation.Security = new List<OpenApiSecurityRequirement>
-            {
-                new OpenApiSecurityRequirement
-                {
-                    [scheme] = requiredScopes
-                }
-            };
         }
+
+        // Add controller scopes if any.
+        var controllerAttributes = context.MethodInfo.DeclaringType?.GetCustomAttributes(_attributeType, true).Cast<AuthorizeAttribute>();
+        if (controllerAttributes is not null)
+        {
+            foreach (var authorizeAttribute in controllerAttributes)
+            {
+                requiresAuth = true;
+                var policy = authorizeAttribute.Policy ?? DefaultAuthPolicy;
+                if (!requiredScopes.Contains(policy, StringComparer.Ordinal))
+                {
+                    requiredScopes.Add(policy);
+                }
+            }
+        }
+
+        if (!requiresAuth)
+        {
+            return;
+        }
+
+        if (!operation.Responses.ContainsKey("401"))
+        {
+            operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+        }
+
+        if (!operation.Responses.ContainsKey("403"))
+        {
+            operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
+        }
+
+        var scheme = new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = AuthenticationSchemes.CustomAuthentication
+            },
+        };
+
+        operation.Security = [new OpenApiSecurityRequirement { [scheme] = requiredScopes }];
     }
 }
