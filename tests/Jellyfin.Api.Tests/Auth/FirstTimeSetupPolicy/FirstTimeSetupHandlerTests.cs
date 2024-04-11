92eb9e3a94b3923172f1b0f5ea7b7dacd21ead5b
Cody Robibero <cody@robibe.ro>
2024-04-10 22:32:37 -0600

    Always grant access for Administrator role

--- tests/Jellyfin.Api.Tests/Auth/FirstTimeSetupPolicy/FirstTimeSetupHandlerTests.cs
+++ tests/Jellyfin.Api.Tests/Auth/FirstTimeSetupPolicy/FirstTimeSetupHandlerTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoMoq;
@@ -66,6 +67,17 @@ namespace Jellyfin.Api.Tests.Auth.FirstTimeSetupPolicy
 
             await _firstTimeSetupHandler.HandleAsync(context);
             Assert.Equal(shouldSucceed, context.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task ShouldAllowAdminApiKeyIfStartupWizardComplete()
+        {
+            TestHelpers.SetupConfigurationManager(_configurationManagerMock, true);
+            var claims = new ClaimsPrincipal(new ClaimsIdentity([new Claim(ClaimTypes.Role, UserRoles.Administrator)]));
+            var context = new AuthorizationHandlerContext(_requirements, claims, null);
+
+            await _firstTimeSetupHandler.HandleAsync(context);
+            Assert.True(context.HasSucceeded);
         }
     }
 }
