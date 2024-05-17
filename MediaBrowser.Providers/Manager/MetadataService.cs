8b938e26962dec132ff43ad6741effc71c1d162d
Shadowghost <Shadowghost@users.noreply.github.com>
2024-05-17 13:51:50 -0400

    Backport pull request #11673 from jellyfin/release-10.9.z
    Fix local playlist scanning
    
    Original-merge: 26714e2c62fb0fe1138bcb731ad7ad4d62bc7637
    
    Merged-by: nielsvanvelzen <nielsvanvelzen@users.noreply.github.com>
    
    Backported-by: Joshua M. Boniface <joshua@boniface.me>

--- MediaBrowser.Providers/Manager/MetadataService.cs
+++ MediaBrowser.Providers/Manager/MetadataService.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Jellyfin.Data.Enums;
 using Jellyfin.Extensions;
 using MediaBrowser.Controller.Configuration;
 using MediaBrowser.Controller.Entities;
