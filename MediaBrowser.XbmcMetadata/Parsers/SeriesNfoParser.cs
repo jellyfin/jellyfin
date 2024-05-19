9a9e8e26487b556d8c008e68bd2db266cb9b9ce8
Shadowghost <Ghost_of_Stone@web.de>
2024-05-19 18:36:18 +0200

    Prevent infite loop

--- MediaBrowser.XbmcMetadata/Parsers/SeriesNfoParser.cs
+++ MediaBrowser.XbmcMetadata/Parsers/SeriesNfoParser.cs
@@ -102,6 +102,7 @@ namespace MediaBrowser.XbmcMetadata.Parsers
 
                 // Season names are processed by SeasonFromSeriesNfoParser
                 case "namedseason":
+                    reader.Skip();
                     break;
                 default:
                     base.FetchDataFromXmlNode(reader, itemResult);
