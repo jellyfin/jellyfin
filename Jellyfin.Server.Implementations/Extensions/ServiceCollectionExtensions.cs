b9c0fc69e8fee820b06da2fed9e7136711d8028d
gnattu <gnattuoc@me.com>
2024-05-25 01:12:00 +0800

    Add Env Var to disable second level cache
    This is an attempt to track down possible causes of remaining database lockups. Add an environment variable to disable the second-level cache entirely to see if it works better on systems that still experience lockups.
    
    Signed-off-by: gnattu <gnattuoc@me.com>

--- Jellyfin.Server.Implementations/Extensions/ServiceCollectionExtensions.cs
+++ Jellyfin.Server.Implementations/Extensions/ServiceCollectionExtensions.cs
@@ -16,21 +16,28 @@ public static class ServiceCollectionExtensions
     /// Adds the <see cref="IDbContextFactory{TContext}"/> interface to the service collection with second level caching enabled.
     /// </summary>
     /// <param name="serviceCollection">An instance of the <see cref="IServiceCollection"/> interface.</param>
+    /// <param name="disableSecondLevelCache">Whether second level cache disabled..</param>
     /// <returns>The updated service collection.</returns>
-    public static IServiceCollection AddJellyfinDbContext(this IServiceCollection serviceCollection)
+    public static IServiceCollection AddJellyfinDbContext(this IServiceCollection serviceCollection, bool disableSecondLevelCache)
     {
-        serviceCollection.AddEFSecondLevelCache(options =>
-            options.UseMemoryCacheProvider()
-                .CacheAllQueries(CacheExpirationMode.Sliding, TimeSpan.FromMinutes(10))
-                .UseCacheKeyPrefix("EF_")
-                // Don't cache null values. Remove this optional setting if it's not necessary.
-                .SkipCachingResults(result => result.Value is null or EFTableRows { RowsCount: 0 }));
+        if (!disableSecondLevelCache)
+        {
+            serviceCollection.AddEFSecondLevelCache(options =>
+                options.UseMemoryCacheProvider()
+                    .CacheAllQueries(CacheExpirationMode.Sliding, TimeSpan.FromMinutes(10))
+                    .UseCacheKeyPrefix("EF_")
+                    // Don't cache null values. Remove this optional setting if it's not necessary.
+                    .SkipCachingResults(result => result.Value is null or EFTableRows { RowsCount: 0 }));
+        }
 
         serviceCollection.AddPooledDbContextFactory<JellyfinDbContext>((serviceProvider, opt) =>
         {
             var applicationPaths = serviceProvider.GetRequiredService<IApplicationPaths>();
-            opt.UseSqlite($"Filename={Path.Combine(applicationPaths.DataPath, "jellyfin.db")}")
-                .AddInterceptors(serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>());
+            var dbOpt = opt.UseSqlite($"Filename={Path.Combine(applicationPaths.DataPath, "jellyfin.db")}");
+            if (!disableSecondLevelCache)
+            {
+                dbOpt.AddInterceptors(serviceProvider.GetRequiredService<SecondLevelCacheInterceptor>());
+            }
         });
 
         return serviceCollection;
