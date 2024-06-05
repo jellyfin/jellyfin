926470829d91d93b4c0b22c5b8b89a791abbb434
Shadowghost <Shadowghost@users.noreply.github.com>
2024-06-05 17:03:03 -0400

    Backport pull request #11958 from jellyfin/release-10.9.z
    Export trailer URLs in new format
    
    Original-merge: 47c5e0c2c718b02cdbbecb633d506fdde52214d7
    
    Merged-by: nielsvanvelzen <nielsvanvelzen@users.noreply.github.com>
    
    Backported-by: Joshua M. Boniface <joshua@boniface.me>

--- MediaBrowser.XbmcMetadata/Savers/BaseNfoSaver.cs
+++ MediaBrowser.XbmcMetadata/Savers/BaseNfoSaver.cs
@@ -825,7 +825,7 @@ namespace MediaBrowser.XbmcMetadata.Savers
         private string GetOutputTrailerUrl(string url)
         {
             // This is what xbmc expects
-            return url.Replace(YouTubeWatchUrl, "plugin://plugin.video.youtube/?action=play_video&videoid=", StringComparison.OrdinalIgnoreCase);
+            return url.Replace(YouTubeWatchUrl, "plugin://plugin.video.youtube/play/?video_id=", StringComparison.OrdinalIgnoreCase);
         }
 
         private void AddImages(BaseItem item, XmlWriter writer, ILibraryManager libraryManager)
