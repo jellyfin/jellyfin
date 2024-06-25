ea7e834ae18d31b453d1163ab7f43cb8df6fa4b0
gnattu <gnattu@users.noreply.github.com>
2024-06-24 20:29:05 -0400

    Backport pull request #12126 from jellyfin/release-10.9.z
    Try to add extracted lyrics during scanning
    
    Original-merge: 25f02658f08c484338d4288e048a6e71dfb39d96
    
    Merged-by: Bond-009 <bond.009@outlook.com>
    
    Backported-by: Joshua M. Boniface <joshua@boniface.me>

--- MediaBrowser.Providers/MediaInfo/AudioFileProber.cs
+++ MediaBrowser.Providers/MediaInfo/AudioFileProber.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Jellyfin.Data.Enums;
+using Jellyfin.Extensions;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Audio;
 using MediaBrowser.Controller.Library;
@@ -136,6 +137,10 @@ namespace MediaBrowser.Providers.MediaInfo
             if (!audio.IsLocked)
             {
                 await FetchDataFromTags(audio, mediaInfo, options, tryExtractEmbeddedLyrics).ConfigureAwait(false);
+                if (tryExtractEmbeddedLyrics)
+                {
+                    AddExternalLyrics(audio, mediaStreams, options);
+                }
             }
 
             audio.HasLyrics = mediaStreams.Any(s => s.Type == MediaStreamType.Lyric);
@@ -369,7 +374,10 @@ namespace MediaBrowser.Providers.MediaInfo
             var externalLyricFiles = _lyricResolver.GetExternalStreams(audio, startIndex, options.DirectoryService, false);
 
             audio.LyricFiles = externalLyricFiles.Select(i => i.Path).Distinct().ToArray();
-            currentStreams.AddRange(externalLyricFiles);
+            if (externalLyricFiles.Count > 0)
+            {
+                currentStreams.Add(externalLyricFiles[0]);
+            }
         }
     }
 }
