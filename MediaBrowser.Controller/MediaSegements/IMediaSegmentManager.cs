381ffc4a68e42d4dc640700b17cb69508d51fb3e
JPVenson <JPVenson@users.noreply.github.com>
2024-08-05 14:43:32 +0000

    Fixed merge conflicts

--- MediaBrowser.Controller/MediaSegements/IMediaSegmentManager.cs
+++ MediaBrowser.Controller/MediaSegements/IMediaSegmentManager.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Threading;
-=======
->>>>>>> master
 using System.Threading.Tasks;
 using Jellyfin.Data.Entities;
 using Jellyfin.Data.Enums;
