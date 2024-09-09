36e62793f51074b580d140cc43e0ba3e1331680a
gnattu <gnattuoc@me.com>
2024-09-09 10:50:14 +0800

    Fix config
    Signed-off-by: gnattu <gnattuoc@me.com>

--- Emby.Server.Implementations/ConfigurationOptions.cs
+++ Emby.Server.Implementations/ConfigurationOptions.cs
@@ -20,8 +20,7 @@ namespace Emby.Server.Implementations
             { PlaylistsAllowDuplicatesKey, bool.FalseString },
             { BindToUnixSocketKey, bool.FalseString },
             { SqliteCacheSizeKey, "20000" },
-            { FfmpegSkipValidationKey, bool.FalseString }
-            { SqliteCacheSizeKey, "20000" },
+            { FfmpegSkipValidationKey, bool.FalseString },
             { DetectNetworkChangeKey, bool.TrueString }
         };
     }
