121491e422e0ca07b8135ae0d8a2a35378d1e01b
gnattu <gnattuoc@me.com>
2024-10-04 15:25:36 +0800

    Extract DoVi thumbnail at 4000nit
    DoVi videos without fallback layers are usually mastered at 4000nit and the default 1000nit extract may result in too bright thumbnail.

--- MediaBrowser.MediaEncoding/Encoder/MediaEncoder.cs
+++ MediaBrowser.MediaEncoding/Encoder/MediaEncoder.cs
@@ -701,8 +701,9 @@ namespace MediaBrowser.MediaEncoding.Encoder
             {
                 if (SupportsFilter("tonemapx"))
                 {
+                    var peak = videoStream.VideoRangeType == VideoRangeType.DOVI ? "400" : "100";
                     enableHdrExtraction = true;
-                    filters.Add("tonemapx=tonemap=bt2390:desat=0:peak=100:t=bt709:m=bt709:p=bt709:format=yuv420p");
+                    filters.Add($"tonemapx=tonemap=bt2390:desat=0:peak={peak}:t=bt709:m=bt709:p=bt709:format=yuv420p");
                 }
                 else if (SupportsFilter("zscale") && videoStream.VideoRangeType != VideoRangeType.DOVI)
                 {
