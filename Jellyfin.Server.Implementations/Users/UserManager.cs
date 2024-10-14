68e4cd165711670a688ef0582cb49e6eee856d15
Seth Pattee <seth.pattee@students.snow.edu>
2024-10-14 11:28:38 -0600

    Fixed and Tested + in username

--- Jellyfin.Server.Implementations/Users/UserManager.cs
+++ Jellyfin.Server.Implementations/Users/UserManager.cs
@@ -113,7 +113,7 @@ namespace Jellyfin.Server.Implementations.Users
         // This is some regex that matches only on unicode "word" characters, as well as -, _ and @
         // In theory this will cut out most if not all 'control' characters which should help minimize any weirdness
         // Usernames can contain letters (a-z + whatever else unicode is cool with), numbers (0-9), at-signs (@), dashes (-), underscores (_), apostrophes ('), periods (.) and spaces ( )
-        [GeneratedRegex(@"^[\w\ \-'._@]+$")]
+        [GeneratedRegex(@"^[\w\ \-'._@+]+$")]
         private static partial Regex ValidUsernameRegex();
 
         /// <inheritdoc/>
