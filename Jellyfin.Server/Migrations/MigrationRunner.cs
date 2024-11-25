26d3b909fcf87255340eb21910e18ce44c617900
JPVenson <ger-delta-07@hotmail.de>
2024-11-25 09:14:48 +0100

    Update MigrationRunner.cs

--- Jellyfin.Server/Migrations/MigrationRunner.cs
+++ Jellyfin.Server/Migrations/MigrationRunner.cs
@@ -89,7 +89,7 @@ namespace Jellyfin.Server.Migrations
                  : new MigrationOptions();
 
             // 10.10 specific EFCore migration check.
-            if (migrationOptions.Applied.Any(f => f.Id.ToString("D") == "5C4B82A2-F053-4009-BD05-B6FCAD82F14C" || f.Name == "MigrateUserDatabase"))
+            if (migrationOptions.Applied.Any(f => f.Id.ToString("D").Equals("36445464-849f-429f-9ad0-bb130efa0664", StringComparison.InvariantCulture) || f.Name == "MigrateLibraryDbData"))
             {
                 throw new InvalidOperationException("You cannot downgrade your jellyfin install from the library.db migration.");
             }
