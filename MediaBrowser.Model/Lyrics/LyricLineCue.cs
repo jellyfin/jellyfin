f89552ef40245a6f35ba78142500c5d1dafd4d74
Alex <48403821+AlexDalas@users.noreply.github.com>
2024-12-05 00:55:13 +1100

    Remove width

--- MediaBrowser.Model/Lyrics/LyricLineCue.cs
+++ MediaBrowser.Model/Lyrics/LyricLineCue.cs
@@ -9,13 +9,11 @@ public class LyricLineCue
     /// Initializes a new instance of the <see cref="LyricLineCue"/> class.
     /// </summary>
     /// <param name="position">The start of the character index of the lyric.</param>
-    /// <param name="width">The count of the characters in the lyric.</param>
     /// <param name="start">The start of the timestamp the lyric is synced to in ticks.</param>
     /// <param name="end">The end of the timestamp the lyric is synced to in ticks.</param>
-    public LyricLineCue(int position, int width, long start, long end)
+    public LyricLineCue(int position, long start, long end)
     {
         Position = position;
-        Width = width;
         Start = start;
         End = end;
     }
@@ -24,11 +22,6 @@ public class LyricLineCue
     /// Gets the character index of the lyric.
     /// </summary>
     public int Position { get; }
-
-    /// <summary>
-    /// Gets the count of the characters in the lyric.
-    /// </summary>
-    public int Width { get; }
 
     /// <summary>
     /// Gets the timestamp the lyric is synced to in ticks.
