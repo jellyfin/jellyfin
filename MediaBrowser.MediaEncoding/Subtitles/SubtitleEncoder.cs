dc88d6f7d99490b70a258a03a14de4acfedca8f4
Carsten Braun <carsten.braun@braun-cloud.de>
2025-01-03 08:19:22 +0100

    Use string.IsNullOrEmpty instead of regular null check.

--- MediaBrowser.MediaEncoding/Subtitles/SubtitleEncoder.cs
+++ MediaBrowser.MediaEncoding/Subtitles/SubtitleEncoder.cs
@@ -539,7 +539,7 @@ namespace MediaBrowser.MediaEncoding.Subtitles
 
             foreach (var subtitleStream in subtitleStreams)
             {
-                if (subtitleStream.Path is null || !subtitleStream.Path.EndsWith(".mks", StringComparison.OrdinalIgnoreCase))
+                if (string.IsNullOrEmpty(subtitleStream.Path) || !subtitleStream.Path.EndsWith(".mks", StringComparison.OrdinalIgnoreCase))
                 {
                     continue;
                 }
@@ -610,7 +610,7 @@ namespace MediaBrowser.MediaEncoding.Subtitles
 
             foreach (var subtitleStream in subtitleStreams)
             {
-                if (subtitleStream.Path is not null && subtitleStream.Path.EndsWith(".mks", StringComparison.OrdinalIgnoreCase))
+                if (!string.IsNullOrEmpty(subtitleStream.Path) && subtitleStream.Path.EndsWith(".mks", StringComparison.OrdinalIgnoreCase))
                 {
                     _logger.LogDebug("Subtitle {Index} for file {InputPath} is part in an MKS file. Skipping", inputPath, subtitleStream.Index);
                     continue;
