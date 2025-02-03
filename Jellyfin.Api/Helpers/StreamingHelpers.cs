b79f96e98bdedef354c56ab2389cbcd8c8035d88
gnattu <gnattu@users.noreply.github.com>
2025-02-03 16:55:55 -0500

    Backport pull request #13169 from jellyfin/release-10.10.z
    Check if the video has an audio track before codec fallback
    
    Original-merge: ff4f3b04411833dd7675be870dde9212394acf95
    
    Merged-by: Bond-009 <bond.009@outlook.com>
    
    Backported-by: Bond_009 <bond.009@outlook.com>

--- Jellyfin.Api/Helpers/StreamingHelpers.cs
+++ Jellyfin.Api/Helpers/StreamingHelpers.cs
@@ -236,7 +236,7 @@ public static class StreamingHelpers
                 }
             }
 
-            if (!EncodingHelper.IsCopyCodec(state.OutputAudioCodec) && string.Equals(state.AudioStream.Codec, state.OutputAudioCodec, StringComparison.OrdinalIgnoreCase) && state.OutputAudioBitrate.HasValue)
+            if (state.AudioStream is not null && !EncodingHelper.IsCopyCodec(state.OutputAudioCodec) && string.Equals(state.AudioStream.Codec, state.OutputAudioCodec, StringComparison.OrdinalIgnoreCase) && state.OutputAudioBitrate.HasValue)
             {
                 state.OutputAudioCodec = state.SupportedAudioCodecs.Where(c => !EncodingHelper.LosslessAudioCodecs.Contains(c)).FirstOrDefault(mediaEncoder.CanEncodeToAudioCodec);
             }
