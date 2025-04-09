5fe22926e1388d575502dd69ce57490828c9062b
JPVenson <github@jpb.email>
2025-04-09 23:07:22 +0000

    Marked old properties obsolete and add new DirectoryInfo ones

--- MediaBrowser.Model/System/SystemInfo.cs
+++ MediaBrowser.Model/System/SystemInfo.cs
@@ -76,43 +76,92 @@ namespace MediaBrowser.Model.System
         /// Gets or sets the program data path.
         /// </summary>
         /// <value>The program data path.</value>
-        public FolderStorageInfo ProgramDataPath { get; set; }
+        [Obsolete("Use the newer DirectoryInfo instead")]
+        public string ProgramDataPath { get; set; }
 
         /// <summary>
         /// Gets or sets the web UI resources path.
         /// </summary>
         /// <value>The web UI resources path.</value>
-        public FolderStorageInfo WebPath { get; set; }
+        [Obsolete("Use the newer DirectoryInfo instead")]
+        public string WebPath { get; set; }
 
         /// <summary>
         /// Gets or sets the items by name path.
         /// </summary>
         /// <value>The items by name path.</value>
-        public FolderStorageInfo ItemsByNamePath { get; set; }
+        [Obsolete("Use the newer DirectoryInfo instead")]
+        public string ItemsByNamePath { get; set; }
 
         /// <summary>
         /// Gets or sets the cache path.
         /// </summary>
         /// <value>The cache path.</value>
-        public FolderStorageInfo CachePath { get; set; }
+        [Obsolete("Use the newer DirectoryInfo instead")]
+        public string CachePath { get; set; }
 
         /// <summary>
         /// Gets or sets the log path.
         /// </summary>
         /// <value>The log path.</value>
-        public FolderStorageInfo LogPath { get; set; }
+        [Obsolete("Use the newer DirectoryInfo instead")]
+        public string LogPath { get; set; }
 
         /// <summary>
         /// Gets or sets the internal metadata path.
         /// </summary>
         /// <value>The internal metadata path.</value>
-        public FolderStorageInfo InternalMetadataPath { get; set; }
+        [Obsolete("Use the newer DirectoryInfo instead")]
+        public string InternalMetadataPath { get; set; }
 
         /// <summary>
         /// Gets or sets the transcode path.
         /// </summary>
         /// <value>The transcode path.</value>
-        public FolderStorageInfo TranscodingTempPath { get; set; }
+        [Obsolete("Use the newer DirectoryInfo instead")]
+        public string TranscodingTempPath { get; set; }
+
+          /// <summary>
+        /// Gets or sets the program data path.
+        /// </summary>
+        /// <value>The program data path.</value>
+        public FolderStorageInfo ProgramDataDirectoryInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the web UI resources path.
+        /// </summary>
+        /// <value>The web UI resources path.</value>
+        public FolderStorageInfo WebDirectoryInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the items by name path.
+        /// </summary>
+        /// <value>The items by name path.</value>
+        public FolderStorageInfo ItemsByNameDirectoryInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cache path.
+        /// </summary>
+        /// <value>The cache path.</value>
+        public FolderStorageInfo CacheDirectoryInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the log path.
+        /// </summary>
+        /// <value>The log path.</value>
+        public FolderStorageInfo LogDirectoryInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the internal metadata path.
+        /// </summary>
+        /// <value>The internal metadata path.</value>
+        public FolderStorageInfo InternalMetadataDirectoryInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the transcode path.
+        /// </summary>
+        /// <value>The transcode path.</value>
+        public FolderStorageInfo TranscodingTempDirectoryInfo { get; set; }
 
         /// <summary>
         /// Gets or sets the list of cast receiver applications.
