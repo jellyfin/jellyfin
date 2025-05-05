jellyfin/jellyfin

commit 0c3ba30de214eddcd6118c3b695b08e5482bf7ed

Cleanup file related code (#14023)

Emby.Server.Implementations/IO/ManagedFileSystem.cs
@@ -159,13 +159,13 @@ namespace Emby.Server.Implementations.IO
             catch (IOException)
             {
                 // Cross device move requires a copy
-                Directory.CreateDirectory(destination);
-                foreach (string file in Directory.GetFiles(source))
+                var directory = Directory.CreateDirectory(destination);
+                foreach (var file in directory.EnumerateFiles())
                 {
-                    File.Copy(file, Path.Combine(destination, Path.GetFileName(file)), true);
+                    file.CopyTo(Path.Combine(destination, file.Name), true);
                 }
 
-                Directory.Delete(source, true);
+                directory.Delete(true);
             }
         }
 

Emby.Server.Implementations/AppBase/BaseApplicationPaths.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Jellyfin.Extensions;
 using MediaBrowser.Common.Configuration;
 
 namespace Emby.Server.Implementations.AppBase
@@ -91,10 +92,7 @@ namespace Emby.Server.Implementations.AppBase
         /// <inheritdoc />
         public void CreateAndCheckMarker(string path, string markerName, bool recursive = false)
         {
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
+            Directory.CreateDirectory(path);
 
             CheckOrCreateMarker(path, $".jellyfin-{markerName}", recursive);
         }
@@ -115,7 +113,7 @@ namespace Emby.Server.Implementations.AppBase
             var markerPath = Path.Combine(path, markerName);
             if (!File.Exists(markerPath))
             {
-                File.Create(markerPath).Dispose();
+                FileHelper.CreateEmpty(markerPath);
             }
         }
     }

Emby.Server.Implementations/Library/DotIgnoreIgnoreRule.cs
@@ -20,7 +20,7 @@ public class DotIgnoreIgnoreRule : IResolverIgnoreRule
         }
 
         var parentDir = directory.Parent;
-        if (parentDir == null || parentDir.FullName == directory.FullName)
+        if (parentDir is null)
         {
             return null;
         }

Emby.Server.Implementations/Library/LibraryManager.cs
@@ -2945,7 +2945,7 @@ namespace Emby.Server.Implementations.Library
                 {
                     var path = Path.Combine(virtualFolderPath, collectionType.ToString()!.ToLowerInvariant() + ".collection"); // Can't be null with legal values?
 
-                    await File.WriteAllBytesAsync(path, []).ConfigureAwait(false);
+                    FileHelper.CreateEmpty(path);
                 }
 
                 CollectionFolder.SaveLibraryOptions(virtualFolderPath, options);

Emby.Server.Implementations/Library/MediaSourceManager.cs
@@ -681,17 +681,17 @@ namespace Emby.Server.Implementations.Library
 
                 mediaInfo = await _mediaEncoder.GetMediaInfo(
                     new MediaInfoRequest
-                {
-                    MediaSource = mediaSource,
-                    MediaType = isAudio ? DlnaProfileType.Audio : DlnaProfileType.Video,
-                    ExtractChapters = false
-                },
+                    {
+                        MediaSource = mediaSource,
+                        MediaType = isAudio ? DlnaProfileType.Audio : DlnaProfileType.Video,
+                        ExtractChapters = false
+                    },
                     cancellationToken).ConfigureAwait(false);
 
                 if (cacheFilePath is not null)
                 {
                     Directory.CreateDirectory(Path.GetDirectoryName(cacheFilePath));
-                    FileStream createStream = File.Create(cacheFilePath);
+                    FileStream createStream = AsyncFile.Create(cacheFilePath);
                     await using (createStream.ConfigureAwait(false))
                     {
                         await JsonSerializer.SerializeAsync(createStream, mediaInfo, _jsonOptions, cancellationToken).ConfigureAwait(false);

Emby.Server.Implementations/Localization/LocalizationManager.cs
@@ -520,7 +520,7 @@ namespace Emby.Server.Implementations.Localization
         public bool TryGetISO6392TFromB(string isoB, [NotNullWhen(true)] out string? isoT)
         {
             // Unlikely case the dictionary is not (yet) initialized properly
-            if (_iso6392BtoT == null)
+            if (_iso6392BtoT is null)
             {
                 isoT = null;
                 return false;

Jellyfin.Api/Controllers/SyncPlayController.cs
@@ -125,7 +125,7 @@ public class SyncPlayController : BaseJellyfinApiController
     {
         var currentSession = await RequestHelpers.GetSession(_sessionManager, _userManager, HttpContext).ConfigureAwait(false);
         var group = _syncPlayManager.GetGroup(currentSession, id);
-        return group == null ? NotFound() : Ok(group);
+        return group is null ? NotFound() : Ok(group);
     }
 
     /// <summary>

Jellyfin.Server.Implementations/StorageHelpers/StorageHelper.cs
@@ -72,10 +72,7 @@ public static class StorageHelper
     private static void TestDataDirectorySize(string path, ILogger logger, long threshold = -1)
     {
         logger.LogDebug("Check path {TestPath} for storage capacity", path);
-        if (!Directory.Exists(path))
-        {
-            Directory.CreateDirectory(path);
-        }
+        Directory.CreateDirectory(path);
 
         var drive = new DriveInfo(path);
         if (threshold != -1 && drive.AvailableFreeSpace < threshold)

commit b88130adee5679ac08852fbe27c0e769d625b109

Fix cross filesystem dir moving
Regression from #14023

Emby.Server.Implementations/IO/ManagedFileSystem.cs
@@ -159,13 +159,14 @@ namespace Emby.Server.Implementations.IO
             catch (IOException)
             {
                 // Cross device move requires a copy
-                var directory = Directory.CreateDirectory(destination);
-                foreach (var file in directory.EnumerateFiles())
+                Directory.CreateDirectory(destination);
+                var sourceDir = new DirectoryInfo(source);
+                foreach (var file in sourceDir.EnumerateFiles())
                 {
                     file.CopyTo(Path.Combine(destination, file.Name), true);
                 }
 
-                directory.Delete(true);
+                sourceDir.Delete(true);
             }
         }
 