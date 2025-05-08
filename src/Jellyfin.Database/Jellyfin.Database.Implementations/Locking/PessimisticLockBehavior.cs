0e58da2f3b08fb68bae364835d26dbdfcc4a055e
JPVenson <github@jpb.email>
2025-05-08 06:37:22 +0000

    Also lock non-queries on pessimistic locking

--- src/Jellyfin.Database/Jellyfin.Database.Implementations/Locking/PessimisticLockBehavior.cs
+++ src/Jellyfin.Database/Jellyfin.Database.Implementations/Locking/PessimisticLockBehavior.cs
@@ -1,3 +1,6 @@
+#pragma warning disable MT1013 // Releasing lock without guarantee of execution
+#pragma warning disable MT1012 // Acquiring lock without guarantee of releasing
+
 using System;
 using System.Data.Common;
 using System.Threading;
@@ -36,7 +39,10 @@ public class PessimisticLockBehavior : IEntityFrameworkCoreLockingBehavior
         }
         finally
         {
-            DatabaseLock.ExitWriteLock();
+            if (DatabaseLock.IsWriteLockHeld)
+            {
+                DatabaseLock.ExitWriteLock();
+            }
         }
     }
 
@@ -57,7 +63,10 @@ public class PessimisticLockBehavior : IEntityFrameworkCoreLockingBehavior
         }
         finally
         {
-            DatabaseLock.ExitWriteLock();
+            if (DatabaseLock.IsWriteLockHeld)
+            {
+                DatabaseLock.ExitWriteLock();
+            }
         }
     }
 
@@ -66,54 +75,97 @@ public class PessimisticLockBehavior : IEntityFrameworkCoreLockingBehavior
     /// </summary>
     private sealed class LockingInterceptor : DbCommandInterceptor
     {
+        public override InterceptionResult<int> NonQueryExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<int> result)
+        {
+            if (!DatabaseLock.IsWriteLockHeld) // enter a write lock as NonQueries are used to manipulate data only if not already held
+            {
+                DatabaseLock.EnterWriteLock();
+            }
+
+            return base.NonQueryExecuting(command, eventData, result);
+        }
+
+        public override int NonQueryExecuted(DbCommand command, CommandExecutedEventData eventData, int result)
+        {
+            if (DatabaseLock.IsWriteLockHeld)
+            {
+                DatabaseLock.ExitWriteLock();
+            }
+
+            return base.NonQueryExecuted(command, eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> NonQueryExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            if (!DatabaseLock.IsWriteLockHeld) // enter a write lock as NonQueries are used to manipulate data only if not already held
+            {
+                DatabaseLock.EnterWriteLock();
+            }
+
+            return base.NonQueryExecutingAsync(command, eventData, result, cancellationToken);
+        }
+
+        public override ValueTask<int> NonQueryExecutedAsync(DbCommand command, CommandExecutedEventData eventData, int result, CancellationToken cancellationToken = default)
+        {
+            if (DatabaseLock.IsWriteLockHeld)
+            {
+                DatabaseLock.ExitWriteLock();
+            }
+
+            return base.NonQueryExecutedAsync(command, eventData, result, cancellationToken);
+        }
+
         /// <inheritdoc/>
         public override InterceptionResult<DbDataReader> ReaderExecuting(
             DbCommand command,
             CommandEventData eventData,
             InterceptionResult<DbDataReader> result)
         {
-#pragma warning disable MT1012 // Acquiring lock without guarantee of releasing
             if (!DatabaseLock.IsWriteLockHeld) // enter a read lock only if not already a write lock has been issued by the savechanges invocation
             {
                 DatabaseLock.EnterReadLock();
             }
 
-#pragma warning restore MT1012 // Acquiring lock without guarantee of releasing
             return base.ReaderExecuting(command, eventData, result);
         }
 
         /// <inheritdoc/>
         public override DbDataReader ReaderExecuted(DbCommand command, CommandExecutedEventData eventData, DbDataReader result)
         {
-#pragma warning disable MT1013 // Releasing lock without guarantee of execution
             if (!DatabaseLock.IsWriteLockHeld && DatabaseLock.IsReadLockHeld) // if the current thread has no write lock, it will have entered a read lock. As the write lock is managed by saveChanges only handle readlock here
             {
                 DatabaseLock.ExitReadLock();
             }
-#pragma warning restore MT1013 // Releasing lock without guarantee of execution
+
             return base.ReaderExecuted(command, eventData, result);
         }
 
         /// <inheritdoc/>
         public override void CommandCanceled(DbCommand command, CommandEndEventData eventData)
         {
-#pragma warning disable MT1013 // Releasing lock without guarantee of execution
             if (!DatabaseLock.IsWriteLockHeld && DatabaseLock.IsReadLockHeld) // if the current thread has no write lock, it will have entered a read lock. As the write lock is managed by saveChanges only handle readlock here
             {
                 DatabaseLock.ExitReadLock();
             }
-#pragma warning restore MT1013 // Releasing lock without guarantee of execution
+            else if (DatabaseLock.IsWriteLockHeld)
+            {
+                DatabaseLock.ExitWriteLock();
+            }
+
             base.CommandCanceled(command, eventData);
         }
 
         public override void CommandFailed(DbCommand command, CommandErrorEventData eventData)
         {
-#pragma warning disable MT1013 // Releasing lock without guarantee of execution
             if (!DatabaseLock.IsWriteLockHeld && DatabaseLock.IsReadLockHeld) // if the current thread has no write lock, it will have entered a read lock. As the write lock is managed by saveChanges only handle readlock here
             {
                 DatabaseLock.ExitReadLock();
             }
-#pragma warning restore MT1013 // Releasing lock without guarantee of execution
+            else if (DatabaseLock.IsWriteLockHeld)
+            {
+                DatabaseLock.ExitWriteLock();
+            }
+
             base.CommandFailed(command, eventData);
         }
     }
