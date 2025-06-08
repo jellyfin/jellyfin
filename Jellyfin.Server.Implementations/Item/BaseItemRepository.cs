dc5f1ee1d1f005dc60a010a18be9e88885d1b447
JPVenson <github@jpb.email>
2025-06-08 20:40:06 +0000

    double negate

--- Jellyfin.Server.Implementations/Item/BaseItemRepository.cs
+++ Jellyfin.Server.Implementations/Item/BaseItemRepository.cs
@@ -2151,7 +2151,7 @@ public sealed class BaseItemRepository
         if (filter.ExcludeProviderIds is not null && filter.ExcludeProviderIds.Count > 0)
         {
             var exclude = filter.ExcludeProviderIds.Select(e => $"{e.Key}:{e.Value}").ToArray();
-            baseQuery = baseQuery.Where(e => !e.Provider!.Select(f => f.ProviderId + ":" + f.ProviderValue)!.All(f => !exclude.Contains(f)));
+            baseQuery = baseQuery.Where(e => e.Provider!.Select(f => f.ProviderId + ":" + f.ProviderValue)!.All(f => !exclude.Contains(f)));
         }
 
         if (filter.HasAnyProviderId is not null && filter.HasAnyProviderId.Count > 0)
