8acdf89fe893c0fbeff6d40d706e62efd00cc81d
JPVenson <github@jpb.email>
2025-06-08 16:21:19 +0000

    Rename file

--- Jellyfin.Server/ServerSetupApp/StartupLoggerOfCategory.cs
+++ Jellyfin.Server/ServerSetupApp/StartupLoggerOfCategory.cs
@@ -8,7 +8,9 @@ namespace Jellyfin.Server.ServerSetupApp;
 /// Startup logger for usage with DI that utilises an underlying logger from the DI.
 /// </summary>
 /// <typeparam name="TCategory">The category of the underlying logger.</typeparam>
+#pragma warning disable SA1649 // File name should match first type name
 public class StartupLogger<TCategory> : StartupLogger, IStartupLogger<TCategory>
+#pragma warning restore SA1649 // File name should match first type name
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="StartupLogger{TCategory}"/> class.
