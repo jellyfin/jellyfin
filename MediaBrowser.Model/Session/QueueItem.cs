6c89445b68707c046fa7e68c4d1b14a80c3275bd
Cody Robibero <cody@robibe.ro>
2025-06-09 17:23:13 -0600

    Update QueueItem.cs

--- MediaBrowser.Model/Session/QueueItem.cs
+++ MediaBrowser.Model/Session/QueueItem.cs
@@ -3,14 +3,11 @@
 
 using System;
 
-namespace MediaBrowser.Model.Session
+namespace MediaBrowser.Model.Session;
+
+public record QueueItem
 {
-    public class QueueItem : IEquatable<QueueItem>
-    {
-        public Guid Id { get; set; }
+    public Guid Id { get; set; }
 
-        public string PlaylistItemId { get; set; }
-
-        public bool Equals(QueueItem other) => other is not null && Id.Equals(other.Id) && string.Equals(PlaylistItemId, other.PlaylistItemId, StringComparison.Ordinal);
-    }
+    public string PlaylistItemId { get; set; }
 }
