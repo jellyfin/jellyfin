c8a219bd2d28a8cd1abc25a410ef7582fbaa4769
Eyt-Lev <ecraft452@gmail.com>
2025-06-20 11:34:48 +0300

    db migartion

---
+++ src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/20250620082553_AddHiddenByUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfin.Server.Implementations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHiddenByUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHiddenByUser",
+                table: "UserData",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserData_ItemId_UserId_IsHiddenByUser",
+                table: "UserData",
+                columns: new[] { "ItemId", "UserId", "IsHiddenByUser" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserData_ItemId_UserId_IsHiddenByUser",
+                table: "UserData");
+
+            migrationBuilder.DropColumn(
+                name: "IsHiddenByUser",
+                table: "UserData");
+        }
+    }
+}
