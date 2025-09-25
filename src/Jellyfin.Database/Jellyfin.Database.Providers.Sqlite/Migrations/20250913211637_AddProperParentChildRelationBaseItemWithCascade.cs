555bef67a99d74bf1267f47d332aecca3e259b30
JPVenson <github@jpb.email>
2025-09-25 10:34:55 +0000

    WIP child delete

--- src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/20250913211637_AddProperParentChildRelationBaseItemWithCascade.cs
+++ src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/20250913211637_AddProperParentChildRelationBaseItemWithCascade.cs
@@ -10,6 +10,18 @@ namespace Jellyfin.Server.Implementations.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("""
+WITH RECURSIVE detachedParents AS (
+    SELECT Id FROM BaseItems child
+        WHERE
+        child.ParentId IS NOT NULL
+        AND
+        NOT EXISTS(SELECT 1 FROM BaseItems parent WHERE child.ParentId = parent.Id)
+)
+
+SELECT * FROM detachedParents
+DELETE FROM BaseItems d WHERE
+""");
             migrationBuilder.AddForeignKey(
                 name: "FK_BaseItems_BaseItems_ParentId",
                 table: "BaseItems",
