0f42aa892e0a7fe2ac4e680e7647515af0909e5e
theguymadmax <theguymadmax@proton.me>
2025-10-01 23:10:31 -0400

    Fix BoxSet sorting (#14919)
    Co-authored-by: Cody Robibero <cody@robibe.ro>

--- MediaBrowser.Controller/Entities/Folder.cs
+++ MediaBrowser.Controller/Entities/Folder.cs
@@ -1351,6 +1351,14 @@ namespace MediaBrowser.Controller.Entities
             var realChildren = visibleChildren
                 .Where(e => query is null || UserViewBuilder.FilterItem(e, query))
                 .ToArray();
+
+            if (this is BoxSet && (query.OrderBy is null || query.OrderBy.Count == 0))
+            {
+                realChildren = realChildren
+                    .OrderBy(e => e.ProductionYear ?? int.MaxValue)
+                    .ToArray();
+            }
+
             var childCount = realChildren.Length;
             if (result.Count < limit)
             {
