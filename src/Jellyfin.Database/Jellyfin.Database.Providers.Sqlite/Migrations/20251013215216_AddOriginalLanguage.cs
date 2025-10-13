3d69d22d9510fd66e5b7092bb1086382763fa138
Lampan-git <22211983+Lampan-git@users.noreply.github.com>
2025-10-13 23:54:28 +0200

    Add migration for original language

---
+++ src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/20251013215216_AddOriginalLanguage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfin.Server.Implementations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOriginalLanguage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOriginal",
+                table: "MediaStreamInfos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalLanguage",
+                table: "BaseItems",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "BaseItems",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "OriginalLanguage",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOriginal",
+                table: "MediaStreamInfos");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalLanguage",
+                table: "BaseItems");
+        }
+    }
+}
