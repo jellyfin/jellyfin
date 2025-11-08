4fbfbbe6705b61d0b7486835b4e49a4b239e3c52
Mikal Stordal <mikalstordal@gmail.com>
2025-11-08 13:29:45 +0100

    Apply changes based on feedback

--- MediaBrowser.Controller/IO/FileSystemHelper.cs
+++ MediaBrowser.Controller/IO/FileSystemHelper.cs
@@ -67,8 +67,6 @@ public static class FileSystemHelper
     /// </summary>
     /// <remarks>
     /// This helper exists because of this upstream runtime issue; https://github.com/dotnet/runtime/issues/92128.
-    ///
-    /// It is adapted to suit the use case of the consumers of this helper.
     /// </remarks>
     /// <param name="linkPath">The path of the file link.</param>
     /// <param name="returnFinalTarget">true to follow links to the final target; false to return the immediate next link.</param>
@@ -83,12 +81,9 @@ public static class FileSystemHelper
             return null;
         }
 
-        var targetInfo = (FileInfo?)null;
+        FileInfo? targetInfo = null;
         if (returnFinalTarget)
         {
-            // We're running the loop ourselves because of the upstream runtime issue (see remarks section of method).
-            // Because of this, we can't incur a nested IOException, but we do need to look out for infinite loops.
-            // Currently there is no check to abort if a link is too deeply nested.
             while (File.ResolveLinkTarget(targetInfo?.FullName ?? linkPath, returnFinalTarget: false) is FileInfo { } linkInfo)
             {
                 // Guard against infinite loops; if we reach the original path or the current target, we're done.
@@ -123,6 +118,9 @@ public static class FileSystemHelper
     /// <summary>
     /// Gets the target of the specified file link.
     /// </summary>
+    /// <remarks>
+    /// This helper exists because of this upstream runtime issue; https://github.com/dotnet/runtime/issues/92128.
+    /// </remarks>
     /// <param name="fileInfo">The file info of the file link.</param>
     /// <param name="returnFinalTarget">true to follow links to the final target; false to return the immediate next link.</param>
     /// <returns>
