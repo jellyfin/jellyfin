faf1cea63e042e3b2bf4ab87f38d86d5a2de0b07
theguymadmax <171496228+theguymadmax@users.noreply.github.com>
2025-11-17 14:09:13 -0500

    Backport pull request #15514 from jellyfin/release-10.11.z
    Add 1 minute tolerance for NFO change detection
    
    Original-merge: 6566188e453b42604dbb3ce532937951e88565d0
    
    Merged-by: crobibero <cody@robibe.ro>
    
    Backported-by: Bond_009 <bond.009@outlook.com>

--- MediaBrowser.XbmcMetadata/Providers/BaseNfoProvider.cs
+++ MediaBrowser.XbmcMetadata/Providers/BaseNfoProvider.cs
@@ -68,12 +68,15 @@ namespace MediaBrowser.XbmcMetadata.Providers
         {
             var file = GetXmlFile(new ItemInfo(item), directoryService);
 
-            if (file is null)
+            if (file?.Exists is not true)
             {
                 return false;
             }
 
-            return file.Exists && _fileSystem.GetLastWriteTimeUtc(file) > item.DateLastSaved;
+            var fileTime = _fileSystem.GetLastWriteTimeUtc(file);
+
+            // 1 minute tolerance to avoid detecting our own file writes
+            return (fileTime - item.DateLastSaved) > TimeSpan.FromMinutes(1);
         }
 
         protected abstract void Fetch(MetadataResult<T> result, string path, CancellationToken cancellationToken);
