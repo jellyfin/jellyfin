91d4ee4160ae07dfaeff6bacdfaa46ebf33ff935
Cody Robibero <cody@robibe.ro>
2025-11-19 19:21:43 -0700

    Add ability to sort and filter activity log entries

---
+++ Jellyfin.Data/Enums/ActivityLogSortBy.cs
@@ -0,0 +1,47 @@
+﻿namespace Jellyfin.Data.Enums;
+
+/// <summary>
+/// Activity log sorting options.
+/// </summary>
+public enum ActivityLogSortBy
+{
+    /// <summary>
+    /// Sort by name.
+    /// </summary>
+    Name = 0,
+
+    /// <summary>
+    /// Sort by overview.
+    /// </summary>
+    Overiew = 1,
+
+    /// <summary>
+    /// Sort by short overview.
+    /// </summary>
+    ShortOverview = 2,
+
+    /// <summary>
+    /// Sort by type.
+    /// </summary>
+    Type = 3,
+
+    /// <summary>
+    /// Sort by item id.
+    /// </summary>
+    ItemId = 4,
+
+    /// <summary>
+    /// Sort by date.
+    /// </summary>
+    DateCreated = 5,
+
+    /// <summary>
+    /// Sort by user id.
+    /// </summary>
+    UserId = 6,
+
+    /// <summary>
+    /// Sort by severity.
+    /// </summary>
+    LogSeverity = 7
+}
--- Jellyfin.Server.Implementations/Activity/ActivityManager.cs
+++ Jellyfin.Server.Implementations/Activity/ActivityManager.cs
@@ -1,103 +1,185 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Jellyfin.Data.Enums;
 using Jellyfin.Data.Events;
 using Jellyfin.Data.Queries;
 using Jellyfin.Database.Implementations;
 using Jellyfin.Database.Implementations.Entities;
+using Jellyfin.Database.Implementations.Enums;
+using Jellyfin.Extensions;
 using MediaBrowser.Model.Activity;
 using MediaBrowser.Model.Querying;
 using Microsoft.EntityFrameworkCore;
 
-namespace Jellyfin.Server.Implementations.Activity
+namespace Jellyfin.Server.Implementations.Activity;
+
+/// <summary>
+/// Manages the storage and retrieval of <see cref="ActivityLog"/> instances.
+/// </summary>
+public class ActivityManager : IActivityManager
 {
+    private readonly IDbContextFactory<JellyfinDbContext> _provider;
+
     /// <summary>
-    /// Manages the storage and retrieval of <see cref="ActivityLog"/> instances.
+    /// Initializes a new instance of the <see cref="ActivityManager"/> class.
     /// </summary>
-    public class ActivityManager : IActivityManager
+    /// <param name="provider">The Jellyfin database provider.</param>
+    public ActivityManager(IDbContextFactory<JellyfinDbContext> provider)
     {
-        private readonly IDbContextFactory<JellyfinDbContext> _provider;
+        _provider = provider;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ActivityManager"/> class.
-        /// </summary>
-        /// <param name="provider">The Jellyfin database provider.</param>
-        public ActivityManager(IDbContextFactory<JellyfinDbContext> provider)
+    /// <inheritdoc/>
+    public event EventHandler<GenericEventArgs<ActivityLogEntry>>? EntryCreated;
+
+    /// <inheritdoc/>
+    public async Task CreateAsync(ActivityLog entry)
+    {
+        var dbContext = await _provider.CreateDbContextAsync().ConfigureAwait(false);
+        await using (dbContext.ConfigureAwait(false))
         {
-            _provider = provider;
+            dbContext.ActivityLogs.Add(entry);
+            await dbContext.SaveChangesAsync().ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
-        public event EventHandler<GenericEventArgs<ActivityLogEntry>>? EntryCreated;
+        EntryCreated?.Invoke(this, new GenericEventArgs<ActivityLogEntry>(ConvertToOldModel(entry)));
+    }
 
-        /// <inheritdoc/>
-        public async Task CreateAsync(ActivityLog entry)
+    /// <inheritdoc/>
+    public async Task<QueryResult<ActivityLogEntry>> GetPagedResultAsync(ActivityLogQuery query)
+    {
+        var dbContext = await _provider.CreateDbContextAsync().ConfigureAwait(false);
+        await using (dbContext.ConfigureAwait(false))
         {
-            var dbContext = await _provider.CreateDbContextAsync().ConfigureAwait(false);
-            await using (dbContext.ConfigureAwait(false))
+            var entries = dbContext.ActivityLogs.AsQueryable();
+
+            if (query.HasUserId is not null)
             {
-                dbContext.ActivityLogs.Add(entry);
-                await dbContext.SaveChangesAsync().ConfigureAwait(false);
+                entries = entries.Where(e => e.UserId.Equals(default) != query.HasUserId.Value);
             }
 
-            EntryCreated?.Invoke(this, new GenericEventArgs<ActivityLogEntry>(ConvertToOldModel(entry)));
-        }
-
-        /// <inheritdoc/>
-        public async Task<QueryResult<ActivityLogEntry>> GetPagedResultAsync(ActivityLogQuery query)
-        {
-            var dbContext = await _provider.CreateDbContextAsync().ConfigureAwait(false);
-            await using (dbContext.ConfigureAwait(false))
+            if (query.MinDate is not null)
             {
-                var entries = dbContext.ActivityLogs
-                    .OrderByDescending(entry => entry.DateCreated)
-                    .Where(entry => query.MinDate == null || entry.DateCreated >= query.MinDate)
-                    .Where(entry => !query.HasUserId.HasValue || entry.UserId.Equals(default) != query.HasUserId.Value);
-
-                return new QueryResult<ActivityLogEntry>(
-                    query.Skip,
-                    await entries.CountAsync().ConfigureAwait(false),
-                    await entries
-                        .Skip(query.Skip ?? 0)
-                        .Take(query.Limit ?? 100)
-                        .Select(entity => new ActivityLogEntry(entity.Name, entity.Type, entity.UserId)
-                        {
-                            Id = entity.Id,
-                            Overview = entity.Overview,
-                            ShortOverview = entity.ShortOverview,
-                            ItemId = entity.ItemId,
-                            Date = entity.DateCreated,
-                            Severity = entity.LogSeverity
-                        })
-                        .ToListAsync()
-                        .ConfigureAwait(false));
+                entries = entries.Where(e => e.DateCreated >= query.MinDate.Value);
             }
-        }
 
-        /// <inheritdoc />
-        public async Task CleanAsync(DateTime startDate)
-        {
-            var dbContext = await _provider.CreateDbContextAsync().ConfigureAwait(false);
-            await using (dbContext.ConfigureAwait(false))
+            if (!string.IsNullOrEmpty(query.Name))
             {
-                await dbContext.ActivityLogs
-                    .Where(entry => entry.DateCreated <= startDate)
-                    .ExecuteDeleteAsync()
-                    .ConfigureAwait(false);
+                entries = entries.Where(e => EF.Functions.Like(e.Name, $"%{query.Name}%"));
             }
+
+            if (!string.IsNullOrEmpty(query.Overview))
+            {
+                entries = entries.Where(e => EF.Functions.Like(e.Overview, $"%{query.Overview}%"));
+            }
+
+            if (!string.IsNullOrEmpty(query.ShortOverview))
+            {
+                entries = entries.Where(e => EF.Functions.Like(e.ShortOverview, $"%{query.ShortOverview}%"));
+            }
+
+            if (!string.IsNullOrEmpty(query.Type))
+            {
+                entries = entries.Where(e => EF.Functions.Like(e.Type, $"%{query.Type}%"));
+            }
+
+            if (!string.IsNullOrEmpty(query.ItemId))
+            {
+                entries = entries.Where(e => e.ItemId == query.ItemId);
+            }
+
+            if (!query.UserId.IsNullOrEmpty())
+            {
+                entries = entries.Where(e => e.UserId.Equals(query.UserId.Value));
+            }
+
+            if (query.Severity is not null)
+            {
+                entries = entries.Where(e => e.LogSeverity == query.Severity);
+            }
+
+            return new QueryResult<ActivityLogEntry>(
+                query.Skip,
+                await entries.CountAsync().ConfigureAwait(false),
+                await ApplyOrdering(entries, query.OrderBy)
+                    .Skip(query.Skip ?? 0)
+                    .Take(query.Limit ?? 100)
+                    .Select(entity => new ActivityLogEntry(entity.Name, entity.Type, entity.UserId)
+                    {
+                        Id = entity.Id,
+                        Overview = entity.Overview,
+                        ShortOverview = entity.ShortOverview,
+                        ItemId = entity.ItemId,
+                        Date = entity.DateCreated,
+                        Severity = entity.LogSeverity
+                    })
+                    .ToListAsync()
+                    .ConfigureAwait(false));
+        }
+    }
+
+    /// <inheritdoc />
+    public async Task CleanAsync(DateTime startDate)
+    {
+        var dbContext = await _provider.CreateDbContextAsync().ConfigureAwait(false);
+        await using (dbContext.ConfigureAwait(false))
+        {
+            await dbContext.ActivityLogs
+                .Where(entry => entry.DateCreated <= startDate)
+                .ExecuteDeleteAsync()
+                .ConfigureAwait(false);
+        }
+    }
+
+    private static ActivityLogEntry ConvertToOldModel(ActivityLog entry)
+    {
+        return new ActivityLogEntry(entry.Name, entry.Type, entry.UserId)
+        {
+            Id = entry.Id,
+            Overview = entry.Overview,
+            ShortOverview = entry.ShortOverview,
+            ItemId = entry.ItemId,
+            Date = entry.DateCreated,
+            Severity = entry.LogSeverity
+        };
+    }
+
+    private IOrderedQueryable<ActivityLog> ApplyOrdering(IQueryable<ActivityLog> query, IReadOnlyCollection<(ActivityLogSortBy, SortOrder)>? sorting)
+    {
+        if (sorting is null || sorting.Count == 0)
+        {
+            return query.OrderByDescending(e => e.DateCreated);
         }
 
-        private static ActivityLogEntry ConvertToOldModel(ActivityLog entry)
+        IOrderedQueryable<ActivityLog> ordered = null!;
+
+        foreach (var (sortBy, sortOrder) in sorting)
         {
-            return new ActivityLogEntry(entry.Name, entry.Type, entry.UserId)
-            {
-                Id = entry.Id,
-                Overview = entry.Overview,
-                ShortOverview = entry.ShortOverview,
-                ItemId = entry.ItemId,
-                Date = entry.DateCreated,
-                Severity = entry.LogSeverity
-            };
+            var orderBy = MapOrderBy(sortBy);
+            ordered = sortOrder == SortOrder.Ascending
+                ? (ordered ?? query).OrderBy(orderBy)
+                : (ordered ?? query).OrderByDescending(orderBy);
         }
+
+        return ordered;
+    }
+
+    private Expression<Func<ActivityLog, object?>> MapOrderBy(ActivityLogSortBy sortBy)
+    {
+        return sortBy switch
+        {
+            ActivityLogSortBy.Name => e => e.Name,
+            ActivityLogSortBy.Overiew => e => e.Overview,
+            ActivityLogSortBy.ShortOverview => e => e.ShortOverview,
+            ActivityLogSortBy.Type => e => e.Type,
+            ActivityLogSortBy.ItemId => e => e.ItemId,
+            ActivityLogSortBy.DateCreated => e => e.DateCreated,
+            ActivityLogSortBy.UserId => e => e.UserId,
+            ActivityLogSortBy.LogSeverity => e => e.LogSeverity,
+            _ => throw new ArgumentOutOfRangeException(nameof(sortBy), sortBy, "Unhandled ActivityLogSortBy")
+        };
     }
 }
