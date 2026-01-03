308fcc1a11ce4012f49e6c3ccd2ed3f1c157dcfe
Peaches_MLG <PeachesMLG100@gmail.com>
2026-01-03 11:39:36 +0000

    Addded comment for min/max playback speed

--- Jellyfin.Api/Models/SyncPlayDtos/SetPlaybackSpeedRequestDto.cs
+++ Jellyfin.Api/Models/SyncPlayDtos/SetPlaybackSpeedRequestDto.cs
@@ -6,7 +6,7 @@ namespace Jellyfin.Api.Models.SyncPlayDtos;
 public class SetPlaybackSpeedRequestDto
 {
     /// <summary>
-    /// Gets or sets the playback speed.
+    /// Gets or sets the playback speed, should be between 0.25 and 5.
     /// </summary>
     /// <value>The playback speed.</value>
     public double PlaybackSpeed { get; set; } = 1.0;
