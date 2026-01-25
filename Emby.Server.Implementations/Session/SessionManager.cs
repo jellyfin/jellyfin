d41e302418120c06602aa03ed1f0ca4c481fab2a
Niels van Velzen <git@ndat.nl>
2026-01-25 21:19:38 +0100

    Fix SessionInfoWebSocketListener not using SessionInfoDto

--- Emby.Server.Implementations/Session/SessionManager.cs
+++ Emby.Server.Implementations/Session/SessionManager.cs
@@ -1175,7 +1175,8 @@ namespace Emby.Server.Implementations.Session
             return session;
         }
 
-        private SessionInfoDto ToSessionInfoDto(SessionInfo sessionInfo)
+        /// <inheritdoc />
+        public SessionInfoDto ToSessionInfoDto(SessionInfo sessionInfo)
         {
             return new SessionInfoDto
             {
