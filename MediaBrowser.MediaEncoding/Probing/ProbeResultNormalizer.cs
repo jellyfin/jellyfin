0bee85c618a42a66823a6f65207ecf1f2c32f2ff
Brady3035 <40967324+Brady3035@users.noreply.github.com>
2026-03-25 15:26:04 -0500

    Revert "Fixed Formating"
    This reverts commit ff41c6450631c4f8df0d50aa6bbef08c35396108.

--- MediaBrowser.MediaEncoding/Probing/ProbeResultNormalizer.cs
+++ MediaBrowser.MediaEncoding/Probing/ProbeResultNormalizer.cs
@@ -996,9 +996,12 @@ namespace MediaBrowser.MediaEncoding.Probing
             }
 
             // FALLBACK C: Use the global format info (useful for FLAC or single-stream containers)
-            if (bitrate <= 0 && formatInfo != null && int.TryParse(formatInfo.BitRate, CultureInfo.InvariantCulture, out var formatVal))
+            if (bitrate <= 0 && formatInfo != null)
             {
-                bitrate = formatVal;
+                if (int.TryParse(formatInfo.BitRate, CultureInfo.InvariantCulture, out var formatVal))
+                {
+                    bitrate = formatVal;
+                }
             }
 
             if (bitrate > 0)
