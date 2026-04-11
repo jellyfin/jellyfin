44df67024437d95ef79242297d32eb3bce96015b
Shadowghost <Ghost_of_Stone@web.de>
2026-04-11 18:05:22 +0200

    Apply review suggestion

--- Jellyfin.Server/ServerSetupApp/SetupServer.cs
+++ Jellyfin.Server/ServerSetupApp/SetupServer.cs
@@ -142,6 +142,7 @@ public sealed class SetupServer : IDisposable
         ThrowIfDisposed();
         var retryAfterValue = TimeSpan.FromSeconds(5);
         var config = _configurationManager.GetNetworkConfiguration()!;
+        _startupServer?.Dispose();
         _startupServer = Host.CreateDefaultBuilder(["hostBuilder:reloadConfigOnChange=false"])
             .UseConsoleLifetime()
             .UseSerilog()
@@ -277,8 +278,6 @@ public sealed class SetupServer : IDisposable
         }
 
         await _startupServer.StopAsync().ConfigureAwait(false);
-        _startupServer.Dispose();
-        _startupServer = null;
         IsAlive = false;
     }
 
