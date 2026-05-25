7939f3b009e830e38a3f37455418011043429ee3
TheMelmacian <76712303+TheMelmacian@users.noreply.github.com>
2026-05-25 23:40:40 +0200

    only fetch language codes for the requested library when generating filter values

--- Emby.Server.Implementations/Library/LibraryManager.cs
+++ Emby.Server.Implementations/Library/LibraryManager.cs
@@ -3862,5 +3862,17 @@ namespace Emby.Server.Implementations.Library
         {
             return _mediaStreamRepository.GetMediaStreamLanguages(mediaStreamType);
         }
+
+        /// <inheritdoc />
+        public IReadOnlyList<string> GetMediaStreamLanguages(MediaStreamType mediaStreamType, InternalItemsQuery query)
+        {
+            if (query.User is not null)
+            {
+                AddUserToQuery(query, query.User);
+            }
+
+            SetTopParentOrAncestorIds(query);
+            return _itemRepository.GetMediaStreamLanguages(query, mediaStreamType);
+        }
     }
 }
