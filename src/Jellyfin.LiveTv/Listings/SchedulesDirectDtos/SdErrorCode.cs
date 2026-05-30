d2b3c0d3ea522bbf0a8450482a2319300ac88957
Arazil <arazil@arazil.net>
2026-05-30 21:28:45 +0000

    Fix incorrect 3XXX series codes.

--- src/Jellyfin.LiveTv/Listings/SchedulesDirectDtos/SdErrorCode.cs
+++ src/Jellyfin.LiveTv/Listings/SchedulesDirectDtos/SdErrorCode.cs
@@ -10,12 +10,12 @@ public enum SdErrorCode
     /// <summary>
     /// Schedules Direct unavailable/out of service.
     /// </summary>
-    SvcUnavailable = 3001,
+    SvcUnavailable = 3000,
 
     /// <summary>
     /// Schedules Direct busy.
     /// </summary>
-    SvcBusy = 3002,
+    SvcBusy = 3001,
 
     /// <summary>
     /// Account expired.
