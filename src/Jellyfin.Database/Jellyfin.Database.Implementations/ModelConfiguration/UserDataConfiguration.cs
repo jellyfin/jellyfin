0e0dacf0cf7608bcdb55c38c56b808209dc3093c
Alberto <alberto8garcia@gmail.com>
2026-07-27 04:44:43 +0200

    fix: delete indexes

--- src/Jellyfin.Database/Jellyfin.Database.Implementations/ModelConfiguration/UserDataConfiguration.cs
+++ src/Jellyfin.Database/Jellyfin.Database.Implementations/ModelConfiguration/UserDataConfiguration.cs
@@ -20,8 +20,6 @@ public class UserDataConfiguration : IEntityTypeConfiguration<UserData>
         builder.HasIndex(d => new { d.UserId, d.ItemId, d.LastPlayedDate });
         builder.HasIndex(d => new { d.UserId, d.Played, d.ItemId });
         builder.HasIndex(d => new { d.UserId, d.IsFavorite, d.ItemId });
-        builder.HasIndex(d => new { d.ItemId, d.UserId, d.SkipCount });
-        builder.HasIndex(d => new { d.UserId, d.SkipCount, d.ItemId });
         builder.HasOne(e => e.Item).WithMany(e => e.UserData);
     }
 }
