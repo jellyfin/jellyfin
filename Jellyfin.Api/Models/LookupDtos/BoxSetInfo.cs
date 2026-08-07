ea05d42d43427c4528c801227e2fdaa970c853db
Shadowghost <Ghost_of_Stone@web.de>
2026-08-07 22:53:02 +0200

    Decouple the remote search API contract from the provider lookup info

---
+++ Jellyfin.Api/Models/LookupDtos/BoxSetInfo.cs
@@ -0,0 +1,9 @@
+namespace Jellyfin.Api.Models.LookupDtos
+{
+    /// <summary>
+    /// The lookup info for box sets.
+    /// </summary>
+    public class BoxSetInfo : ItemLookupInfo
+    {
+    }
+}
