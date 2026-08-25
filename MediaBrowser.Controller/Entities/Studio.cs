8c0775e9412082ab427bb93cf40ecca6ce83c492
Shadowghost <Ghost_of_Stone@web.de>
2026-08-25 20:19:52 +0200

    Bind the folder name an item-by-name entity resolves to

--- MediaBrowser.Controller/Entities/Studio.cs
+++ MediaBrowser.Controller/Entities/Studio.cs
@@ -78,10 +78,7 @@ namespace MediaBrowser.Controller.Entities
 
         public static string GetPath(string name, bool normalizeName)
         {
-            // Trim the period at the end because windows will have a hard time with that
-            var validName = normalizeName ?
-                FileSystem.GetValidFilename(name).Trim().TrimEnd('.') :
-                name;
+            var validName = normalizeName ? GetItemByNameFolderName(name) : name;
 
             return System.IO.Path.Combine(ConfigurationManager.ApplicationPaths.StudioPath, validName);
         }
