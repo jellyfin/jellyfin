a0dca2f121db41ec43a8bb5e519b936cc9bab661
Cody Robibero <cody@robibe.ro>
2026-09-08 12:10:27 -0400

    Fix trickplay using the wrong video stream

--- MediaBrowser.MediaEncoding/Encoder/MediaEncoder.cs
+++ MediaBrowser.MediaEncoding/Encoder/MediaEncoder.cs
@@ -922,6 +922,9 @@ namespace MediaBrowser.MediaEncoding.Encoder
                 inputArg = "-hwaccel_flags +low_priority " + inputArg;
             }
 
+            // Force the video stream, otherwise ffmpeg may pick a cover image.
+            inputArg += " -map 0:" + imageStream.Index.ToString(CultureInfo.InvariantCulture);
+
             var filterParam = encodingHelper.GetVideoProcessingFilterParam(jobState, options, vidEncoder).Trim();
             if (string.IsNullOrWhiteSpace(filterParam))
             {
